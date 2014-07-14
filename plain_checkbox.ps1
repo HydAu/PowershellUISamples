c40cb535e45d21acbe847af73954d03724fd8f6c
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-07-13 21:33:57 -0400

    final version

--- plain_checkbox.ps1
+++ plain_checkbox.ps1
@@ -1,3 +1,12 @@
+  $RESULT_OK = 0
+  $RESULT_CANCEL = 2
+  $Readable = @{ 
+    $RESULT_OK = 'OK' 
+    $RESULT_CANCEL = 'CANCEL'
+  } 
+
+
+
 Add-Type -TypeDefinition @"
 using System;
 using System.Windows.Forms;
@@ -39,196 +48,172 @@ function PromptAuto(
 	[Object] $caller = $null 
 	){
 
-[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Drawing') 
-[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Collections') 
-[void] [System.Reflection.Assembly]::LoadWithPartialName('System.ComponentModel') 
-[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
-[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Data') 
+  [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Drawing') 
+  [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Collections') 
+  [void] [System.Reflection.Assembly]::LoadWithPartialName('System.ComponentModel') 
+  [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+  [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Data') 
 
-$f = New-Object System.Windows.Forms.Form 
-$f.Text = $title
-$groupBox1 = New-Object System.Windows.Forms.GroupBox;
-$checkBox1 = New-Object System.Windows.Forms.CheckBox;
-$checkBox2 = New-Object System.Windows.Forms.CheckBox ;
-$checkBox3 = New-Object System.Windows.Forms.CheckBox ;
-$radioButton1     = New-Object System.Windows.Forms.RadioButton;
-$radioButton2     = New-Object System.Windows.Forms.RadioButton;
-$radioButton3     = New-Object System.Windows.Forms.RadioButton;
-$button1  = New-Object System.Windows.Forms.Button;
+  $f = New-Object System.Windows.Forms.Form 
+  $f.Text = $title
+  $groupBox1 = New-Object System.Windows.Forms.GroupBox
+  $checkBox1 = New-Object System.Windows.Forms.CheckBox
+  $checkBox2 = New-Object System.Windows.Forms.CheckBox
+  $checkBox3 = New-Object System.Windows.Forms.CheckBox
+  $radioButton1 = New-Object System.Windows.Forms.RadioButton
+  $radioButton2 = New-Object System.Windows.Forms.RadioButton
+  $radioButton3 = New-Object System.Windows.Forms.RadioButton
+  $button1  = New-Object System.Windows.Forms.Button
+  $components =  New-Object System.ComponentModel.Container
 
-$components =  New-Object System.ComponentModel.Container;
+  $groupBox1.SuspendLayout()
+  $f.SuspendLayout()
+  $color = ''
+  $shapes = @()     
 
-$groupBox1.SuspendLayout();
-$f.SuspendLayout();
-            $str = "";
-             
-            # groupBox1
-             
-            $groupBox1.Controls.AddRange(  @(
-                                                                                    $radioButton1,
-                                                                                    $radioButton2,
-                                                                                    $radioButton3 ));
-            $groupBox1.Location = New-Object System.Drawing.Point(8, 120);
-            $groupBox1.Name = "groupBox1";
-            $groupBox1.Size = New-Object System.Drawing.Size(120, 144);
-            $groupBox1.TabIndex = 0;
-            $groupBox1.TabStop = false;
-            $groupBox1.Text = "Color";
+  # groupBox1   
+  $groupBox1.Controls.AddRange(  
+     @(
+       $radioButton1,
+       $radioButton2,
+       $radioButton3 
+      ))
+  $groupBox1.Location = New-Object System.Drawing.Point(8, 120)
+  $groupBox1.Name = 'groupBox1'
+  $groupBox1.Size = New-Object System.Drawing.Size(120, 144)
+  $groupBox1.TabIndex = 0
+  $groupBox1.TabStop = $false
+  $groupBox1.Text = 'Color'
 
+  # checkBox1   
+  $checkBox1.Location = New-Object System.Drawing.Point(8, 8)
+  $checkBox1.Name = 'checkBox1'
+  $checkBox1.TabIndex = 1
+  $checkBox1.Text = 'Circle'
+   
+  # checkBox2
+   
+  $checkBox2.Location = New-Object System.Drawing.Point(8, 40)
+  $checkBox2.Name = 'checkBox2'
+  $checkBox2.TabIndex = 2
+  $checkBox2.Text = 'Rectangle'
+   
+  # checkBox3
+   
+  $checkBox3.Location = New-Object System.Drawing.Point(8, 72)
+  $checkBox3.Name = 'checkBox3'
+  $checkBox3.TabIndex = 3
+  $checkBox3.Text = 'Triangle'
+  
+  # radioButton1
+   
+  $radioButton1.Location = New-Object System.Drawing.Point(8, 32)
+  $radioButton1.Name = 'radioButton1'
+  $radioButton1.TabIndex = 4
+  $radioButton1.Text = 'Red'
+  $radioButton1.Add_CheckedChanged({ })
+   
+  # radioButton2
+   
+  $radioButton2.Location = New-Object System.Drawing.Point(8, 64)
+  $radioButton2.Name = 'radioButton2'
+  $radioButton2.TabIndex = 5
+  $radioButton2.Text = 'Green'
+   
+  # radioButton3
+   
+  $radioButton3.Location = New-Object System.Drawing.Point(8, 96)
+  $radioButton3.Name = 'radioButton3'
+  $radioButton3.TabIndex = 6
+  $radioButton3.Text = 'Blue'
+   
+  # button1
+   
+  $button1.Location = New-Object System.Drawing.Point(8, 280)
+  $button1.Name = 'button1'
+  $button1.Size = New-Object System.Drawing.Size(112, 32)
+  $button1.TabIndex = 4
+  $button1.Text = 'Draw'
 
-            # $groupBox1.Enter += New-Object System.EventHandler({ } );
+  $button1.Add_Click({
 
-            $groupBox1.Add_Enter({ 
+  $color = ''
+  $shapes = @()     
+  foreach ($o in @($radioButton1, $radioButton2, $radioButton3)){
+  if ($o.Checked){
+      $color = $o.Text}
+  }
+  foreach ($o in @($checkBox1, $checkBox2, $checkBox3)){
+  if ($o.Checked){
+      $shapes += $o.Text}
 
-} );
-            # checkBox1
-             
-            $checkBox1.Location = New-Object System.Drawing.Point(8, 8);
-            $checkBox1.Name = "checkBox1";
-            $checkBox1.TabIndex = 1;
-            $checkBox1.Text = "Circle";
-             
-            # checkBox2
-             
-            $checkBox2.Location = New-Object System.Drawing.Point(8, 40);
-            $checkBox2.Name = "checkBox2";
-            $checkBox2.TabIndex = 2;
-            $checkBox2.Text = "Rectangle";
-             
-            # checkBox3
-             
-            $checkBox3.Location = New-Object System.Drawing.Point(8, 72);
-            $checkBox3.Name = "checkBox3";
-            $checkBox3.TabIndex = 3;
-            $checkBox3.Text = "Triangle";
-            
-            # radioButton1
-             
-            $radioButton1.Location = New-Object System.Drawing.Point(8, 32);
-            $radioButton1.Name = "radioButton1";
-            $radioButton1.TabIndex = 4;
-            $radioButton1.Text = "Red";
-            # $b3.Add_Click({$caller.Data =  $RESULT_CANCEL ; $f.Close()})
-            $radioButton1.Add_CheckedChanged({$caller.Data =  $RESULT_CANCEL ; });
-             
-            # radioButton2
-             
-            $radioButton2.Location = New-Object System.Drawing.Point(8, 64);
-            $radioButton2.Name = "radioButton2";
-            $radioButton2.TabIndex = 5;
-            $radioButton2.Text = "Green";
-             
-            # radioButton3
-             
-            $radioButton3.Location = New-Object System.Drawing.Point(8, 96);
-            $radioButton3.Name = "radioButton3";
-            $radioButton3.TabIndex = 6;
-            $radioButton3.Text = "Blue";
-             
-            # button1
-             
-            $button1.Location = New-Object System.Drawing.Point(8, 280);
-            $button1.Name = "button1";
-            $button1.Size = New-Object System.Drawing.Size(112, 32);
-            $button1.TabIndex = 4;
-            $button1.Text = "Draw";
+  }
+  $g = [System.Drawing.Graphics]::FromHwnd($f.Handle)
+  $rc = New-Object System.Drawing.Rectangle(150, 50, 250, 250)
+  $brush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::White)
+  $g.FillRectangle($brush, $rc)
+  $font = New-Object System.Drawing.Font('Verdana', 12)
+  $col = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::Black)
+  $str = [String]::Join(';', $shapes )
+  $pos1 = New-Object System.Drawing.PointF(160, 60)
+  $pos2 = New-Object System.Drawing.PointF(160, 80)
 
-            # this.button1.Click += New-Object  System.EventHandler(this.button1_Click);
-            $button1.Add_Click({
+  $g.DrawString($color, $font, $col , $pos1)
+  $g.DrawString($str, $font, $col , $pos2)
+  start-sleep 1
 
-            # $g = Graphics.FromHwnd($f.Handle);
-            # $rc = New-Object Rectangle(150, 50, 250, 250);
-            $str = '';             
-            if($radioButton1.Checked)
-            {
-                $str = $radioButton1.Text;
-            }
-            if($radioButton2.Checked)
-            {
-                $str =$radioButton2.Text;
-            }
-            if($radioButton3.Checked)
-            {
-                $str =$radioButton3.Text;
-            }
+  $caller.Message =  ('color:{0} shapes:{1}' -f $color , $str)
 
-            if ($checkBox1.Checked)
-            {
-                $str += $checkBox1.Text;
-            }
-            if ($checkBox2.Checked)
-            {
-                $str += $checkBox2.Text;
-            }
-            if ($checkBox3.Checked)
-            {
-                $str += $checkBox3.Text;
-              #  $g.FillRectangle(New-Object SolidBrush(Color.White), rc);
-              #  $g.DrawString($str, New-Object Font("Verdana", 12), New-Object SolidBrush(Color.Black), rc);
-            }
-
-            $caller.Message = $str; 
-            $f.Close();
+  $f.Close()
  })
-             
-            # Form1
-            
-            $f.AutoScaleBaseSize = New-Object System.Drawing.Size(5, 13);
-            $f.ClientSize = New-Object System.Drawing.Size(408, 317);
-            $f.Controls.AddRange( @(
-                                                                          $button1,
-                                                                          $checkBox3,
-                                                                          $checkBox2,
-                                                                          $checkBox1,
-                                                                          $groupBox1));
+   
+  # Form1
+  
+  $f.AutoScaleBaseSize = New-Object System.Drawing.Size(5, 13)
+  $f.ClientSize = New-Object System.Drawing.Size(408, 317)
+  $f.Controls.AddRange( @(
+     $button1,
+     $checkBox3,
+     $checkBox2,
+     $checkBox1,
+     $groupBox1))
 
-            $f.Name = "Form1";
-            $f.Text = "CheckBox and RadioButton Sample";
-            # $f.groupBox1.ResumeLayout($false);
-            $f.ResumeLayout($false);
+  $f.Name = 'Form1'
+  $f.Text = 'CheckBox and RadioButton Sample'
+  $groupBox1.ResumeLayout($false)
+  $f.ResumeLayout($false)
+
+  $f.StartPosition = 'CenterScreen'
+
+  $f.KeyPreview = $True
+
+  $f.Add_KeyDown({
+
+    if ($_.KeyCode -eq 'Escape')  { $caller.Data = $RESULT_CANCEL }
+    else          {  }  
+    $f.Close()
+  })
 
 
+  $f.Topmost = $True
+  if ($caller -eq $null ){
+    $caller = New-Object Win32Window -ArgumentList ([System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle)
+  }
 
+  $f.Add_Shown( { $f.Activate() } )
 
-$RESULT_MANUAL = 0
-$RESULT_AUTOMATIC = 1
-$RESULT_CANCEL = 2
-$Readable = @{ 
-	$RESULT_AUTOMATIC = 'AUTOMATIC'; 
-	$RESULT_MANUAL = 'MANUAL' ; 
-	$RESULT_CANCEL = 'CANCEL'
-	} 
+  [Void] $f.ShowDialog([Win32Window ] ($caller) )
+  $F.Dispose()
 
-$f.StartPosition = 'CenterScreen'
-
-$f.KeyPreview = $True
-
-<#$f.Add_KeyDown({
-
-	if     ($_.KeyCode -eq 'M')       { $caller.Data = $RESULT_MANUAL }
-	elseif ($_.KeyCode -eq 'A')       { $caller.Data = $RESULT_AUTOMATIC }
-	elseif ($_.KeyCode -eq 'Escape')  { $caller.Data = $RESULT_CANCEL }
-	else                              { return }  
-	$f.Close()
-
-})
-#>
-
-$f.Topmost = $True
-if ($caller -eq $null ){
-  $caller = New-Object Win32Window -ArgumentList ([System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle)
+  return $caller.Data
 }
 
-$f.Add_Shown( { $f.Activate() } )
+$DebugPreference = 'Continue'
+$process_window = New-Object Win32Window -ArgumentList ([System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle)
 
-[Void] $f.ShowDialog([Win32Window ] ($caller) )
-
-# NOTE: set $DebugPreference = "Continue" to reveal debug messages
-# write-debug ("Result is : {0} ({1})" -f $Readable.Item($caller.Data) , $caller.Data )
-write-output ("Selection is : {0}" -f  ,$caller.Message )
-
-return $caller.Data
-
-        }
-           
-PromptAuto
+PromptAuto "" "" $process_window
+  if($process_window.Data -ne $RESULT_CANCEL) {
+    write-debug ('Selection is : {0}' -f  , $process_window.Message )
+  } else { 
+    write-debug ('Result is : {0} ({1})' -f $Readable.Item($process_window.Data) , $process_window.Data )
+  }
