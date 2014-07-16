3a0ef99566be99ffbce305d5a82866320830edb7
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-07-15 21:11:49 -0400

    minor cleanup and formatting

--- grid.ps1
+++ grid.ps1
@@ -8,42 +8,40 @@ New-Module -ScriptBlock {
 
   $f = New-Object System.Windows.Forms.Form
 
-$f.Text = 'how do we open these stones? '
-$f.AutoSize  = $true 
-$grid = New-Object System.Windows.Forms.DataGrid
-$grid.PreferredColumnWidth = 100 
+  $f.Text = 'how do we open these stones? '
+  $f.AutoSize  = $true 
 
-$System_Drawing_Size = New-Object System.Drawing.Size
-$grid.DataBindings.DefaultDataSourceUpdateMode = 0
-$grid.HeaderForeColor = [System.Drawing.Color]::FromArgb(255,0,0,0)
+  $grid = New-Object System.Windows.Forms.DataGrid
+  $grid.PreferredColumnWidth = 100 
 
-$grid.Name = "dataGrid1"
-$grid.DataMember = ''
-$grid.TabIndex = 0
-$System_Drawing_Point = New-Object System.Drawing.Point
-$System_Drawing_Point.X = 13;
-$System_Drawing_Point.Y = 48 ;
-$grid.Location = $System_Drawing_Point
-$grid.Dock = [System.Windows.Forms.DockStyle]::Fill
+  $grid.DataBindings.DefaultDataSourceUpdateMode = 0
+  $grid.HeaderForeColor = [System.Drawing.Color]::FromArgb(255,0,0,0)
+
+  $grid.Name = 'dataGrid1'
+  $grid.DataMember = ''
+  $grid.TabIndex = 0
+  $grid.Location = New-Object System.Drawing.Point(13,48)
+  $grid.Dock = [System.Windows.Forms.DockStyle]::Fill
  
-$button = New-Object System.Windows.Forms.Button
-$button.Text = 'Open'
-$button.Dock = [System.Windows.Forms.DockStyle]::Bottom
+  $button = New-Object System.Windows.Forms.Button
+  $button.Text = 'Open'
+  $button.Dock = [System.Windows.Forms.DockStyle]::Bottom
  
-$f.Controls.Add( $button )
-$f.Controls.Add( $grid )
+  $f.Controls.Add( $button )
+  $f.Controls.Add( $grid )
 
+  
+  $button.add_Click({$f.Close()})
 
-$button.add_Click({$f.Close()})
-
-function Edit-Object($obj) {
-$grid.DataSource =  $obj
-$f.ShowDialog() | out-null
-$f.refresh()
-$f.dispose()
-}
+  function Edit-Object($obj) {
+    $grid.DataSource =  $obj
+    $f.ShowDialog() | out-null
+    $f.refresh()
+    $f.dispose()
+  }
 
 Export-ModuleMember -Function Edit-Object
+
 } | out-null
  
 
