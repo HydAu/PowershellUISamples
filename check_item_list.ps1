e7e702bd49609d592c3cb98eb8dde7333126c184
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-07-18 18:05:22 -0400

    final version

--- check_item_list.ps1
+++ check_item_list.ps1
@@ -39,8 +39,6 @@ function PromptAuto(
 	[Object] $caller = $null 
 	){
 
-
-
   [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Drawing') 
   [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Collections.Generic') 
   [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Collections') 
@@ -51,115 +49,77 @@ function PromptAuto(
   $f = New-Object System.Windows.Forms.Form 
   $f.Text = $title
 
-  $input_checked_list_box = new-object System.Windows.Forms.CheckedListBox
-  $display_list_box = new-object System.Windows.Forms.ListBox
-  $display_list_box.SuspendLayout()
-  $input_checked_list_box.SuspendLayout()
+  $i = new-object System.Windows.Forms.CheckedListBox
+  $d = new-object System.Windows.Forms.ListBox
+  $d.SuspendLayout()
+  $i.SuspendLayout()
   $f.SuspendLayout()
-  $font =   new-object System.Drawing.Font("Microsoft Sans Serif", 12, [System.Drawing.FontStyle]::Regular, [System.Drawing.GraphicsUnit]::Point, 0);
-  # $font = New-Object System.Drawing.Font('Verdana', 12)
-  $input_checked_list_box.Font = $font
-  $input_checked_list_box.FormattingEnabled = $true;
-  $input_checked_list_box.Items.AddRange(@(
-            "A",
-            "B",
-            "C",
-            "D",
-            "E",
-            "F",
-            "G",
-            "H"));
+  $i.Font = new-object System.Drawing.Font('Microsoft Sans Serif', 11, [System.Drawing.FontStyle]::Regular, [System.Drawing.GraphicsUnit]::Point, 0);
+  $i.FormattingEnabled = $true;
+  $i.Items.AddRange(@(
+            'Lorem',
+            'ipsum',
+            'dolor',
+            'sit',
+            'amet',
+            'consectetur',
+            'adipiscing',
+            'elit'));
 
-  $input_checked_list_box.Location = New-Object System.Drawing.Point(17, 12)
-  $input_checked_list_box.Name = 'inputCheckedListBox'
-  $input_checked_list_box.Size = New-Object System.Drawing.Size(202, 188)
-  $input_checked_list_box.TabIndex = 0
-  $input_checked_list_box.TabStop = $false
- $handler = { 
- param(
-   [Object] $sender, 
-   [System.Windows.Forms.ItemCheckEventArgs ] $eventargs 
-  )
-         $item = $input_checked_list_box.SelectedItem
+  $i.Location = New-Object System.Drawing.Point(17, 12)
+  $i.Name = 'inputCheckedListBox'
+  $i.Size = New-Object System.Drawing.Size(202, 188)
+  $i.TabIndex = 0
+  $i.TabStop = $false
+  $event_handler = {  
+       param(
+            [Object] $sender, 
+            [System.Windows.Forms.ItemCheckEventArgs ] $eventargs 
+         )
+         $item = $i.SelectedItem
          if ( $eventargs.NewValue -eq  [System.Windows.Forms.CheckState]::Checked ) { 
-            $display_list_box.Items.Add( $item );
-} 
-         else {
-            $display_list_box.Items.Remove( $item );
-      }
-}
-  $input_checked_list_box.Add_ItemCheck($handler) 
-
-  # this.inputCheckedListBox.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.inputCheckedListBox_ItemCheck);
-
-  $font = New-Object System.Drawing.Font('Verdana', 12)
-  $display_list_box.Font = $font
-  $display_list_box.FormattingEnabled = $true
-  $display_list_box.ItemHeight = 20;
-  $display_list_box.Location =  New-Object System.Drawing.Point(236, 12);
-  $display_list_box.Name = "displayListBox";
-  $display_list_box.Size = New-Object System.Drawing.Size(190, 184);
-  $display_list_box.TabIndex = 1;
-
-<#
-
-    
-      private void inputCheckedListBox_ItemCheck(object sender, ItemCheckEventArgs e )
-      {
-         string item = inputCheckedListBox.SelectedItem.ToString();
-
-         if ( e.NewValue == CheckState.Checked )
-            displayListBox.Items.Add( item );
-         else
-            displayListBox.Items.Remove( item );
-      }
-#>
-<#
-  $button1.Add_Click({
-
-  $color = ''
-  $shapes = @()     
-  foreach ($o in @($radioButton1, $radioButton2, $radioButton3)){
-  if ($o.Checked){
-      $color = $o.Text}
+            $d.Items.Add( $item );
+         } else {
+            $d.Items.Remove( $item );
+         }
   }
-  foreach ($o in @($checkBox1, $checkBox2, $checkBox3)){
-  if ($o.Checked){
-      $shapes += $o.Text}
+  $i.Add_ItemCheck($event_handler) 
 
-  }
-  $g = [System.Drawing.Graphics]::FromHwnd($f.Handle)
-  $rc = New-Object System.Drawing.Rectangle(150, 50, 250, 250)
-  $brush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::White)
-  $g.FillRectangle($brush, $rc)
-  $font = New-Object System.Drawing.Font('Verdana', 12)
-  $col = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::Black)
-  $str = [String]::Join(';', $shapes )
-  $pos1 = New-Object System.Drawing.PointF(160, 60)
-  $pos2 = New-Object System.Drawing.PointF(160, 80)
+  $d.Font = New-Object System.Drawing.Font('Verdana', 11)
+  $d.FormattingEnabled = $true
+  $d.ItemHeight = 20;
+  $d.Location =  New-Object System.Drawing.Point(236, 12);
+  $d.Name = 'displayListBox';
+  $d.Size = New-Object System.Drawing.Size(190, 184);
+  $d.TabIndex = 1;
 
-  $g.DrawString($color, $font, $col , $pos1)
-  $g.DrawString($str, $font, $col , $pos2)
-  start-sleep 1
+  $b  = New-Object System.Windows.Forms.Button
+  $b.Location = New-Object System.Drawing.Point(8, 280)
+  $b.Name = 'button1'
+  $b.Size = New-Object System.Drawing.Size(112, 32)
+  $b.TabIndex = 4
+  $b.Text = 'Done'
 
-  $caller.Message =  ('color:{0} shapes:{1}' -f $color , $str)
 
-  $f.Close()
+  $b.Add_Click({
+    $shapes = @()    
+    foreach ($o in $d.Items){
+      $shapes += $o
+    } 
+    $caller.Message =  [String]::Join(';', $shapes )
+    $f.Close()
  })
-#>
-   
-  # Form1
-  
+     
   $f.AutoScaleBaseSize = New-Object System.Drawing.Size(5, 13)
   $f.ClientSize = New-Object System.Drawing.Size(408, 317)
-  $f.Controls.AddRange( @(
-     $input_checked_list_box,
-     $display_list_box))
+  $components =  New-Object System.ComponentModel.Container
+
+  $f.Controls.AddRange( @( $i, $d, $b))
 
   $f.Name = 'Form1'
-  $f.Text = 'CheckBox and RadioButton Sample'
-  $input_checked_list_box.ResumeLayout($false)
-  $display_list_box.ResumeLayout($false)
+  $f.Text = 'CheckListBox Sample'
+  $i.ResumeLayout($false)
+  $d.ResumeLayout($false)
 
   $f.ResumeLayout($false)
 
