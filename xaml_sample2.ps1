5fd202305b257d9330a3572a698d77656a83e5cc
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-07-22 20:55:19 -0400

    multi-button functional revision

--- xaml_sample2.ps1
+++ xaml_sample2.ps1
@@ -1,6 +1,19 @@
+
 # origin: http://www.java2s.com/Code/CSharp/Windows-Presentation-Foundation/EmbeddedCodeinWindowxaml.htm
 # origin: http://stackoverflow.com/questions/5863209/compile-wpf-xaml-using-add-type-of-powershell-without-using-powerboots
 #requires -version 2
+$pink = ([System.Windows.Media.Colors]::Pink)
+$white = ([System.Windows.Media.Colors]::White)
+$orange = ([System.Windows.Media.Colors]::Orange)
+$brown = ([System.Windows.Media.Colors]::Brown)
+$colors = @{
+'Steve Buscemi' = $pink;
+'Larry Dimmick' = $white;
+'Quentin Tarantino' = $brown;
+'Tim Roth' = $orange;
+ }
+
+
 Add-Type -AssemblyName PresentationFramework
 [xml]$xaml = 
 @"
@@ -9,54 +22,26 @@ Add-Type -AssemblyName PresentationFramework
     xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
     Title="Row GridSplitter Example">
 
-        <StackPanel Width="Auto" Height="Auto" Grid.Row="0" Grid.Column="0">
+        <StackPanel Height="Auto">
 
-            <Grid >
+            <Grid Height="400">
                 <Grid.RowDefinitions>
-                    <RowDefinition Height="50" />
-                    <RowDefinition Height="50"/>
+                    <RowDefinition Height="50*" />
+                    <RowDefinition Height="50*"/>
                 </Grid.RowDefinitions>
                 <Grid.ColumnDefinitions>
                     <ColumnDefinition/>
                     <ColumnDefinition/>
                 </Grid.ColumnDefinitions>
 
-                <StackPanel Grid.Row="0" Grid.Column="0" 
-                      Background="Red">
-                </StackPanel>
-                <StackPanel Grid.Row="0" Grid.Column="1" 
-                      Background="Green"/>
-
-                <StackPanel Grid.Row="1" Grid.Column="0" 
-                      Background="Tan"/>
-                <StackPanel Grid.Row="1" Grid.Column="1" 
-                      Background="Blue"/>
-
-
-                <GridSplitter Grid.Row="1" 
-                        Grid.ColumnSpan="2" 
-                        HorizontalAlignment="Stretch" 
-                        VerticalAlignment="Top"
-                        Background="Black" 
-                        ShowsPreview="true"
-                        ResizeDirection="Columns"
-                        Height="5"/>
-                <GridSplitter Grid.Column="1" 
-                        Grid.RowSpan="2" 
-                        HorizontalAlignment="Left" 
-                        VerticalAlignment="Stretch"
-                        Background="White" 
-                        ShowsPreview="true"
-                        Width="5"/>
-
-      <Button Background="Transparent" Grid.Column="0" Grid.Row="0" x:Name="button00" HorizontalAlignment="Stretch" VerticalAlignment="Stretch"
-                Content="Click Here" />
-      <Button Background="Transparent" Grid.Column="0" Grid.Row="1" x:Name="button01" HorizontalAlignment="Stretch" VerticalAlignment="Stretch"
-                Content="Click Here" />
-      <Button Background="Transparent" Grid.Column="1" Grid.Row="0" x:Name="button10" HorizontalAlignment="Stretch" VerticalAlignment="Stretch"
-                Content="Click Here" />
-      <Button Background="Transparent" Grid.Column="1" Grid.Row="1" x:Name="button11" HorizontalAlignment="Stretch" VerticalAlignment="Stretch"
-                Content="Click Here" />
+      <Button Background="gray" Grid.Column="0" Grid.Row="0" x:Name="button00" HorizontalAlignment="Stretch" VerticalAlignment="Stretch"
+                Content="Quentin Tarantino" />
+      <Button Background="gray" Grid.Column="0" Grid.Row="1" x:Name="button01" HorizontalAlignment="Stretch" VerticalAlignment="Stretch"
+                Content="Larry Dimmick" />
+      <Button Background="gray" Grid.Column="1" Grid.Row="0" x:Name="button10" HorizontalAlignment="Stretch" VerticalAlignment="Stretch"
+                Content="Steve Buscemi" />
+      <Button Background="gray" Grid.Column="1" Grid.Row="1" x:Name="button11" HorizontalAlignment="Stretch" VerticalAlignment="Stretch"
+                Content="Tim Roth" />
 
             </Grid>
 
@@ -68,7 +53,32 @@ Add-Type -AssemblyName PresentationFramework
 Clear-Host
 $reader=(New-Object System.Xml.XmlNodeReader $xaml)
 $target=[Windows.Markup.XamlReader]::Load($reader)
-$control=$target.FindName("button01")
-$eventMethod=$control.add_click
-$eventMethod.Invoke({$target.Title="Hello $((Get-Date).ToString('G'))"})
+
+foreach ($button in @("button01" , "button00", "button10", "button11")) {
+       $control=$target.FindName($button)
+       $eventMethod=$control.add_click
+$eventMethod.Invoke({
+       param(
+            [Object] $sender, 
+            [System.Windows.RoutedEventArgs ] $eventargs 
+         )
+
+$target.Title=("You will be  Mr. {0} {1} {2}" -f 
+
+$sender.Content.ToString(),
+$sender.Background.ToString(),
+'x'
+# ($colors[$sender.Content.ToString()]).ToString()
+)
+# $colors[[System.Windows.Media.Colors]::Pink).ToString()])
+# $colors[$sender.Background.ToString()]) 
+# ([System.Windows.Media.Colors]::Pink).ToString() )
+$sender.Background = new-Object System.Windows.Media.SolidColorBrush($colors[$sender.Content.ToString()])
+
+# [System.Windows.Media.Colors]::Pink);
+
+})
+}
+
 $target.ShowDialog() | out-null 
+# $colors   |format-table -autosize
