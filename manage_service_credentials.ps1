157e239471ff344ebf7c8f38df0195713616e5a5
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-07-25 19:41:01 -0400

    use Microsoft Get-Credential cmdlet to read NTLM credentials of the user

---
+++ manage_service_credentials.ps1
@@ -0,0 +1,21 @@
+$target_service_name = 'MsDepSvc'
+$domain = $env:USERDOMAIN 
+
+if ($domain -like 'UAT') {
+  $user = '_uatmsdeploy'
+}
+if ($domain -like 'PROD') {
+  $user = '_msdeploy'
+}
+
+clear-host
+
+$target_account = "${domain}\${user}"
+$credential = Get-Credential -username $target_account -message 'Please authenticate' 
+$target_account = $credential.Username 
+$target_password = $credential.GetNetworkCredential().Password
+$DebugPreference = 'Continue'
+
+write-Debug $target_password
+return
+# change the newly installed service (code omitted)
