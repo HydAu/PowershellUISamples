9e73d7f7cd2121d1be77e6cfba1deaa44d47a41c
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-07-26 15:44:21 -0400

    initial_revision

---
+++ tabbed_treeviews.ps1
@@ -0,0 +1,177 @@
+#Copyright (c) 2014 Serguei Kouzmine
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+# http://www.java2s.com/Code/CSharpAPI/System.Windows.Forms/TabControlControlsAdd.htm
+# with sizes adjusted to run the focus demo
+function PromptWithTabs(
+	[String] $title, 
+        [Object] $caller
+	){
+
+        [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+        [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Drawing') 
+
+        $f = New-Object System.Windows.Forms.Form 
+        $f.Text = $title
+
+        $panel2 = new-object System.Windows.Forms.TabPage
+        $panel1 = new-object System.Windows.Forms.TabPage
+        $tab_contol1 = new-object System.Windows.Forms.TabControl
+        $panel2.SuspendLayout()
+        $panel1.SuspendLayout()
+        $tab_contol1.SuspendLayout()
+        $f.SuspendLayout()
+
+        $panel2.Location = new-object System.Drawing.Point(4, 22)
+        $panel2.Name = "tabPage2"
+        $panel2.Padding = new-object System.Windows.Forms.Padding(3)
+        $panel2.Size = new-object System.Drawing.Size(259, 52)
+        $panel2.TabIndex = 1
+        $panel2.Text = "Source Node"
+
+        $l1 = New-Object System.Windows.Forms.Label
+        $l1.Location = New-Object System.Drawing.Size(72,32) 
+        $l1.Size = New-Object System.Drawing.Size(100,16) 
+        $l1.Text = ''         
+
+        $l1.Font = new-object System.Drawing.Font('Microsoft Sans Serif', 8, [System.Drawing.FontStyle]::Regular, [System.Drawing.GraphicsUnit]::Point, 0);
+        $panel2.Controls.Add($l1) 
+
+
+        $t = New-Object  System.Windows.Forms.TreeView 
+  $t.Font  = new-object System.Drawing.Font('Tahoma', 10.25, [System.Drawing.FontStyle]::Regular, [System.Drawing.GraphicsUnit]::Point, [System.Byte]0);
+
+  $i = new-Object System.Windows.Forms.ImageList($components)
+  $i.Images.Add([System.Drawing.SystemIcons]::Application)
+  $t.ImageList = $i
+
+
+  $t.Anchor = ((([System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Bottom) `
+        -bor [System.Windows.Forms.AnchorStyles]::Left) `
+        -bor [System.Windows.Forms.AnchorStyles]::Right)
+  $t.ImageIndex = -1
+  $t.Location = new-object System.Drawing.Point(4, 5)
+  $t.Name = "treeFood"
+  $t.SelectedImageIndex = -1
+  $t.Size = new-object System.Drawing.Size(284, 256)
+  $t.TabIndex = 1;
+  $panel2.Controls.AddRange(@($t)) 
+
+ $panel2.add_VisibleChanged({
+   param(
+    [Object] $sender, 
+    [System.EventArgs] $eventargs 
+   )
+    
+    $node = $t.Nodes.Add("Fruits") 
+    $apple = $node.Nodes.Add("Apple") 
+    $apple.ImageIndex  = 1
+
+    $node.Nodes.Add("Peach") 
+    
+    $node = $t.Nodes.Add("Vegetables")
+    $tomato = $node.Nodes.Add("Tomato")
+    $tomato.ImageIndex  = 2 
+    $node.Nodes.Add("Eggplant")
+     
+     
+})
+
+
+        $panel1.Location = new-object System.Drawing.Point(4, 22)
+        $panel1.Name = "tabPage1"
+        $panel1.Padding = new-object System.Windows.Forms.Padding(3)
+        $panel1.Size = new-object System.Drawing.Size(259, 52)
+        $panel1.TabIndex = 0
+        $panel1.Text = "Destination Node"
+
+
+        $tab_contol1.Controls.Add($panel1)
+        $tab_contol1.Controls.Add($panel2)
+
+        $tab_contol1.Location = new-object System.Drawing.Point(13, 13)
+        $tab_contol1.Name = "tabControl1"
+        $tab_contol1.SelectedIndex = 1
+        $tab_contol1.Size = new-object System.Drawing.Size(267, 88)
+        $tab_contol1.TabIndex = 0
+        
+        $f.AutoScaleBaseSize = new-object System.Drawing.Size(5, 13)
+        $f.ClientSize = new-object System.Drawing.Size(292, 108)
+        $f.Controls.Add($tab_contol1)
+        $panel2.ResumeLayout($false)
+        $panel2.PerformLayout()
+        $panel1.ResumeLayout($false)
+        $tab_contol1.ResumeLayout($false)
+        $f.ResumeLayout($false)
+        # $f.ActiveControl = $textbox1
+
+        $f.Topmost = $true
+
+
+        $f.Add_Shown( { $f.Activate() } )
+        $f.KeyPreview = $True
+
+
+        [Void] $f.ShowDialog([Win32Window ] ($caller) )
+
+        $f.Dispose() 
+}
+
+Add-Type -TypeDefinition @"
+using System;
+using System.Windows.Forms;
+public class Win32Window : IWin32Window
+{
+    private IntPtr _hWnd;
+    private int _data;
+    private string _message;
+
+    public int Data
+    {
+        get { return _data; }
+        set { _data = value; }
+    }
+
+
+    public string Message
+    {
+        get { return _message; }
+        set { _message = value; }
+    }
+
+    public Win32Window(IntPtr handle)
+    {
+        _hWnd = handle;
+    }
+
+    public IntPtr Handle
+    {
+        get { return _hWnd; }
+    }
+}
+
+"@ -ReferencedAssemblies 'System.Windows.Forms.dll'
+
+$DebugPreference = 'Continue'
+$title = 'Enter Message' 
+$caller = New-Object Win32Window -ArgumentList ([System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle)
+
+PromptWithTabs -title $title -caller $caller
+write-debug ("Message is : {0} " -f  $caller.Message )
