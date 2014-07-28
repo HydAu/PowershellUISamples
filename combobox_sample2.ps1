4b01db38faa43568356334936bab091b62427766
sergueik <sergueik@ubuntu.(none)>
2014-07-28 18:17:10 -0400

    semi-fixed XAML combobox example and a replica of the plain windows formss example

--- combobox_sample2.ps1
+++ combobox_sample2.ps1
@@ -29,11 +29,11 @@ Add-Type -AssemblyName PresentationFramework
   Title="Window1" Height="300" Width="408">
     <StackPanel>
     <ComboBox Name="comboBox" IsEditable="True" Margin="5">
-            <ComboBoxItem Content="ComboBox Item 1" Selected="ComboBoxItem_Selected" />
-            <ComboBoxItem Content="ComboBox Item 2" Selected="ComboBoxItem_Selected" />
-            <ComboBoxItem Content="ComboBox Item 3" Selected="ComboBoxItem_Selected" IsSelected="True"/>
-            <ComboBoxItem Content="ComboBox Item 4" Selected="ComboBoxItem_Selected" />
-            <ComboBoxItem Content="ComboBox Item 5" Selected="ComboBoxItem_Selected" />
+            <ComboBoxItem Name="Item_1" Content="ComboBox Item 1"/>
+            <ComboBoxItem Name="Item_2" Content="ComboBox Item 2"/>
+            <ComboBoxItem Name="Item_3" Content="ComboBox Item 3" IsSelected="True"/>
+            <ComboBoxItem Name="Item_4" Content="ComboBox Item 4" />
+            <ComboBoxItem Name="Item_5" Content="ComboBox Item 5"/>
         </ComboBox>
     </StackPanel>
 </Window>
@@ -42,13 +42,34 @@ Add-Type -AssemblyName PresentationFramework
 Clear-Host
 $reader=(New-Object System.Xml.XmlNodeReader $xaml)
 $target=[Windows.Markup.XamlReader]::Load($reader)
-# $control1=$target.FindName("comboBox")
 
-$control2=$target.FindName("comboBoxItem")
-$eventMethod=$control1.add_Selected
-$eventMethod.Invoke({
+
+foreach ($item in ("Item_1", "Item_5", "Item_2","Item_3","Item_4") ){
+
+write-output ('Wiring {0} ?' -f $item )
+$control2 = $target.FindName($item)
+$eventMethod2 = $control2.add_Selected
+$eventMethod2.Invoke( {
 param ([object] $sender, 
-       [System.Windows.SelectionChangedEventArgs] $e )
-$target.Title="Hello $((Get-Date).ToString('G'))"})
+[System.Windows.RoutedEventArgs] $e )
+# $target.Title="Hello $((Get-Date).ToString('G'))"
+$target.Title=( "Hello {0} " -f $sender.GetType().ToString() )
+write-output ( "Hello {0} " -f $sender.ToString() )
+})
+$control2 = $null 
+}
+
+$control1=$target.FindName("comboBox")
+$eventMethod1=$control1.add_SelectionChanged
+$eventMethod1.Invoke({
+param ([object] $sender, 
+       [System.Windows.Controls.SelectionChangedEventArgs] $e )
+write-host 'x'
+ write-host ('Event routed from {0} to Powershell'  -f $sender.ToString())
+
+})
+
+
+
 
 $target.ShowDialog() | out-null 
