8150ec82cab58b37cee02fb6c7d286d4fe0dad52
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-07-30 21:49:11 -0400

    painting arrows, polishing the behavior

--- xaml_polygon_events.ps1
+++ xaml_polygon_events.ps1
@@ -13,33 +13,40 @@ Add-Type -AssemblyName PresentationFramework
   <Polygon Points="0,0 0,30 0,10 30,10 30,-10 45,10 30,30 30,20 0,20 0,0 30,0 30,10 0,10" 
     Fill="Blue" Name="Polygon1" Canvas.Left="40" Canvas.Top="30" Canvas.ZIndex="40" />
   <Polygon Points="0,0 0,30 0,10 30,10 30,-10 45,10 30,30 30,20 0,20 0,0 30,0 30,10 0,10" 
-    Fill="Violet" Name="Polygon2" Canvas.Left="70" Canvas.Top="30" Canvas.ZIndex="30"/>
+    Fill="Green" Name="Polygon2" Canvas.Left="70" Canvas.Top="30" Canvas.ZIndex="30"/>
   <Polygon Points="0,0 0,30 0,10 30,10 30,-10 45,10 30,30 30,20 0,20 0,0 30,0 30,10 0,10" 
-    Fill="Brown" Name="Polygon3" Canvas.Left="100" Canvas.Top="30" Canvas.ZIndex="20"/>
+    Fill="Red" Name="Polygon3" Canvas.Left="100" Canvas.Top="30" Canvas.ZIndex="20"/>
 
 </Canvas>
 </Window>
 "@
 Clear-Host
-$reader=(New-Object System.Xml.XmlNodeReader $xaml)
-$target=[Windows.Markup.XamlReader]::Load($reader)
-$canvas=$target.FindName("Canvas1")
+$reader = (New-Object System.Xml.XmlNodeReader $xaml)
+$target = [Windows.Markup.XamlReader]::Load($reader)
+$canvas = $target.FindName("Canvas1")
 
+function save_orig{
+  
+  $control = $target.FindName('Polygon1')
+  write-output $control.Fill
+  write-output [System.Windows.Controls.Canvas]::SetZIndex($control)
+}
 function restore_orig{
 
-$control = $target.FindName('Polygon1')
-$control.Fill =  new-Object System.Windows.Media.SolidColorBrush([System.Windows.Media.Colors]::Blue)
-[System.Windows.Controls.Canvas]::SetZIndex($control,[Object] 40)
+  $control = $target.FindName('Polygon1')
+  $control.Fill =  new-Object System.Windows.Media.SolidColorBrush([System.Windows.Media.Colors]::Blue)
+  [System.Windows.Controls.Canvas]::SetZIndex($control,[Object] 40)
 
-$control = $target.FindName('Polygon2')
-$control.Fill =  new-Object System.Windows.Media.SolidColorBrush([System.Windows.Media.Colors]::Violet)
-[System.Windows.Controls.Canvas]::SetZIndex($control,[Object] 30)
+  $control = $target.FindName('Polygon2')
+  $control.Fill =  new-Object System.Windows.Media.SolidColorBrush([System.Windows.Media.Colors]::Green)
+  [System.Windows.Controls.Canvas]::SetZIndex($control,[Object] 30)
 
-$control = $target.FindName('Polygon3')
-$control.Fill =  new-Object System.Windows.Media.SolidColorBrush([System.Windows.Media.Colors]::Brown)
-[System.Windows.Controls.Canvas]::SetZIndex(($target.FindName('Polygon3')),[Object] 20)
+  $control = $target.FindName('Polygon3')
+  $control.Fill =  new-Object System.Windows.Media.SolidColorBrush([System.Windows.Media.Colors]::Red)
+  [System.Windows.Controls.Canvas]::SetZIndex(($target.FindName('Polygon3')),[Object] 20)
 
 }
+
 $handler = {
 param (
     [Object]  $sender, 
@@ -48,22 +55,21 @@ param (
 
 restore_orig
 
-# highlight $sender
-$sender.Fill = new-Object System.Windows.Media.SolidColorBrush([System.Windows.Media.Colors]::Green)
+# Highlight $sender
+$sender.Fill = new-Object System.Windows.Media.SolidColorBrush([System.Windows.Media.Colors]::Orange)
+# uncomment to reveal a distortion
+# $sender.Stroke = new-Object System.Windows.Media.SolidColorBrush([System.Windows.Media.Colors]::Black)
 
-# bring $sender to front
-
+# Bring $sender to front
 [System.Windows.Controls.Canvas]::SetZIndex($sender,[Object]100)
-$name = $sender.Name
 $target.Title="Hello $($sender.Name)"
 }
-
+save_orig
 foreach ($item in ("Polygon1", "Polygon2", "Polygon3") ){
-   $control = $target.FindName($item)
-   $control | get-member | out-null
-   $eventMethod=$control.add_MouseDown
+  $control = $target.FindName($item)
+  $eventMethod=$control.add_MouseDown
   $eventMethod.Invoke( $handler )
-  $combobox_item_control = $null 
+  $control = $null 
 }
 
 $eventMethod.Invoke($handler)
