4380a40e5fcfedba92fe19f819d21980d73b99b8
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-08-05 20:33:21 -0400

    recovered poor merge

--- xaml_tooltip_management.ps1
+++ xaml_tooltip_management.ps1
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-#Copyright (c) 2014 Serguei Kouzmine
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-#The above copyright notice and this permission notice shall be included in
-#all copies or substantial portions of the Software.
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-#THE SOFTWARE.
 
-
-=======
 #Copyright (c) 2014 Serguei Kouzmine
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -34,7 +15,6 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #THE SOFTWARE.
->>>>>>> a8924da3a3089f0c339f3ddd4d522c36cc886733
 #requires -version 2
 Add-Type -AssemblyName PresentationFramework
 # http://stackoverflow.com/questions/8343767/how-to-get-the-current-directory-of-the-cmdlet-being-executed
@@ -178,8 +158,4 @@ While (-Not $handle.IsCompleted) {
     }
 }
 $run_script.EndInvoke($handle)
-<<<<<<< HEAD
-
-=======
->>>>>>> a8924da3a3089f0c339f3ddd4d522c36cc886733
 $rs.Close()
