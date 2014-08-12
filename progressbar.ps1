d8cf0679dfd9ef55dcb1bf45d6842851654f68e7
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-08-12 13:49:03 -0400

    first vesion of progressbar

---
+++ progressbar.ps1
@@ -0,0 +1,281 @@
+#Copyright (c) 2014 Serguei Kouzmine
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+#Copyright (c) 2014 Serguei Kouzmine
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+Add-Type -TypeDefinition @"
+
+// "
+// http://www.java2s.com/Code/CSharp/GUI-Windows-Form/ProgressBarHost.htm
+
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+
+namespace ProgressBarHost
+{
+
+
+    public class Progress : System.Windows.Forms.UserControl
+    {
+        internal System.Windows.Forms.Label lblProgress;
+        internal System.Windows.Forms.ProgressBar Bar;
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.Container components = null;
+
+        public Progress()
+        {
+            // This call is required by the Windows.Forms Form Designer.
+            InitializeComponent();
+
+            // TODO: Add any initialization after the InitForm call
+
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing )
+            {
+                if(components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Component Designer generated code
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.Bar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.Anchor = ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+                | System.Windows.Forms.AnchorStyles.Right);
+            this.lblProgress.Location = new System.Drawing.Point(5, 46);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(152, 16);
+            this.lblProgress.TabIndex = 3;
+            this.lblProgress.Text = "0% Done";
+            this.lblProgress.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Bar
+            // 
+            this.Bar.Anchor = ((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+                | System.Windows.Forms.AnchorStyles.Right);
+            this.Bar.Location = new System.Drawing.Point(5, 6);
+            this.Bar.Name = "Bar";
+            this.Bar.Size = new System.Drawing.Size(154, 32);
+            this.Bar.TabIndex = 2;
+            // 
+            // Progress
+            // 
+            this.Controls.AddRange(new System.Windows.Forms.Control[] {
+                                                                          this.lblProgress,
+                                                                          this.Bar});
+            this.Name = "Progress";
+            this.Size = new System.Drawing.Size(164, 68);
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+        
+        [Description("The current value (between 0 and Maximum) which sets the position of the progress bar"), 
+        Category("Behavior"), DefaultValue(0)]
+        public int Value
+        {
+            get
+            {
+                return Bar.Value;
+            }
+            set
+            {
+                Bar.Value = value;
+                UpdateLabel();
+            }
+        }
+
+        public int Maximum
+        {
+            get
+            {
+                return Bar.Maximum;
+            }
+            set
+            {
+                Bar.Maximum = value;
+            }
+        }
+
+        public int Step
+        {
+            get
+            {
+                return Bar.Step;
+            }
+            set
+            {
+                Bar.Step = value;
+            }
+        }
+
+        public void PerformStep()
+        {
+            Bar.PerformStep();
+            UpdateLabel();
+        }
+
+        private void UpdateLabel()
+        {
+            lblProgress.Text = (Math.Round((decimal)(Bar.Value * 100) /
+                Bar.Maximum)).ToString();
+            lblProgress.Text += "% Done";
+        }
+    }
+
+
+
+}
+
+"@ -ReferencedAssemblies 'System.Windows.Forms.dll', 'System.Drawing.dll', 'System.Data.dll', 'System.ComponentModel.dll'
+
+Add-Type -TypeDefinition @"  
+
+// "
+using System;
+using System.Windows.Forms;
+public class Win32Window : IWin32Window
+{
+    private IntPtr _hWnd;
+    private int _data;
+
+    public int Data
+    {
+        get { return _data; }
+        set { _data = value; }
+    }
+
+    public Win32Window(IntPtr handle)
+    {
+        _hWnd = handle;
+    }
+
+    public IntPtr Handle
+    {
+        get { return _hWnd; }
+    }
+}
+
+"@ -ReferencedAssemblies 'System.Windows.Forms.dll'
+
+
+function Progressbar(
+	[String] $title, 
+	[String] $message,
+        [Object] $caller
+	){
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Drawing') 
+
+
+$f = New-Object System.Windows.Forms.Form 
+$f.Text = $title
+
+
+$f.Size = New-Object System.Drawing.Size(650,120) 
+$f.StartPosition = 'CenterScreen'
+$components = new-object System.ComponentModel.Container
+$progress_status = new-object ProgressBarHost.Progress
+
+$progress_status.Location = new-object System.Drawing.Point(12, 8) 
+$progress_status.Name = "status"
+$progress_status.Size = new-object System.Drawing.Size(272, 88)
+$progress_status.TabIndex = 0
+
+$b1 = New-Object System.Windows.Forms.Button
+$b1.Location = New-Object System.Drawing.Size(140, 152)
+$b1.Size = New-Object System.Drawing.Size(92, 24)
+$b1.Text = 'forward'
+$b1.Add_Click({ $progress_status.PerformStep(); 
+if ($progress_status.Maximum -eq $progress_status.Value)
+            {
+                $b1.Enabled = false;
+            }
+
+})
+$f.Controls.Add($b1)
+$f.AutoScaleBaseSize = new-object System.Drawing.Size(5, 14)
+$f.ClientSize = new-object System.Drawing.Size(292, 194)
+$f.Controls.Add($progress_status ) 
+$f.Topmost = $True
+
+
+$caller.Data = $RESULT_CANCEL;
+$f.Add_Shown( { $f.Activate() } )
+
+[Void] $f.ShowDialog([Win32Window ] ($caller) )
+
+$f.Dispose() 
+}
+
+
+<#
+$env:path="${env:path};C:\Windows\Microsoft.NET\Framework\v4.0.30319"
+#>
+
+
+$caller = New-Object Win32Window -ArgumentList ([System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle)
+
+Progressbar -title $title -message $message -caller $caller
+$result = $caller.Data 
+write-debug ("Result is : {0} ({1})" -f $Readable.Item($result) , $result )
