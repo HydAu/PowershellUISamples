00f8852a4a2c2e10b7a7c6b78cb080e4f7e78213
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-08-22 14:01:27 -0400

    resolve the name and the path correctly

--- assembly_path_finder.ps1
+++ assembly_path_finder.ps1
@@ -1,11 +1,27 @@
-# Microsoft.VisualStudio.TestTools.UnitTesting
-# registry  Visual_Studio_Team_Test_Agent_x86
-# Microsoft Visual Studio Test Agent 2010 - ENU
-# HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{6088FCFB-2FA4-3C74-A1D1-F687C5F14A0D}
-# HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Components\15D46E6F7EE9FA540A32DC953FDEFB9E
-# string assert class
-# note CodedUI  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\VisualStudio\10.0\Packages\{6077292c-6751-4483-8425-9026bc0187b6}
+#Copyright (c) 2014 Serguei Kouzmine
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+
+# for Microsoft.VisualStudio.TestTools.UnitTesting.dll
 # http://msdn.microsoft.com/en-us/library/microsoft.visualstudio.testtools.unittesting.assert.aspx
+# note CodedUI  HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\VisualStudio\10.0\Packages\{6077292c-6751-4483-8425-9026bc0187b6}
 
 function read_registry{
   param ([string] $registry_path = '/HKEY_LOCAL_MACHINE/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall' , 
@@ -30,8 +46,80 @@ $result
 
 }
 
-# for Microsoft.VisualStudio.TestTools.UnitTesting.dll
-write-output  ( read_registry -registry_path '/HKEY_LOCAL_MACHINE/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall' -package_name '{6088FCFB-2FA4-3C74-A1D1-F687C5F14A0D}' )
+
+
+# http://stackoverflow.com/questions/8343767/how-to-get-the-current-directory-of-the-cmdlet-being-executed
+function Get-ScriptDirectory
+{
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value;
+    if($Invocation.PSScriptRoot)
+    {
+        $Invocation.PSScriptRoot;
+    }
+    Elseif($Invocation.MyCommand.Path)
+    {
+        Split-Path $Invocation.MyCommand.Path
+    }
+    else
+    {
+        $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf("\"));
+    }
+}
+
+# http://poshcode.org/1942
+function Assert {
+  [CmdletBinding()]
+  param(
+   [Parameter(Position=0,ParameterSetName='Script', Mandatory=$true)]
+   [ScriptBlock]$Script,
+   [Parameter(Position=0,ParameterSetName='Condition', Mandatory=$true)]
+   [bool]$Condition,
+   [Parameter(Position=1,Mandatory=$true)]
+   [string]$message )
+     
+  $message = "ASSERT FAILED: $message"
+  if($PSCmdlet.ParameterSetName -eq 'Script') {
+    try {
+      $ErrorActionPreference = 'STOP'
+      $success = &$Script
+    } catch {
+      $success = $false
+      $message = "$message`nEXCEPTION THROWN: $($_.Exception.GetType().FullName)"        
+    }
+  } 
+  if($PSCmdlet.ParameterSetName -eq 'Condition') {
+    try {
+      $ErrorActionPreference = 'STOP'
+      $success = $Condition
+    } catch {
+      $success = $false
+      $message = "$message`nEXCEPTION THROWN: $($_.Exception.GetType().FullName)"        
+    }
+  } 
+
+  if(!$success) {
+    throw $message
+  }
+}
+
+$shared_assemblies =  @(
+    'Microsoft.VisualStudio.QualityTools.UnitTestFramework.dll'
+)
+
+$env:SHARED_ASSEMBLIES_PATH =  'c:\developer\sergueik\csharp\SharedAssemblies'
+$env:SCREENSHOT_PATH =  'C:\developer\sergueik\powershell_ui_samples' 
+
+
+
+
+$shared_assemblies_path = (  "{0}\{1}" -f   ( read_registry -registry_path '/HKEY_LOCAL_MACHINE/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall' -package_name '{6088FCFB-2FA4-3C74-A1D1-F687C5F14A0D}' ) , 'Common7\IDE\PublicAssemblies' ) 
+$screenshot_path = $env:SCREENSHOT_PATH
+pushd $shared_assemblies_path
+$shared_assemblies | foreach-object { Unblock-File -Path $_ ; Add-Type -Path  $_ } 
+popd
+
+[Microsoft.VisualStudio.TestTools.UnitTesting.Assert]::AreNotEqual('true', [string]"false") 
+
 # TODO for nunit.framework.dll 
 # HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-21-52832475-59735490-1501187911-186867\Components\01A9DBD987BE1B05F8AD3F77F95AAEAA
 # HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-21-52832475-59735490-1501187911-186867\Components\2FE4EB3A1F5407F5CA3307D9E2B23146
