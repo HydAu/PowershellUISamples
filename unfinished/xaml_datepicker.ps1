43997e3e7b3c07c47a53449e5fe19d97453c2110
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-09-01 21:41:02 -0400

    initial version without timepicker - there isn't one in default WPF 4.0

--- unfinished/xaml_datepicker.ps1
+++ unfinished/xaml_datepicker.ps1
@@ -2,11 +2,18 @@
 # among other things.
 # http://www.c-sharpcorner.com/UploadFile/mahesh/wpf-datepicker/
 # http://www.codeproject.com/Tips/399140/WPF-DatePicker-Background-Fix
+# http://www.codeproject.com/Articles/237011/CREATING-A-CUSTOM-TIMEPICKER-CONTROL-IN-WPF
+# WPF 4.0 doesn't provide a DateTimePicker out of the box.
+# http://jobijoy.blogspot.com/2007/10/time-picker-user-control.html
+
 Add-Type -AssemblyName PresentationFramework
+# WPF 4.0 doesn't provide a DateTimePicker out of the box. 
+# Add-Type -AssemblyName System.Windows.Controls.Toolkit
+# Add-Type -AssemblyName System.Windows.Controls
 [xml]$xaml =
 @"
 <?xml version="1.0"?>
-<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml" Title="DatePicker Background Sample" Height="80" Width="150">
+<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml" xmlns:toolkit="http://schemas.microsoft.com/winfx/2006/xaml/presentation/toolkit" Title="DatePicker Background Sample" Height="80" Width="150">
   <Grid VerticalAlignment="Center">
     <Grid.ColumnDefinitions>
       <ColumnDefinition/>
@@ -18,12 +25,16 @@ Add-Type -AssemblyName PresentationFramework
     </Grid.RowDefinitions>
     <Label Content="Pick Date" Grid.Column="0" Grid.Row="0" Margin="1"/>
     <DatePicker x:Name="datePicker" Grid.Column="1" Grid.Row="0" Margin="2" Background="#FFC000"/>
+    <!-- 
+    <toolkit:TimePicker x:Name="TimePicker" Header="Arrival Time" Grid.Column="1" Grid.Row="1" Margin="2" Background="#FFC000"/>
+     -->
   </Grid>
 </Window>
 "@
 # http://msdn.microsoft.com/en-us/library/windows/apps/xaml/dn308514.aspx
 # 
-Clear-Host
+# Clear-Host
+$result = $null
 $reader = (New-Object System.Xml.XmlNodeReader $xaml)
 $target = [Windows.Markup.XamlReader]::Load($reader)
 $control = $target.FindName('datePicker')
@@ -33,10 +44,12 @@ $handler = {
   param(
     [object]$sender,
     [System.Windows.Controls.SelectionChangedEventArgs]$e)
-    write-host $sender.SelectedDate.ToString()
+    $result =  $sender.SelectedDate.ToString()
 }
 $eventMethod.Invoke($handler )
-$result = $target.ShowDialog() 
+$target.ShowDialog()  
+# | Out-Null
+
 write-output $result
 
 <#
