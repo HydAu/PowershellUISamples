6b8a56235c670eace89f4ad7e76112416d825c94
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-09-08 19:26:52 -0400

    initial revision

--- selenium/selenum_console_addon.ps1
+++ selenium/selenum_console_addon.ps1
@@ -1,4 +1,3 @@
-
 #Copyright (c) 2014 Serguei Kouzmine
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -38,21 +37,42 @@ function Get-ScriptDirectory
 $shared_assemblies = @(
   "WebDriver.dll",
   "WebDriver.Support.dll",
-  "Selenium.WebDriverBackedSelenium.dll",
+  # "Selenium.WebDriverBackedSelenium.dll",
+  # TODO - resolve dependencies
   'nunit.core.dll',
   'nunit.framework.dll'
 
 )
+<#
+
+Add-Type : Could not load file or assembly 
+'file:///C:\developer\sergueik\csharp\SharedAssemblies\WebDriver.dll' 
+or one of its dependencies. This assembly is built by a runtime newer than the currently loaded runtime and cannot be loaded.
+
+Add-Type : Could not load file or assembly 
+'file:///C:\developer\sergueik\csharp\SharedAssemblies\nunit.framework.dll' or one of its dependencies. 
+Operation is not supported. (Exception from HRESULT: 0x80131515) 
+
+use fixw2k3.ps1
+
+Add-Type : Unable to load one or more of the requested types. Retrieve the LoaderExceptions property for more information.
+#>
 
 $env:SHARED_ASSEMBLIES_PATH = "c:\developer\sergueik\csharp\SharedAssemblies"
 
 $shared_assemblies_path = $env:SHARED_ASSEMBLIES_PATH
 pushd $shared_assemblies_path
-$shared_assemblies | ForEach-Object { Unblock-File -Path $_; Add-Type -Path $_ }
+$shared_assemblies | ForEach-Object { 
+ if ($host.Version.Major -gt 2){
+   Unblock-File -Path $_;
+ }
+ write-output $_
+ Add-Type -Path $_ 
+ }
 popd
 
-
-
+$verificationErrors = New-Object System.Text.StringBuilder
+# use Default Web Site to host the page. Enable Directory Browsing.
 $phantomjs_executable_folder = "C:\tools\phantomjs"
 if ($PSBoundParameters["browser"]) {
   try {
@@ -61,11 +81,13 @@ if ($PSBoundParameters["browser"]) {
     $connection.Close()
   } catch {
     Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\hub.cmd"
-    Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\node.cmd"
+    Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\node_ie.cmd"
     Start-Sleep -Seconds 10
   }
   $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Firefox()
-#   $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Chrome()
+#  $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Chrome()
+#  $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::InternetExplorer()
+
   $uri = [System.Uri]("http://127.0.0.1:4444/wd/hub")
   $selenium = New-Object OpenQA.Selenium.Remote.RemoteWebDriver ($uri,$capability)
 } else {
@@ -78,17 +100,55 @@ if ($PSBoundParameters["browser"]) {
   $options.AddAdditionalCapability("phantomjs.executable.path",$phantomjs_executable_folder)
 }
 
-
-
-$verificationErrors = New-Object System.Text.StringBuilder
-# use Default Web Site to host the page. Enable Directory Browsing.
-$baseURL = "http://localhost/jOrgChart-master2/example.html"
-
-
-$selenium.Navigate().GoToUrl($baseURL )
+# $selenium.Navigate().GoToUrl("file:///C:/developer/sergueik/powershell_ui_samples/selenium/popup.html" )
+$baseURL = 'file:///C:/developer/sergueik/powershell_ui_samples/external/grid-console.html'
+$selenium.Navigate().GoToUrl('file:///C:/developer/sergueik/powershell_ui_samples/external/grid-console.html')
 $selenium.Navigate().Refresh()
 $selenium.Manage().Window.Maximize()
 
+
+$selenium.Navigate().Refresh()
+$selenium.Manage().Window.Maximize()
+[OpenQA.Selenium.Remote.RemoteWebElement]$proxy_id = $selenium.FindElement([OpenQA.Selenium.By]::CssSelector("p[class='proxyid']"))
+write-host $proxy_id.Text
+# jquery works in FireBug but not Selenium
+[string]$script = @"
+return
+`$("p[class='proxyid']").innerHTML
+"@
+
+write-output $script
+[string]$result = ([OpenQA.Selenium.IJavaScriptExecutor]$selenium).ExecuteScript($script)
+write-host $result
+# http://www.w3schools.com/xpath/xpath_examples.asp
+$script = @"
+function SelectSingleNode(xmlDoc, elementPath) {
+      if (xmlDoc.evaluate) {
+        var nodes = xmlDoc.evaluate(elementPath, xmlDoc, null, XPathResult.ANY_TYPE, null);
+        var results = nodes.iterateNext();
+        return results;
+      }
+      else
+        return xmlDoc.selectSingleNode(elementPath); 
+    }
+
+
+var result = SelectSingleNode(window.document, "//p[@class='proxyid']") ;
+
+var p = document.createElement('div');
+var t = document.createTextNode('DNS Data from Powershell');
+p.appendChild(t); 
+// t.style.background-color = '#f00'; 
+// result.parentNode.appendChild(p); 
+result.appendChild(p); 
+"@
+[string]$result = ([OpenQA.Selenium.IJavaScriptExecutor]$selenium).ExecuteScript($script)
+
+
+start-sleep 4
+[OpenQA.Selenium.Remote.RemoteWebElement]$proxy_id = $selenium.FindElement([OpenQA.Selenium.By]::CssSelector("p[class='proxyid']"))
+write-host $proxy_id.Text
+start-sleep 6
 
 <#
 
