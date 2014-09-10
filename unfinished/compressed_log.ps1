71c61b4589768939487b37976333991fcda4049a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-09-10 15:53:04 -0400

    initial version

--- unfinished/compressed_log.ps1
+++ unfinished/compressed_log.ps1
@@ -16,8 +16,19 @@ Param(
 	[System.Management.Automation.PSReference] $ref_read
 	 )
      [System.IO.Stream]$output  = $ref_output.Value
+
      [System.Byte[]]$buffer = $ref_buffer.Value
-     [Int32]$read = $ref_read.Value
+     [string] $string_buffer  = [System.Text.Encoding]::Default.GetString($buffer );
+     [char[]] $newlines = @( 0xd, 0xa )
+     $lines =  $string_buffer.split($newlines)
+# http://msdn.microsoft.com/en-us/library/ms228388.aspx
+# The following will not work :"
+#      $lines =  $string_buffer -split '`l'
+#     write-output "`n---`n${string_buffer}`n---`n" 
+     $lines.count
+     start-sleep 1;
+ 
+     [Int32]$read  =$ref_read.Value
 	$output.Write($buffer, 0, $read)	
 }
 
@@ -26,14 +37,15 @@ $input = New-Object System.IO.FileStream $inFile, ([IO.FileMode]::Open), ([IO.Fi
 $output = New-Object System.IO.FileStream $outFile, ([IO.FileMode]::Create), ([IO.FileAccess]::Write), ([IO.FileShare]::None)
 $gzipStream = New-Object System.IO.Compression.GzipStream $input, ([IO.Compression.CompressionMode]::Decompress)
 
-$buffer = New-Object byte[](1024)
+$buffer = New-Object byte[](4096)
 while($true){
-	$read = $gzipstream.Read($buffer, 0, 1024)
+	$read = $gzipstream.Read($buffer, 0, 4096)
 	if ($read -le 0){break}
-		write-debug ('{0}={1}' -f 'output', $output.GetType() )
-		write-debug ('{0}={1}' -f 'buffer', $buffer.GetType())
-		write-debug ('{0}={1}' -f 'read', $read.GetType())
-    	process  ([ref]$output ) ([ref]$buffer )  ([ref]$read ) 
+write-debug ('{0}={1}' -f 'output', $output.GetType() )
+write-debug ('{0}={1}' -f 'buffer', $buffer.GetType())
+write-debug ('{0}={1}' -f 'read', $read.GetType())
+	
+	process  ([ref]$output ) ([ref]$buffer )  ([ref]$read ) 
 
 	}
 
