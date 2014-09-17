4398a74a100ee9e879097b9df920db29c4a60b61
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-09-17 12:16:19 -0400

    moved function to the top of the script

--- selenium/profile_management.ps1
+++ selenium/profile_management.ps1
@@ -1,3 +1,17 @@
+# http://stackoverflow.com/questions/8343767/how-to-get-the-current-directory-of-the-cmdlet-being-executed
+function Get-ScriptDirectory {
+  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+  if ($Invocation.PSScriptRoot) {
+    $Invocation.PSScriptRoot
+  }
+  elseif ($Invocation.MyCommand.Path) {
+    Split-Path $Invocation.MyCommand.Path
+  } else {
+    $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf(""))
+  }
+}
+
+
 Write-Output "# 1. Definitions "
 
 
@@ -9,21 +23,21 @@ $profile_path_localappdata = ('{0}\{1}' -f $env:LOCALAPPDATA,$profile_path_relat
 
 Write-Output "# 2. Locate default profile name"
 
-pushd $profile_path_appdata 
+pushd $profile_path_appdata
 cd 'Profiles'
-$directory_name_current =  (get-childitem -path '.' -filter '*default' | select-object -expandproperty Name )
+$directory_name_current = (Get-ChildItem -Path '.' -Filter '*default' | Select-Object -ExpandProperty Name)
 
 
-if ($directory_name_current -eq $null ){
-   throw  ('The profile directory was not found in {0}\Profiles' -f $profile_path_appdata  )
+if ($directory_name_current -eq $null) {
+  throw ('The profile directory was not found in {0}\Profiles' -f $profile_path_appdata)
 }
 
-$display_name = 'default' 
+$display_name = 'default'
 # TODO Regexp 
-$new_name = $directory_name_current -replace '^.+\.' ,'xxxxx.' 
+$new_name = $directory_name_current -replace '^.+\.','xxxxx.'
 # Use C escapes not Powershell escapes
 popd
-Write-Output ("default profile name: {0}"  -f  $directory_name_current )
+Write-Output ("default profile name: {0}" -f $directory_name_current)
 
 Write-Output "# 3.  Compose new profiles.ini"
 $new_profile = @"
@@ -46,23 +60,11 @@ Default=1
 
 "@
 
-write-output $new_profile 
+Write-Output $new_profile
 
 
 Write-Output "# 3.  Dump copy"
 
-# http://stackoverflow.com/questions/8343767/how-to-get-the-current-directory-of-the-cmdlet-being-executed
-function Get-ScriptDirectory {
-  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
-  if ($Invocation.PSScriptRoot) {
-    $Invocation.PSScriptRoot
-  }
-  elseif ($Invocation.MyCommand.Path) {
-    Split-Path $Invocation.MyCommand.Path
-  } else {
-    $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf(""))
-  }
-}
 # TODO Transaction 
 $profile_path_relative = 'Mozilla\Firefox'
 
@@ -73,23 +75,45 @@ $mapped_paths = @{
 
 }
 
+<#
+
+$mapped_paths.Keys | ForEach-Object {
+  $data_source_path = $_
+  $data_target_path = $mapped_paths[$data_source_path]
+  # Write-Output "remove-item -recurse -Path ${data_target_path} -Force -ErrorAction 'SilentlyContinue'" -filter 
+  # TODO: Removal has to copy default profile
+  # Remove-Item -Recurse -Path $data_target_path -Force -ErrorAction 'SilentlyContinue'
+
+}
+#>
+
 $mapped_paths.Keys | ForEach-Object {
   $data_source_path = $_
   $data_target_path = $mapped_paths[$data_source_path]
 
-  # Write-Output "remove-item -recurse -Path ${data_target_path} -Force -ErrorAction 'SilentlyContinue'" -filter 
-  #  NEED work - have to copy default profile
-  # Remove-Item -Recurse -Path $data_target_path -Force -ErrorAction 'SilentlyContinue'
   Write-Output "copy-item -recurse -Path ${data_source_path}  -Destination ${data_target_path} -force -errorAction 'SilentlyContinue'"
-  Copy-Item -Recurse -Path ('{0}\*' -f $data_source_path) -Destination $data_target_path -Force -ErrorAction 'SilentlyContinue'
+  Start-Sleep 10
+  $title = "Copy profiles"
+  $message = "..."
+  $continue = New-Object System.Management.Automation.Host.ChoiceDescription "&YES",`
+     "YES- continue"
+  $skip = New-Object System.Management.Automation.Host.ChoiceDescription "&NO",`
+     "NO- Skip copying"
+  $options = [System.Management.Automation.Host.ChoiceDescription[]]($continue,$skip)
+  $result = $host.ui.PromptForChoice($title,$message,$options,0)
+  Write-Debug $result
+
+  if ($result -eq 0) {
+    Copy-Item -Recurse -Path ('{0}\*' -f $data_source_path) -Destination $data_target_path -Force -ErrorAction 'SilentlyContinue'
+  }
 }
 
 
 Write-Output "# 4 Write new profiles.ini"
 
-write-output $new_profile | out-file -encoding ascii  -Filepath ('{0}\profiles.ini' -f  $profile_path_appdata  ) 
+Write-Output $new_profile | Out-File -Encoding ascii -FilePath ('{0}\profiles.ini' -f $profile_path_appdata)
 
-return 
+return
 
 <#
  & 'D:\Program Files (x86)\Mozilla Firefox\firefox.exe'  -p Selenium
