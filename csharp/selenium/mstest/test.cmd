df92dfa9905ac075f468b9c44f09a7ef2db8e23b
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-09-17 17:54:50 -0400

    added test environment

--- csharp/selenium/mstest/test.cmd
+++ csharp/selenium/mstest/test.cmd
@@ -2,6 +2,7 @@
 setlocal
 del /q %CD%\results.trx
 PATH=%PATH%;c:\Windows\Microsoft.NET\Framework\v4.0.30319
+PATH=%PATH%;C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE
 PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE
 call mstest.exe /testcontainer:Sample\bin\Debug\Sample.dll ^
 /resultsfile:%CD%\results.trx
