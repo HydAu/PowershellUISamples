5cadc1a015ae07cfda6ef62e9a1ba5687f259e65
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-09-18 15:29:07 -0400

    interim version

--- selenium/parse_console.ps1
+++ selenium/parse_console.ps1
@@ -19,25 +19,25 @@
 #THE SOFTWARE.
 
 param(
-  [string] $debug = '' 
+  [string]$debug = ''
 )
 
 # http://stackoverflow.com/questions/8343767/how-to-get-the-current-directory-of-the-cmdlet-being-executed
 function Get-ScriptDirectory
 {
-    $Invocation = (Get-Variable MyInvocation -Scope 1).Value;
-    if($Invocation.PSScriptRoot)
-    {
-        $Invocation.PSScriptRoot;
-    }
-    Elseif($Invocation.MyCommand.Path)
-    {
-        Split-Path $Invocation.MyCommand.Path
-    }
-    else
-    {
-        $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf("\"));
-    }
+  $Invocation = (Get-Variable MyInvocation -Scope 1).Value;
+  if ($Invocation.PSScriptRoot)
+  {
+    $Invocation.PSScriptRoot;
+  }
+  elseif ($Invocation.MyCommand.Path)
+  {
+    Split-Path $Invocation.MyCommand.Path
+  }
+  else
+  {
+    $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf("\"));
+  }
 }
 
 
@@ -53,25 +53,25 @@ $shared_assemblies | ForEach-Object { Unblock-File -Path $_; Add-Type -Path $_; 
 popd
 
 
-$url =  'http://localhost:4444/grid/console#' 
+$url = 'http://localhost:4444/grid/console#'
 
-$sleep_interval = 30 
-$max_retries  = 1
+$sleep_interval = 30
+$max_retries = 1
 $build_log = 'test.properties'
-$expected_http_status  = 200 
+$expected_http_status = 200
 
-$proxyAddr = (get-itemproperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings').ProxyServer
-if ($proxyAddr -eq $null ){
-  $proxyAddr = (get-itemproperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings').AutoConfigURL
+$proxyAddr = (Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings').ProxyServer
+if ($proxyAddr -eq $null) {
+  $proxyAddr = (Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings').AutoConfigURL
 }
 
-if ($proxyAddr -eq $null){
+if ($proxyAddr -eq $null) {
   $proxyAddr = 'http://proxy.carnival.com:8080/array.dll?Get.Routing.Script'
 }
 
-$proxy = new-object System.Net.WebProxy
+$proxy = New-Object System.Net.WebProxy
 $proxy.Address = $proxyAddr
-write-debug ('Probing {0}' -f $proxy.Address )
+Write-Debug ('Probing {0}' -f $proxy.Address)
 $proxy.useDefaultCredentials = $true
 
 $req = [system.Net.WebRequest]::Create($url)
@@ -81,76 +81,79 @@ $req.useDefaultCredentials = $true
 $req.PreAuthenticate = $true
 $req.Headers.Add('UserAgent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/535.2')
 
-$req.Credentials = new-object system.net.networkcredential($build_user, $build_password )
+$req.Credentials = New-Object system.net.networkcredential ($build_user,$build_password)
 # $response = $webrequest.GetResponse()
-[Io.StreamReader] $sr  =  $null
+[Io.StreamReader]$sr = $null
 [int]$int = 0
-for ($i = 0 ; $i -ne $max_retries  ; $i ++ ) {
+for ($i = 0; $i -ne $max_retries; $i++) {
 
 
-try {
+  try {
     $res = $req.GetResponse()
-    $sr = [Io.StreamReader]($res.GetResponseStream()) 
-# may fail!
-# [xml]$xmlout = $sr.ReadToEnd()
+    $sr = [Io.StreamReader]($res.GetResponseStream())
+    # may fail!
+    # [xml]$xmlout = $sr.ReadToEnd()
 
-} catch [System.Net.WebException] {
+  } catch [System.Net.WebException]{
     $res = $_.Exception.Response
+  }
+  $int = [int]$res.StatusCode
+  $time_stamp = (Get-Date -Format 'yyyy/MM/dd hh:mm')
+  $status = $res.StatusCode
+  Write-Output "$time_stamp`t$url`t$int`t$status"
+  if (($int -ne $expected_http_status) -or ($sr -eq $null)) {
+    Start-Sleep -Seconds $sleep_interval
+  }
 }
-$int = [int]$res.StatusCode
-$time_stamp =  ( Get-Date -format 'yyyy/MM/dd hh:mm' ) 
-$status = $res.StatusCode
-write-output "$time_stamp`t$url`t$int`t$status" 
-if (($int -ne $expected_http_status ) -or ( $sr -eq $null )) {
-start-sleep -seconds $sleep_interval
-}
-}
-$time_stamp =  $null 
-if ($int -ne $expected_http_status ) {
-# write error status to a log file and exit
-# 
-    write-output 'Unexpected http status detected. Error reported.'
-    log_message  'STEP_STATUS=ERROR' $build_status 
-	exit 1 
+$time_stamp = $null
+if ($int -ne $expected_http_status) {
+  # write error status to a log file and exit
+  # 
+  Write-Output 'Unexpected http status detected. Error reported.'
+  log_message 'STEP_STATUS=ERROR' $build_status
+  exit 1
 
 }
-[string]$source =$sr.ReadToEnd()
-try{
-# may fail!
-[xml]$xmlout = $source 
+[string]$source = $sr.ReadToEnd()
+try {
+  # will fail to load. 
+  [xml]$xmlout = $source
 
-} 
-catch [Exception]  {
-write-output 'ignoring the exception' 
-# write-output $_.Exception.Message
-<# Cannot convert value "<html><... </a></div></body></html>" to type "System.Xml.XmlDocument". 
+}
+catch [exception]{
+  Write-Output 'ignoring the exception'
+  # write-output $_.Exception.Message
+  <# Cannot convert value "<html><... </a></div></body></html>" to type "System.Xml.XmlDocument". 
 Error: "The 'p' start tag on line 1 position 749 does not match the end tag of 'div'. Line 4, position 833." 
 #>
 }
 
-[System.Net.WebUtility]::HtmlDecode($source) 
-write-debug $source
-[HtmlAgilityPack.HtmlDocument]$resultat  = new-Object HtmlAgilityPack.HtmlDocument
-$resultat.LoadHtml($source) 
+[System.Net.WebUtility]::HtmlDecode($source)
+
+
 # http://www.codeproject.com/Tips/804660/How-to-Parse-Html-using-csharp
 # http://htmlagilitypack.codeplex.com/downloads/get/437941
-
-[HtmlAgilityPack.HtmlNodeCollection] $nodes = $resultat.DocumentNode.SelectNodes("//p[@class='proxyid']")
-foreach ( $node in $nodes)
+[HtmlAgilityPack.HtmlNodeCollection]$nodes = $resultat.DocumentNode.SelectNodes("//p[@class='proxyid']")
+foreach ($node in $nodes)
 {
-     write-output $node.InnerText   
-     [HtmlAgilityPack.HtmlNodeCollection] $browsers = $node.ParentNode.SelectNodes("//div[@type='browsers']//img")
+  Write-Output $node.InnerText
+  [HtmlAgilityPack.HtmlNodeCollection]$browsers = $node.ParentNode.SelectNodes("//div[@type='browsers']//img")
 
-# [HtmlAgilityPack.HtmlNode] $node = $null
-foreach ( $image in $browsers)
-{
+  # [HtmlAgilityPack.HtmlNode] $node = $null
+  foreach ($image in $browsers)
+  {
 
-     write-output $image.Attributes["title"].Value
-     write-output $image.Attributes["class"].Value
+    Write-Output $image.Attributes["title"].Value
+    Write-Output $image.Attributes["class"].Value
+
+  }
 
 }
 
-}
+[HtmlAgilityPack.HtmlWeb]$web = New-Object HtmlAgilityPack.HtmlWeb
+[System.Xml.XmlTextWriter]$wr = New-Object XML.XmlTextWriter ('{0}\{1}' -f (Get-ScriptDirectory),'test12.xml'),([Text.Encoding]::Unicode)
+[void]$web.LoadHtmlAsXml($url,$wr)
+
+$wr.Close()
 
 return
-
