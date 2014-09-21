4ddba8db2af904a2a6c3804b155b4d4f523cbe6b
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-09-20 21:11:13 -0400

    added copyright

--- unfinished/barchart_vbnet_example.ps1
+++ unfinished/barchart_vbnet_example.ps1
@@ -1,4 +1,22 @@
-﻿Add-Type -Language 'VisualBasic' -TypeDefinition @"
+﻿#Copyright (c) 2014 Serguei Kouzmine
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+
+Add-Type -Language 'VisualBasic' -TypeDefinition @"
 
 ' origin: 
 ' http://www.codeproject.com/Articles/7456/Drawing-a-Bar-Chart
@@ -364,7 +382,7 @@ $data2.Add("Item9", 267)
 $object.LoadData([System.Collections.Hashtable] $data1, [System.Collections.Hashtable] $data2)
 
 [void]$object.ShowDialog([System.Windows.Forms.IWin32Window] ($caller) )
-# start-sleep -seconds 4
+
 $object.Close()
 $object.Dispose()
 
