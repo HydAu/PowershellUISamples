9eb598f346a9ee5da2e75f7957f5e9bd0ff0ab33
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-10-02 12:27:36 -0400

    fixes for default W7 environment

--- unfinished/carnival_search.ps1
+++ unfinished/carnival_search.ps1
@@ -20,12 +20,18 @@ $shared_assemblies = @(
   'Selenium.WebDriverBackedSelenium.dll',
   'nunit.framework.dll'
 )
-
+# NOTE  a call to fix_w2k3.ps1 may be needed
 $env:SHARED_ASSEMBLIES_PATH = "c:\developer\sergueik\csharp\SharedAssemblies"
 
 $shared_assemblies_path = $env:SHARED_ASSEMBLIES_PATH
 pushd $shared_assemblies_path
-$shared_assemblies | ForEach-Object { Unblock-File -Path $_; Add-Type -Path $_ }
+$shared_assemblies | ForEach-Object { 
+ if ($host.Version.Major -gt 2){
+   Unblock-File -Path $_;
+ }
+ write-output $_
+ Add-Type -Path $_ 
+ }
 popd
 
 $verificationErrors = New-Object System.Text.StringBuilder
