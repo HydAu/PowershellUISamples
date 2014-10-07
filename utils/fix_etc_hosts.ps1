cbcb3102ca8da2158e9c971e582936c49973f349
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-10-07 07:45:07 -0400

    minor variable rename

--- utils/fix_etc_hosts.ps1
+++ utils/fix_etc_hosts.ps1
@@ -43,40 +43,40 @@ function fix_hosts_file {
     [string]$append
   )
 
-  # http://stackoverflow.com/questions/8343767/how-to-get-the-current-directory-of-the-cmdlet-being-executed
-  function Get-ScriptDirectory
-  {
-    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
-    if ($Invocation.PSScriptRoot) {
-      $Invocation.PSScriptRoot
-    }
-    elseif ($Invocation.MyCommand.Path) {
-      Split-Path $Invocation.MyCommand.Path
-    } else {
-      $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf(""))
-    }
+# http://stackoverflow.com/questions/8343767/how-to-get-the-current-directory-of-the-cmdlet-being-executed
+function Get-ScriptDirectory
+{
+  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+  if ($Invocation.PSScriptRoot) {
+    $Invocation.PSScriptRoot
   }
-  $domains_loopback = @(
-    'metrics.carnival.com',
-    'smetrics.carnival.com',
-    'metrics.carnival.co.uk',
-    'smetrics.carnival.co.uk',
-    'static.ak.facebook.com',
-    's-static.ak.facebook.com',
-    'ad.doubleclick.net',
-    'ad.yieldmanager.com',
-    'pc1.yumenetworks.com',
-    'fbstatic-a.akamaihd.net',
-    'ad.amgdgt.com'
-  )
-
-  # note the order is opposite to what is written in hosts file
-  $domains_routed = @{
-
-    'www3.syscarnival.com' = '172.25.178.70';
-    'uk3.syscarnival.com' = '172.25.178.77';
-
+  elseif ($Invocation.MyCommand.Path) {
+    Split-Path $Invocation.MyCommand.Path
+  } else {
+    $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf(""))
   }
+}
+$hostnames_loopback = @(
+  'metrics.carnival.com',
+  'smetrics.carnival.com',
+  'metrics.carnival.co.uk',
+  'smetrics.carnival.co.uk',
+  'static.ak.facebook.com',
+  's-static.ak.facebook.com',
+  'ad.doubleclick.net',
+  'ad.yieldmanager.com',
+  'pc1.yumenetworks.com',
+  'fbstatic-a.akamaihd.net',
+  'ad.amgdgt.com'
+)
+
+# note the order is opposite to what is written in hosts file
+$hostnames_resolved = @{
+
+  'www3.syscarnival.com' = '172.25.178.70';
+  'uk3.syscarnival.com' = '172.25.178.77';
+
+}
 
   Write-Output $env:COMPUTERNAME
   $local_hosts_file = 'c:\windows\system32\drivers\etc\hosts'
@@ -91,7 +91,7 @@ function fix_hosts_file {
     $fixed_hosts_file = $local_hosts_file
   }
 
-  Write-Host -ForegroundColor 'green' @"
+Write-Host -ForegroundColor 'green' @"
 This script updates  ${fixed_hosts_file}
 "@
 
@@ -110,48 +110,48 @@ This script updates  ${fixed_hosts_file}
   }
 
 
-  Write-Host -ForegroundColor 'green' 'Prune old routes'
+Write-Host -ForegroundColor 'green' 'Prune old routes'
 
-  $new_file_content = @()
+$new_file_content = @()
 
-  ($current_file_content -split "`n") | ForEach-Object {
-    $line = $_
-    $keep = $true
-    $domains_routed.Keys | ForEach-Object {
-      $domain_name = $_;
+($current_file_content -split "`n") | ForEach-Object {
+  $line = $_
+  $keep = $true
+  $hostnames_resolved.Keys | ForEach-Object {
+    $domain_name = $_;
 
-      if ($line -match $domain_name) {
-        Write-Host -ForegroundColor 'yellow' ("Removing {0}" -f $line)
-        $keep = $false
-      } else {
-        $keep = $false
-      }
+    if ($line -match $domain_name) {
+      Write-Host -ForegroundColor 'yellow' ("Removing {0}" -f $line)
+      $keep = $false
+    } else {
+      $keep = $false
     }
-    if ($keep) {
-      $new_file_content += $line
-    }
-
-
   }
-
-  Write-Host -ForegroundColor 'green' 'Writing Loopback Rootes'
-  $domains_loopback | ForEach-Object { $domain_name = $_
-
-    $domain_defined_check = $new_file_content -match $domain_name
-    if ($domain_defined_check -eq $null -or $domain_defined_check.count -eq 0) {
-      $new_file_content += ('127.0.0.1 {0}' -f $domain_name)
-    }
+  if ($keep) {
+    $new_file_content += $line
   }
 
 
-  Write-Host -ForegroundColor 'green' 'Writing Environment-specific Routes'
-  $domains_routed.Keys | ForEach-Object {
-    # Always add route
-    $domain_name = $_; $ip_addreress = $domains_routed[$domain_name]
-    $new_file_content += ('{0} {1}' -f $ip_addreress,$domain_name)
-  }
+}
 
-  Set-Content -Path $fixed_hosts_file -Value $new_file_content
+Write-Host -ForegroundColor 'green' 'Writing Loopback Rootes'
+$hostnames_loopback | ForEach-Object { $domain_name = $_
+
+  $domain_defined_check = $new_file_content -match $domain_name
+  if ($domain_defined_check -eq $null -or $domain_defined_check.count -eq 0) {
+    $new_file_content += ('127.0.0.1 {0}' -f $domain_name)
+  }
+}
+
+
+Write-Host -ForegroundColor 'green' 'Writing Environment-specific Routes'
+$hostnames_resolved.Keys | ForEach-Object {
+  # Always add route
+  $domain_name = $_; $ip_addreress = $hostnames_resolved[$domain_name]
+  $new_file_content += ('{0} {1}' -f $ip_addreress,$domain_name)
+}
+
+Set-Content -Path $fixed_hosts_file -Value $new_file_content
 
 }
 
