ac667c255466ebaa39ab2eae8701c5c2e16d3858
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-10-08 07:43:29 -0400

    minor changes towards template

--- unfinished/carnival_search.ps1
+++ unfinished/carnival_search.ps1
@@ -1,3 +1,22 @@
+#Copyright (c) 2014 Serguei Kouzmine
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
 param(
   [string]$browser,
   [int]$version
@@ -18,29 +37,36 @@ function Get-ScriptDirectory
 $shared_assemblies = @(
   'WebDriver.dll',
   'WebDriver.Support.dll',
-  'Selenium.WebDriverBackedSelenium.dll',
   'nunit.framework.dll'
 )
 
-$env:SHARED_ASSEMBLIES_PATH = "c:\developer\sergueik\csharp\SharedAssemblies"
+$shared_assemblies_path = 'c:\developer\sergueik\csharp\SharedAssemblies'
 
-$shared_assemblies_path = $env:SHARED_ASSEMBLIES_PATH
+if (($env:SHARED_ASSEMBLIES_PATH -ne $null) -and ($env:SHARED_ASSEMBLIES_PATH -ne '')) {
+  $shared_assemblies_path = $env:SHARED_ASSEMBLIES_PATH
+}
+
 pushd $shared_assemblies_path
-$shared_assemblies | ForEach-Object { 
- if ($host.Version.Major -gt 2){
-   Unblock-File -Path $_;
- }
- write-output $_
- Add-Type -Path $_ 
- }
+$shared_assemblies | ForEach-Object {
+
+  if ($host.Version.Major -gt 2) {
+    Unblock-File -Path $_;
+  }
+  Write-Debug $_
+  Add-Type -Path $_
+}
 popd
+
+$hub_host = '127.0.0.1'
+$hub_port = '4444'
+$uri = [System.Uri](('http://{0}:{1}/wd/hub' -f $hub_host,$hub_port))
 
 $verificationErrors = New-Object System.Text.StringBuilder
 
 if ($browser -ne $null -and $browser -ne '') {
   try {
     $connection = (New-Object Net.Sockets.TcpClient)
-    $connection.Connect("127.0.0.1",4444)
+    $connection.Connect($hub_host,[int]$hub_port)
     $connection.Close()
   } catch {
     Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\hub.cmd"
@@ -69,8 +95,12 @@ if ($browser -ne $null -and $browser -ne '') {
   else {
     throw "unknown browser choice:${browser}"
   }
-  $uri = [System.Uri]("http://127.0.0.1:4444/wd/hub")
-  $selenium = New-Object OpenQA.Selenium.Remote.RemoteWebDriver ($uri,$capability)
+  try{
+    $selenium = New-Object OpenQA.Selenium.Remote.RemoteWebDriver ($uri,$capability)
+  } catch [Exception] {
+  Write-Output $_.Exception.Message
+     exit
+  }
 } else {
   Write-Host 'Running on phantomjs'
   $phantomjs_executable_folder = "C:\tools\phantomjs"
@@ -248,8 +278,9 @@ try {
 try {
   $selenium.Quit()
 } catch [exception]{
-  # Ignore errors if unable to close the browser
+  Write-Output (($_.Exception.Message) -split "`n")[0]
 }
+
 
 <#
 https://www.youtube.com/watch?v=76qeLNMHgF4
