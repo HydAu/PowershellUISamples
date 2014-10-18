753b6523691c24604ce2808fa2c85db63d71863f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-10-18 17:35:18 -0400

    initial revision

---
+++ selenium/mouse.ps1
@@ -0,0 +1,145 @@
+#Copyright (c) 2014 Serguei Kouzmine
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+param(
+  [switch]$browser
+)
+# http://stackoverflow.com/questions/8343767/how-to-get-the-current-directory-of-the-cmdlet-being-executed
+function Get-ScriptDirectory
+{
+  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+  if ($Invocation.PSScriptRoot) {
+    $Invocation.PSScriptRoot
+  }
+  elseif ($Invocation.MyCommand.Path) {
+    Split-Path $Invocation.MyCommand.Path
+  } else {
+    $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf(""))
+  }
+}
+$shared_assemblies = @(
+  "WebDriver.dll",
+  "WebDriver.Support.dll",
+  "Selenium.WebDriverBackedSelenium.dll",
+  'nunit.core.dll',
+  'nunit.framework.dll'
+
+)
+
+$env:SHARED_ASSEMBLIES_PATH = "c:\developer\sergueik\csharp\SharedAssemblies"
+
+$shared_assemblies_path = $env:SHARED_ASSEMBLIES_PATH
+pushd $shared_assemblies_path
+$shared_assemblies | ForEach-Object { Unblock-File -Path $_; Add-Type -Path $_ }
+popd
+
+$verificationErrors = New-Object System.Text.StringBuilder
+$phantomjs_executable_folder = "C:\tools\phantomjs"
+if ($PSBoundParameters["browser"]) {
+  try {
+    $connection = (New-Object Net.Sockets.TcpClient)
+    $connection.Connect("127.0.0.1",4444)
+    $connection.Close()
+  } catch {
+    Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\hub.cmd"
+    Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\node.cmd"
+    Start-Sleep -Seconds 10
+  }
+  $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Firefox()
+  $uri = [System.Uri]("http://127.0.0.1:4444/wd/hub")
+  $selenium = New-Object OpenQA.Selenium.Remote.RemoteWebDriver ($uri,$capability)
+} else {
+  $selenium = New-Object OpenQA.Selenium.PhantomJS.PhantomJSDriver ($phantomjs_executable_folder)
+  $selenium.Capabilities.SetCapability("ssl-protocol","any")
+  $selenium.Capabilities.SetCapability("ignore-ssl-errors",$true)
+  $selenium.Capabilities.SetCapability("takesScreenshot",$true)
+  $selenium.Capabilities.SetCapability("userAgent","Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.34 (KHTML, like Gecko) PhantomJS/1.9.7 Safari/534.34")
+  $options = New-Object OpenQA.Selenium.PhantomJS.PhantomJSOptions
+  $options.AddAdditionalCapability("phantomjs.executable.path",$phantomjs_executable_folder)
+}
+
+
+$baseURL = "file:///C:/developer/sergueik/powershell_ui_samples/external/architecture.svg"
+$selenium.Navigate().GoToUrl($baseURL)
+$selenium.Navigate().Refresh()
+
+
+# https://zoomcharts.com/templates/default/doc/time-chart/images/architecture.svg
+# https://zoomcharts.com/en/gallery/all:time-chart-area-gold-prices
+# https://www.linkedin.com/groups/Simulate-mouse-clicking-961927.S.5928176227885027333?view=&item=5928176227885027333&type=member&gid=961927&trk=eml-b2_anet_digest-group_discussions-11-grouppost-disc-8&midToken=AQGpqMsZZNuraw&fromEmail=fromEmail&ut=0-qp6wmfrXwCs1
+[OpenQA.Selenium.IMouse]$mouse = ([OpenQA.Selenium.IHasInputDevices]$selenium).Mouse
+
+try {
+  [OpenQA.Selenium.Support.UI.WebDriverWait]$wait = New-Object OpenQA.Selenium.Support.UI.WebDriverWait ($selenium,[System.TimeSpan]::FromSeconds(1))
+  $wait.PollingInterval = 30
+  [void]$wait.Until([OpenQA.Selenium.Support.UI.ExpectedConditions]::ElementExists([OpenQA.Selenium.By]::TagName('image')))
+
+} catch [exception]{
+  Write-Output ("Exception : {0} ...`n" -f (($_.Exception.Message) -split "`n")[0])
+}
+
+[OpenQA.Selenium.IWebElement]$element = $selenium.FindElement([OpenQA.Selenium.By]::TagName('image'))
+[OpenQA.Selenium.ILocatable]$loc = ([OpenQA.Selenium.ILocatable]$element)
+
+$coord = $loc.Coordinates
+$coord.LocationInDom.X = 100
+$coord.LocationInDom.Y = 100
+$coord.LocationInViewport.X = $coord.LocationInDom.X
+$coord.LocationInViewport.Y = $coord.LocationInDom.Y
+$coord
+$mouse.MouseMove($coord)
+$mouse.Click($coord)
+<#
+
+//declaration
+public static class StaticExtentions
+{
+
+// http://www.pinvoke.net/default.aspx/user32.mouse_event
+[DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+public static extern void mouse_event(uint dwFlags, int dx, int dy, uint cButtons, uint dwExtraInfo);
+// static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+
+
+[DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+public static extern long SetCursorPos(int X, int Y);
+
+private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+private const int MOUSEEVENTF_LEFTUP = 0x04;
+private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+private const int MOUSEEVENTF_RIGHTUP = 0x10;
+private const int MOUSEEVENTF_WHEEL = 0x800;
+}
+
+//usage
+IWebElement element = driver.FindElement(........);
+var X = element.Location.X;
+var Y = element.Location.Y;
+SetCursorPos(X, Y);
+mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0); 
+
+#>
+try {
+  $selenium.Quit()
+} catch [exception]{
+  # Ignore errors if unable to close the browser
+}
+
+
