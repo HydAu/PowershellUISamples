8adab2f9398a4e47f20d9754e5fdebfe2a5b2ff7
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-10-25 17:44:15 -0400

    refactoring

--- utils/page_with_proxy.ps1
+++ utils/page_with_proxy.ps1
@@ -1,8 +1,10 @@
-$sleep_interval = 30 
-$max_retries  = 3
-$build_status = 'test.properties'
-# this is a test
-$expected_status  = 200 
+param (
+[String]$username = 'sergueik',
+[String]$url = '',
+[switch]$use_proxy,
+[String]$password = ''
+
+)
 
 function log_message {
 param(
@@ -11,35 +13,42 @@ param(
     [Parameter(Position=1)]
     [string] $logfile 
 )
-
-if ($SOLVED_UTF16_BUG -and $host.version.major -gt 2 ) {
-
-  <# WARNING Tee-Object corrupts files with utf16
-     WARNING:  Tee-Object in Powershell v2 does not have append option:
-     Tee-Object : A parameter cannot be found that matches parameter name 'append'.
-    PS D:\java\Jenkins\master\jobs\SQL_RUNNER_2\workspace> 
-    Tee-Object  -FilePath 'test.properties' -append -InputObject 'hello world'
-    hello world
-    type 'test.properties' 
-    h e l l o  w o r l d
-  #>
-  Tee-Object  -FilePath $logfile -Encoding ascii -append -InputObject $message
-} else {
-  # on certain machines
-  # $host.version  = 2.0 
-  # the tee-object does not support -append option.a
   write-output -InputObject $message 
   write-output -InputObject $message  | out-file -FilePath $logfile -Encoding ascii -Force -append
 }
+
+function page_content {
+param (
+[String]$username = 'sergueik',
+[String]$url = '',
+[String]$password = ''
+)
+write-host '2'
+if ($url -eq $null -or $url -eq ''){ 
+#  $url =  ('https://github.com/{0}' -f $username)
+  $url =   'https://api.github.com/user'
 }
+write-host '3'
+
+$sleep_interval = 3
+$max_retries  = 30
+$build_status = 'test.properties'
+# this is a test
+$expected_status  = 200 
+
 
 ( $build_status ) | foreach-object {set-content -Path $_ -value ''}
 
 
 $log_file  = 'healthcheck.txt' 
-$url = "http://haldev.service-now.com/api/now/table/change_request"
-$url =  "https://my.keynote.com/newmykeynote/scatterplotdrilldown.do?transid=1972395&agentid=40187&butd=561933119&profid=0&pageid=1"
+# $url = "http://haldev.service-now.com/api/now/table/change_request"
+# $url =  "https://my.keynote.com/newmykeynote/scatterplotdrilldown.do?transid=1972395&agentid=40187&butd=561933119&profid=0&pageid=1"
+
+  if ($PSBoundParameters['use_proxy']) {
+
+# Use current user NTLM credentials do deal with corporate firewall
 $proxyAddr = (get-itemproperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings').ProxyServer
+
 if ($proxyAddr -eq $null ){
   $proxyAddr = (get-itemproperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings').AutoConfigURL
 }
@@ -50,64 +59,72 @@ if ($proxyAddr -eq $null){
 
 $proxy = new-object System.Net.WebProxy
 $proxy.Address = $proxyAddr
-write-debug ("Probing {0}" -f $proxy.Address )
+write-output ("Probing {0}" -f $proxy.Address )
 $proxy.useDefaultCredentials = $true
 
-$req = [system.Net.WebRequest]::Create($url)
-$req.proxy = $proxy
-$req.useDefaultCredentials = $true
-
-# Use current user NTLM credentials do deal with corporate firewall
-# Note github returns a json result saying that it requires authentication 
-# normally the server returns a "classic" 401 html page
+}
 <#
-[WebRequest] request = (HttpWebRequest)WebRequest.Create("https://api.github.com/user")
-String username = "sergueik"
-String password = "D0ss1aXML"
-String encoded = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password));
-httpWebRequest.Headers.Add("Authorization", "Basic " + encoded);
-
-request.Credentials = new NetworkCredential("githubUsername", "githubPassword");
-request.PreAuthenticate = true;
-var response = request.GetResponse();
-
-CookieContainer myContainer = new CookieContainer();
-HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://telematicoprova.agenziadogane.it/TelematicoServiziDiUtilitaWeb/ServiziDiUtilitaAutServlet?UC=22&SC=1&ST=2");
 request.Credentials = new NetworkCredential(xxx,xxx);
+CookieContainer myContainer = new CookieContainer();
 request.CookieContainer = myContainer;
 request.PreAuthenticate = true;
-HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
 #>
 
-for ($i = 0 ; $i -ne $max_retries  ; $i ++ ) {
+[system.Net.WebRequest]$request = [system.Net.WebRequest]::Create($url)
+[String]$encoded = [System.Convert]::ToBase64String([System.Text.Encoding]::GetEncoding("ISO-8859-1").GetBytes(($username + ':' + $password)))
+$request.Headers.Add("Authorization", "Basic " + $encoded)
 
+  if ($PSBoundParameters['use_proxy']) {
+
+$request.proxy = $proxy
+$request.useDefaultCredentials = $true
+}
+# Note github returns a json result saying that it requires authentication 
+# normally the server returns a "classic" 401 html page
+
+write-host ('Open {0}' -f $url )
+
+for ($i = 0 ; $i -ne $max_retries  ; $i ++ ) {
+write-host  $i
+write-output $i
+write-output $i
 
 try {
-    $res = $req.GetResponse()
+    $response = $request.GetResponse()
 } catch [System.Net.WebException] {
-    $res = $_.Exception.Response
-
-
-
+    $response = $_.Exception.Response
 }
-$int = [int]$res.StatusCode
+
+$int_status = [int]$response.StatusCode
 $time_stamp =  ( Get-Date -format 'yyyy/MM/dd hh:mm' ) 
-$status = $res.StatusCode
-write-output "$time_stamp`t$url`t$int`t$status"  | Tee-Object  -FilePath $log_file  -append 
+$status = $response.StatusCode # not casting
+
+write-output "$time_stamp`t$url`t$int_status`t$status" 
+# | Tee-Object  -FilePath $log_file  -append 
+if ($int_status -ne $expected_status ) {
+    write-output 'Unexpected http status detected. sleep and retry.'
+
 start-sleep -seconds $sleep_interval
+
+# sleep and retry
+} else { 
+break
+}
+}
+
 $time_stamp =  $null 
-if ($int -ne $expected_status ) {
+if ($int_status -ne $expected_status ) {
 # write error status to a log file and exit
 # 
-    write-output 'Unexpected http status detected. Error reported.'
+    write-output ('Unexpected http status detected. Error reported. {0}, {1} ' -f $int_status)
     log_message  'STEP_STATUS=ERROR' $build_status 
 }
 
-#---
-
-    $respstream = $res.GetResponseStream() 
+    $respstream = $response.GetResponseStream() 
        $stream_reader = new-object System.IO.StreamReader $respstream
        $result_page = $stream_reader.ReadToEnd()
+       <#
        if ($result_page -match $confirm_page_text) {
          $found_expected_status =  $true
          if ($result_page.size -lt 100 )
@@ -119,8 +136,16 @@ if ($int -ne $expected_status ) {
          $found_expected_status =  $false
          $result_page = ''
        }
-#---
+       #> 
+
+
+write-debug $result_page
+
+return $result_page
+
 }
 
+write-output '1'
+write-output ( page_content -username $username -password $password -url $url)
 
-return
+
