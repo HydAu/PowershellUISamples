f92c15b14f0ca6b0f43fe2562d52f1f6f0ee5fe7
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-10-26 19:18:03 -0400

    initial revision

--- browse_for_folder_default.ps1
+++ browse_for_folder_default.ps1
@@ -89,24 +89,29 @@ $shell_obj = new-object -com 'Shell.Application'
 $folder_path = $folder_obj.Self.Path
 write-output $folder_path
 #>
+<#
+$file_obj = $null
 try{ 
 [System.__ComObject]$file_obj = ($shell_obj.BrowseForFolder(
   [System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle,
   'Choose File',
   $BIF_BROWSEINCLUDEFILES,
-  0)
-) }
-catch [Exception] { 
-
+  0)) 
+} catch [Exception] { 
+write-output $_.Exception.Message
+# System.Reflection.TargetInvocationException
+# $_.Exception | get-member
+# Exception has been thrown by the target of an invocation. ???
 }
 
+$file_obj.GetType() | format-list 
 $file_obj.Self | get-member
 # Exception from HRESULT: 0x80004005 (E_FAIL)
 # $folder_obj.GetType() | format-list
 $file_path = $file_obj.Self.Path
 write-output $file_path
+#>
 
-<#
 
 
 Function Get-FileName($initialDirectory)
@@ -121,11 +126,32 @@ Function Get-FileName($initialDirectory)
  $OpenFileDialog.filename
 } #end function Get-FileName
 
+<#
+Add-Type -AssemblyName System.Windows.Forms
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
+    InitialDirectory = [Environment]::GetFolderPath('MyDocuments')
+    Filter = 'Documents (*.docx)|*.docx|SpreadSheet (*.xlsx)|*.xlsx'
+}
+[void]$FileBrowser.ShowDialog()
+$FileBrowser.FileNames
+
+Add-Type -AssemblyName System.Windows.Forms
+ 
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{
+    Multiselect = $true
+}
+ 
+[void]$FileBrowser.ShowDialog()
+$FileBrowser.FileNames
+
+# shttp://www.powershellmagazine.com/2013/07/01/pstip-using-the-system-windows-forms-openfiledialog-class/
+#>
 # *** Entry Point to Script ***
 
 Get-FileName -initialDirectory "c:\fso"
 https://social.technet.microsoft.com/Forums/en-US/ebc8a618-4cd0-4328-b0f1-3c70d811309c/getting-the-file-location-using-openfiledialog-in-powershell
 #>
+<#
 Add-Type -AssemblyName system.Windows.Forms
 $SetBackupLocation = New-Object System.Windows.Forms.OpenFileDialog
 
@@ -155,3 +181,20 @@ $SetBackupLocation2.RestoreDirectory = $true
 $SetBackupLocation2.FileName = 'backupfile'
 $rc = $SetBackupLocation2.ShowDialog()
 $rc
+http://www.experts-exchange.com/Programming/Languages/Scripting/Powershell/Q_28330518.html
+
+
+if($result -eq “OK”) {
+
+$inputFile = $myDialog.FileName
+
+# Continue working with file
+
+}
+
+else {
+
+Write-Host “Cancelled by user”
+
+}
+#>
