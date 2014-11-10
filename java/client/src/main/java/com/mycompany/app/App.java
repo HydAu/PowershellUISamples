4636a2dc68658e3adf7b24f0f1462047c614f34e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-11-10 08:59:24 -0500

    code cleanup - removed android references from non-android class

--- java/client/src/main/java/com/mycompany/app/App.java
+++ java/client/src/main/java/com/mycompany/app/App.java
@@ -14,7 +14,7 @@ import org.apache.http.HttpResponse;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import org.openqa.selenium.WebElement;
 
 import org.apache.http.message.BasicHttpEntityEnclosingRequest;
 // ??
