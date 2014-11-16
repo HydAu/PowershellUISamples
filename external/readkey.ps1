631fe14c5efb4109be2b56435fdb4a7485ad35f3
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-11-15 19:20:46 -0500

    initial memo

--- external/readkey.ps1
+++ external/readkey.ps1
@@ -6,3 +6,8 @@ $g = $host
 $g.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") ; write-output $x
 
 # TODO : draw a div with prompt on the page
+<#
+https://customer.onlinelic.in/ForgotPwd.htm
+http://testing-ground.scraping.pro/captcha?fsubmit
+http://scraping.pro/example-captcha-solver-java/
+#>
