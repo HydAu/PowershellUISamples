f68c9de76cdc2ef3a4eb49bf4213220adaa63194
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-11-22 14:44:05 -0500

    skeleton sqlite project

---
+++ java/sqlite/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+import java.io.File;
+import java.sql.*;
+
+public class App
+{
+  public static void main( String args[] )
+  {
+    Connection c = null;
+    try {
+      Class.forName("org.sqlite.JDBC");
+      c = DriverManager.getConnection("jdbc:sqlite:test.db");
+    } catch ( Exception e ) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      System.exit(0);
+    }
+    System.out.println("Opened database successfully");
+  }
+}
