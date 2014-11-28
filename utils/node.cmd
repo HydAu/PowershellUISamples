ba8512b93e8e4b7ac293e7bd84b0715010c2a79a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-11-28 15:33:11 -0500

    bumped the maven and groovy versions

--- utils/node.cmd
+++ utils/node.cmd
@@ -13,20 +13,21 @@ if "%HUB_HOST%" equ "" set HUB_HOST=127.0.0.1
 set HUB_HTTP_PORT=4444
 
 set HTTPS_PORT=-1
-set APP_VERSION=2.44.0
-set JAVA_VERSION=1.6.0_45
-set JAVA_HOME=c:\progra~1\java\jdk%JAVA_VERSION%
-set JAVA_HOME=c:\java\jdk%JAVA_VERSION%
-set MAX_MEMORY=-Xmx256m
-set STACK_SIZE=-Xss8m
-set LOGFILE=node.log
 
-rem Need to keep 1.7 and 1.6 both installed
-set GROOVY_HOME=c:\java\groovy-2.3.2
-set HUB_URL=http://127.0.0.1:4444/grid/register
-REM cannot use paths
-set NODE_CONFIG=NODE.json
-PATH=%JAVA_HOME%\bin;%PATH%;%GROOVY_HOME%\bin
+
+set SELENIUM_VERSION=2.44.0
+set GROOVY_VERSION=2.3.8
+set JAVA_VERSION=1.6.0_45
+set MAVEN_VERSION=3.2.1
+set JAVA_HOME=c:\java\jdk%JAVA_VERSION%
+set GROOVY_HOME=c:\java\groovy-%GROOVY_VERSION%
+set M2_HOME=c:\java\apache-maven-%MAVEN_VERSION%
+set M2=%M2_HOME%\bin
+set MAVEN_OPTS=-Xms256m -Xmx512m
+
+PATH=%JAVA_HOME%\bin;%PATH%;%GROOVY_HOME%\bin;%M2%
+
+PATH=%PATH%;c:\Program Files\Mozilla Firefox
 
 IF "%PROCESSOR_ARCHITECTURE%"=="x86" GOTO :PATH_x86
 PATH=%PATH%;%ProgramFiles(x86)%\Google\Chrome\Application
@@ -43,22 +44,32 @@ GOTO :END_PATH
 PATH=%PATH%;%LOCALAPPDATA%\Mozilla Firefox
 
 where.exe firefox.exe
-CHOICE /T 1 /C ync /CS /D y 
 where.exe chrome.exe
-CHOICE /T 1 /C ync /CS /D y
 where.exe iexplore.exe
 CHOICE /T 1 /C ync /CS /D y
+
+
+
+set MAX_MEMORY=-Xmx256m
+set STACK_SIZE=-Xss8m
+
+rem Need to keep 1.7 and 1.6 both installed
+set HUB_URL=http://127.0.0.1:4444/grid/register
+
+REM cannot use paths
+set NODE_CONFIG=NODE.json
 
 set LAUNCHER_OPTS=-XX:MaxPermSize=1028M -Xmn128M
 set LAUNCHER_OPTS=%MAX_MEMORY% %STACK_SIZE%
 
 REM java %LAUNCHER_OPTS% ^
-REM -jar selenium-server-standalone-%APP_VERSION%.jar ^
+REM -jar selenium-server-standalone-%SELENIUM_VERSION%.jar ^
 
+set LOGFILE=node.log
 type NUL  > %LOGFILE%
 
 java %LAUNCHER_OPTS% ^
--classpath %SELENIUM_HOME%/log4j-1.2.17.jar;%SELENIUM_HOME%/selenium-server-standalone-%APP_VERSION%.jar; ^-Dlog4j.configuration=node.log4j.properties ^
+-classpath %SELENIUM_HOME%/log4j-1.2.17.jar;%SELENIUM_HOME%/selenium-server-standalone-%SELENIUM_VERSION%.jar; ^-Dlog4j.configuration=node.log4j.properties ^
 org.openqa.grid.selenium.GridLauncher ^
 -role node ^
 -host %NODE_HOST% ^
@@ -71,12 +82,8 @@ org.openqa.grid.selenium.GridLauncher ^
 -ensureCleanSession true ^
 -trustAllSSLCertificates 
 
-
 REM Keep the Blank line above intact
 goto :EOF
 REM http://www.deepshiftlabs.com/sel_blog/?p=2155&&lang=en-us
 REM http://grokbase.com/t/gg/webdriver/1282vm4ej0/how-to-set-the-command-line-switches-for-iedriverserver-exe-when-running-it-along-with-grid-node 
 REM http://seleniumonlinetrainingexpert.wordpress.com/2012/12/11/how-do-i-start-the-internet-explorer-webdriver-for-selenium/
-
-
-
--- utils/selenium.cmd
+++ utils/selenium.cmd
@@ -2,18 +2,27 @@
 pushd %~dp0
 set HTTP_PORT=4444
 set HTTPS_PORT=-1
-set APP_VERSION=2.44.0
-set JAVA_HOME=c:\java\jdk1.6.0_45
-set GROOVY_HOME=c:\java\groovy-2.3.2
-PATH=%JAVA_HOME%\bin;%PATH%;%GROOVY_HOME%\bin
+set SELENIUM_VERSION=2.44.0
+set GROOVY_VERSION=2.3.8
+set JAVA_VERSION=1.6.0_45
+set MAVEN_VERSION=3.2.1
+set JAVA_HOME=c:\java\jdk%JAVA_VERSION%
+set GROOVY_HOME=c:\java\groovy-%GROOVY_VERSION%
+set M2_HOME=c:\java\apache-maven-%MAVEN_VERSION%
+set M2=%M2_HOME%\bin
+set MAVEN_OPTS=-Xms256m -Xmx512m
+PATH=%JAVA_HOME%\bin;%PATH%;%GROOVY_HOME%\bin;%M2%
+
 PATH=%PATH%;c:\Program Files\Mozilla Firefox
 WHERE firefox.exe
-REM
 CHOICE /T 1 /C ync /CS /D y
+
 REM This setting needs adjustment.
 REM set LAUNCHER_OPTS=-XX:PermSize=512M -XX:MaxPermSize=1028M -Xmn128M -Xms512M -Xmx1024M
+
+
 set LAUNCHER_OPTS=-XX:MaxPermSize=1028M -Xmn128M
-java %LAUNCHER_OPTS% -jar selenium-server-standalone-%APP_VERSION%.jar -port %HTTP_PORT% 
+java %LAUNCHER_OPTS% -jar selenium-server-standalone-%SELENIUM_VERSION%.jar -port %HTTP_PORT%
 	
 REM https://code.google.com/p/selenium/wiki/InternetExplorerDriver
 rem http://seleniumonlinetrainingexpert.wordpress.com/2012/12/11/how-do-i-start-the-internet-explorer-webdriver-for-selenium/
