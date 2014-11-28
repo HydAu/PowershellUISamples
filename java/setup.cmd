2dbc043eea846fa0d3d37fd9b90c25d808d4e886
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-11-28 15:36:49 -0500

    bumped the maven and groovy versions

--- java/setup.cmd
+++ java/setup.cmd
@@ -1,10 +1,15 @@
 @echo OFF
 pushd %~dp0
-set JAVA_HOME=c:\java\jdk1.6.0_45
-set M2_HOME=c:\java\apache-maven-3.2.1
+set GROOVY_VERSION=2.3.8
+set JAVA_VERSION=1.6.0_45
+set MAVEN_VERSION=3.2.1
+set JAVA_HOME=c:\java\jdk%JAVA_VERSION%
+set GROOVY_HOME=c:\java\groovy-%GROOVY_VERSION%
+
+set M2_HOME=c:\java\apache-maven-%MAVEN_VERSION%
 set M2=%M2_HOME%\bin
 set MAVEN_OPTS=-Xms256m -Xmx512m
-set GROOVY_HOME=c:\java\groovy-2.3.2
+
 PATH=%JAVA_HOME%\bin;%PATH%;%GROOVY_HOME%\bin;%M2%
 
 REM http://www.tutorialspoint.com/maven/maven_environment_setup.htm
