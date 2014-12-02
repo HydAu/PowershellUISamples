cce918829a783beab5f46cb4fcef474f1d9d4034
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-12-01 22:17:56 -0500

    more links

--- unfinished/ms_internet_explorer_driver.ps1
+++ unfinished/ms_internet_explorer_driver.ps1
@@ -1,1 +1,20 @@
+# cover:
 # http://jimevansmusic.blogspot.com/2014/09/using-internet-explorer-webdriver.html
+# http://msdn.microsoft.com/en-us/library/ie/dn725043%28v=vs.85%29.aspx
+
+# IEDCWebDriver.dll - not found after installing KB2990999 - need a restart.
+# and install of Internet Explorer Developer Channel for Windows 7 SP1 
+# "c:\Program Files\Common Files\IEDCWebDriver\IEDCWebDriver.dll" 
+# is native
+#  DllCanUnloadNow
+#   DllGetClassObject
+#   DllInstall
+#   DllRegisterServer
+#   DllUnregisterServer
+# http://www.microsoft.com/en-us/download/details.aspx?id=43360
+# API
+# http://msdn.microsoft.com/en-us/library/ie/dn722338%28v=vs.85%29.aspx
+
+# Sample project
+# https://code.msdn.microsoft.com/windowsdesktop/Internet-Explorer-02ac106f
+# http://msdn.microsoft.com/en-us/library/ie/dn725045%28v=vs.85%29.aspx
