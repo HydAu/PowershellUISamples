68b88f31b42e0c05618dfd807ed239510e9cea65
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-12-01 22:36:16 -0500

    more notes

--- unfinished/ms_internet_explorer_driver.ps1
+++ unfinished/ms_internet_explorer_driver.ps1
@@ -18,3 +18,16 @@
 # Sample project
 # https://code.msdn.microsoft.com/windowsdesktop/Internet-Explorer-02ac106f
 # http://msdn.microsoft.com/en-us/library/ie/dn725045%28v=vs.85%29.aspx
+
+#WebDriver will only run against the DeveloperPreview build. Please run this EXE
+#with the following argument:
+#For x64 Systems:
+     /appvve:9BD02EED-6C11-4FF0-8A3E-0B4733EE86A1_6A0357B5-AB99-4856-8A59-CF2C38579E78
+#Or for x86 Systems:
+     /appvve:9BD02EED-6C11-4FF0-8A3E-0B4733EE86A1_681E2361-2C6F-4D47-A8B7-D3F7B288CB45
+
+# Test code runs successfully, but the nag screen prompts for
+# install the latest version of Internet Explorer Developer Channel
+# from 
+# http://go.microsoft.com/fwlink/?LinkId=396394
+# which in turn requires installing Windows 10 Technical Preview
