0902bedd269a604875265e506e1db6059adeb1f4
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-12-02 18:19:51 -0500

    Detect Internet Explorer Developer Channel

--- utils/get_ie_version.ps1
+++ utils/get_ie_version.ps1
@@ -26,3 +26,25 @@ Write-Output $setting
 <#
 This does not work with IE 8 
 #>
+
+
+# Detect Internet Explorer Developer Channel
+# "${env:LOCALAPPDATA}\Microsoft\AppV\Client\Integration\9BD02EED-6C11-4FF0-8A3E-0B4733EE86A1\Root\VFS\ProgramFiles\Internet Explorer\iexplore.exe"
+
+# HKEY_CLASSES_ROOT\AppV\Client\Packages\9BD02EED-6C11-4FF0-8A3E-0B4733EE86A1
+# HKEY_CURRENT_USER\Software\Classes\AppV\Client\Packages\9BD02EED-6C11-4FF0-8A3E-0B4733EE86A1
+# HKEY_CURRENT_USER\Software\Microsoft\AppV\Client\Integration\Ownership\SOFTWARE\Microsoft\AppV\Client\Packages\9bd02eed-6c11-4ff0-8a3e-0b4733ee86a1
+# HKEY_CURRENT_USER\Software\Microsoft\AppV\Client\Integration\Packages\{9BD02EED-6C11-4FF0-8A3E-0B4733EE86A1}
+
+"Integration Location" "%LOCALAPPDATA%\Microsoft\AppV\Client\Integration\9BD02EED-6C11-4FF0-8A3E-0B4733EE86A1"
+"Staged Location" "C:\ProgramData\App-V\9BD02EED-6C11-4FF0-8A3E-0B4733EE86A1\681E2361-2C6F-4D47-A8B7-D3F7B288CB45"
+
+
+# HKEY_CURRENT_USER\Software\Microsoft\AppV\Client\Packages\9bd02eed-6c11-4ff0-8a3e-0b4733ee86a1\REGISTRY\USER\S-1-5-21-440999728-2294759910-2183037890-1000\Software\Microsoft\Internet Explorer
+
+# HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\AppV\Client\Packages\9BD02EED-6C11-4FF0-8A3E-0B4733EE86A1\Versions\681E2361-2C6F-4D47-A8B7-D3F7B288CB45\REGISTRY\MACHINE\Software\Microsoft\Internet Explorer
+
+"svcVersion" "DC1"
+
+
+# HKEY_USERS\S-1-5-21-440999728-2294759910-2183037890-1000\Software\Classes\AppV\Client\Packages\9BD02EED-6C11-4FF0-8A3E-0B4733EE86A1\REGISTRY\USER\S-1-5-21-440999728-2294759910-2183037890-1000_Classes
