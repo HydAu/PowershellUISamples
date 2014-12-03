63a06dcbeccc394ee112e3ab4e4c0bdc4822ba5e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-12-02 19:20:06 -0500

    Detect Internet Explorer Developer Channel

--- utils/get_ie_version.ps1
+++ utils/get_ie_version.ps1
@@ -1,3 +1,8 @@
+<#
+This does not work with IE 8 
+#>
+
+
 Write-Host -ForegroundColor 'green' @"
 This call shows 32-bit IE version
 "@
@@ -6,7 +11,7 @@ if (-not [environment]::Is64BitProcess) {
   $path = '/SOFTWARE/Microsoft/Internet Explorer'
 
 } else {
-  $path = '/SOFTWARE/Wow6432Node/Microsoft/Internet Explorer'}
+  $path = '/SOFTWARE/Wow6432Node/Microsoft/Internet Explorer' }
 
 $hive = 'HKLM:'
 
@@ -23,9 +28,62 @@ if ($setting -ne $null) {
 popd
 
 Write-Output $setting
-<#
-This does not work with IE 8 
-#>
+write-output 'Detect Internet Explorer Developer Channel'
+$hive = 'HKCU:'
+$path = '/Software/Microsoft/AppV/Client/Integration/Packages/{9BD02EED-6C11-4FF0-8A3E-0B4733EE86A1}'
+$path = '/Software/Microsoft/AppV/Client/Integration/Packages'
+
+$name = 'Integration Location'
+$value = $null
+
+
+pushd $hive
+cd $path
+$app_record = Get-ChildItem . | Where-Object { $_.Name -match '9BD02EED-6C11-4FF0-8A3E-0B4733EE86A1' } | Select-Object -First 1
+$app_record_fields = $app_record.GetValueNames()
+if (-not ($app_record_fields.GetType().BaseType.Name -match 'Array')) {
+  Write-Output 'Unexpected result type'
+}
+# not good for mixed types 
+$results = $app_record_fields | ForEach-Object { @{ 'name' = $_; 'expression' = & { $app_record.GetValue($_) } } };
+
+$results_lookup = @{}
+$results | ForEach-Object { $results_lookup[$_.Name] = $_.expression }
+$fields = @()
+$fields += $results_lookup.Keys
+$fields | ForEach-Object { $key = $_;
+  if (-not ($key -match '(?:Staged|Integration) Location')) {
+    $results_lookup.Remove($key)
+  }
+}
+
+popd
+
+$results_lookup | Format-Table -AutoSize
+
+if (-not [environment]::Is64BitProcess) {
+  $path = '/SOFTWARE/Microsoft/AppV/Client/Packages/9BD02EED-6C11-4FF0-8A3E-0B4733EE86A1/Versions/681E2361-2C6F-4D47-A8B7-D3F7B288CB45/REGISTRY/MACHINE/Software/Microsoft/Internet Explorer'
+
+} else {
+# TODO:
+#  $path = '/SOFTWARE/Wow6432Node/Microsoft/Internet Explorer' 
+}
+
+$hive = 'HKLM:'
+
+$name = 'svcVersion'
+$value = '0'
+
+
+pushd $hive
+cd $path
+$setting = Get-ItemProperty -Path ('{0}/{1}' -f $hive,$path) -Name $name -ErrorAction 'SilentlyContinue'
+if ($setting -ne $null) {
+  $setting = $setting.svcVersion
+}
+popd
+
+Write-Output $setting
 
 
 # Detect Internet Explorer Developer Channel
