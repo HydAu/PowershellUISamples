248ad5ff2d247bfb1d9b153153d680f17a3ed53a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-12-06 18:47:10 -0500

    pretty-print

--- unfinished/grouped_list_demo.ps1
+++ unfinished/grouped_list_demo.ps1
@@ -60,21 +60,21 @@ public class Win32Window : IWin32Window
 
 # http://stackoverflow.com/questions/8343767/how-to-get-the-current-directory-of-the-cmdlet-being-executed
 function Get-ScriptDirectory {
-  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
-  if ($Invocation.PSScriptRoot) {
-    $Invocation.PSScriptRoot
+  $invocation = (Get-Variable MyInvocation -Scope 1).Value
+  if ($invocation.PSScriptRoot) {
+    $invocation.PSScriptRoot
   }
-  elseif ($Invocation.MyCommand.Path) {
-    Split-Path $Invocation.MyCommand.Path
+  elseif ($invocation.MyCommand.Path) {
+    Split-Path $invocation.MyCommand.Path
   } else {
-    $Invocation.InvocationName.Substring(0,$Invocation.InvocationName.LastIndexOf(""))
+    $invocation.InvocationName.Substring(0,$invocation.InvocationName.LastIndexOf(""))
   }
 }
 
-$shared_assemblies = @{ 
+$shared_assemblies = @{
   'nunit.core.dll' = $null;
   'nunit.framework.dll' = $null;
-  'GroupedListControl.dll' = $null ;
+  'GroupedListControl.dll' = $null;
 }
 
 $shared_assemblies_path = 'c:\developer\sergueik\csharp\SharedAssemblies'
@@ -110,69 +110,73 @@ popd
 
 function GroupedListBox
 {
-     Param(
-	[String] $title, 
-	[String] $message)
+  param(
+    [string]$title)
 
-  [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Drawing') 
-  [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Collections.Generic') 
-  [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Collections') 
-  [void] [System.Reflection.Assembly]::LoadWithPartialName('System.ComponentModel') 
-  [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
-  [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Text') 
-  [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Data') 
+  [void][System.Reflection.Assembly]::LoadWithPartialName('System.Drawing')
+  [void][System.Reflection.Assembly]::LoadWithPartialName('System.Collections.Generic')
+  [void][System.Reflection.Assembly]::LoadWithPartialName('System.Collections')
+  [void][System.Reflection.Assembly]::LoadWithPartialName('System.ComponentModel')
+  [void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+  [void][System.Reflection.Assembly]::LoadWithPartialName('System.Text')
+  [void][System.Reflection.Assembly]::LoadWithPartialName('System.Data')
 
-  $f = New-Object System.Windows.Forms.Form 
+  $f = New-Object System.Windows.Forms.Form
 
   $f.Text = $title
   $width = 700
   $f.Size = New-Object System.Drawing.Size ($width,400)
   $f.SuspendLayout()
 
-if ($PSBoundParameters["show_buttons"]) {
-  try {
-} catch [Exception] { 
-}
-}
+  if ($PSBoundParameters["show_buttons"]) {
+    try {
+    } catch [exception]{
+    }
+  }
 
-// http://www.codeproject.com/Articles/451742/Extending-Csharp-ListView-with-Collapsible-Groups
-// http://www.codeproject.com/Articles/451735/Extending-Csharp-ListView-with-Collapsible-Groups
-$glc = new-object -TypeName 'GroupedListControl.GroupListControl'
-$glc.SuspendLayout()
-$glc.Size=$f.Size
-$glc.Anchor = ([System.Windows.Forms.AnchorStyles]( 0 `
-    -bor [System.Windows.Forms.AnchorStyles]::Top     `
-    -bor [System.Windows.Forms.AnchorStyles]::Bottom  `
-    -bor [System.Windows.Forms.AnchorStyles]::Left    `
-    -bor [System.Windows.Forms.AnchorStyles]::Right   `
-))
+  # http://www.codeproject.com/Articles/451742/Extending-Csharp-ListView-with-Collapsible-Groups
+  # http://www.codeproject.com/Articles/451735/Extending-Csharp-ListView-with-Collapsible-Groups
+  $glc = New-Object -TypeName 'GroupedListControl.GroupListControl'
+  $glc.SuspendLayout()
+  $glc.Size = $f.Size
+  $glc.Anchor = ([System.Windows.Forms.AnchorStyles](0 `
+         -bor [System.Windows.Forms.AnchorStyles]::Top `
+         -bor [System.Windows.Forms.AnchorStyles]::Bottom `
+         -bor [System.Windows.Forms.AnchorStyles]::Left `
+         -bor [System.Windows.Forms.AnchorStyles]::Right `
+      ))
 
-<#
-            glc.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                | System.Windows.Forms.AnchorStyles.Left)
-                | System.Windows.Forms.AnchorStyles.Right)))
-  #>          
-            for($i = 1; $i -le 5; $i++)
-            {
-                [GroupedListControl.ListGroup]$lg = new-object  -TypeName 'GroupedListControl.ListGroup'
-                $lg.Columns.Add("List Group " + $i.ToString(), 120)
-                $lg.Columns.Add("Group " + $i + " SubItem 1", 150)
-                $lg.Columns.Add("Group " + $i + " Subitem 2", 150)
-                $lg.Name = ( "Group " + $i )
-            # add some sample items:
-            for($j = 1; $j -le 5; $j++)
-                {
-                    [System.Windows.Forms.ListViewItem]$item = $lg.Items.Add(("Item " + $j.ToString()))
-                    $item.SubItems.Add($item.Text + " SubItem 1")
-                    $item.SubItems.Add($item.Text + " SubItem 2")
-                }
+  for ($group = 1; $group -le 5; $group++)
+  {
+    [GroupedListControl.ListGroup]$lg = New-Object -TypeName 'GroupedListControl.ListGroup'
+    $lg.Columns.Add("List Group " + $group.ToString(), 120 )
+    $lg.Columns.Add("Group " + $group + " SubItem 1", 150 )
+    $lg.Columns.Add("Group " + $group + " Subitem 2", 150 )
+    $lg.Name = ("Group " + $group)
+    # add some sample items:
+    for ($j = 1; $j -le 5; $j++){
+      [System.Windows.Forms.ListViewItem]$item = $lg.Items.Add(("Item " + $j.ToString()))
+      $item.SubItems.Add($item.Text + " SubItem 1")
+      $item.SubItems.Add($item.Text + " SubItem 2")
+    }
 
-            # Add handling for the columnRightClick Event
-            #    lg.ColumnRightClick += new ListGroup.ColumnRightClickHandler(lg_ColumnRightClick)
-            #    lg.MouseClick += new MouseEventHandler(lg_MouseClick)
+    # Add handling for the columnRightClick Event. 
+    # Note: It is unfinished in the original GroupedListBox
+    #    lg.ColumnRightClick += new ListGroup.ColumnRightClickHandler(lg_ColumnRightClick)
+    #    lg.MouseClick += new MouseEventHandler(lg_MouseClick)
+    # TODO: http://msdn.microsoft.com/en-us/library/system.windows.forms.listview.columnclick%28v=vs.110%29.aspx
+    <#
+     private void ColumnClick(object o, ColumnClickEventArgs e)
+        {
+            // Set the ListViewItemSorter property to a new ListViewItemComparer  
+            // object. Setting this property immediately sorts the  
+            // ListView using the ListViewItemComparer object. 
+            this.listView1.ListViewItemSorter = new ListViewItemComparer(e.Column);
+        }
 
-                $glc.Controls.Add($lg)
-}
+    #>
+    $glc.Controls.Add($lg)
+  }
 
   $f.Controls.Add($glc)
   $glc.ResumeLayout($false)
@@ -185,11 +189,11 @@ $glc.Anchor = ([System.Windows.Forms.AnchorStyles]( 0 `
 
   $f.Topmost = $True
 
-  $caller = New-Object -typeName 'Win32Window' -ArgumentList ([System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle)
+  $caller = New-Object -TypeName 'Win32Window' -ArgumentList ([System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle)
 
-  $f.Add_Shown( { $f.Activate() } )
+  $f.Add_Shown({ $f.Activate() })
 
-  [Void] $f.ShowDialog([Win32Window] ($caller) )
+  [void]$f.ShowDialog([win32window]($caller))
   $f.Dispose()
   $result = $caller.Message
   $caller = $null
@@ -197,4 +201,4 @@ $glc.Anchor = ([System.Windows.Forms.AnchorStyles]( 0 `
 }
 
 $DebugPreference = 'Continue'
-[void]GroupedListBox ''  ''
+GroupedListBox -Title '' | Out-Null
