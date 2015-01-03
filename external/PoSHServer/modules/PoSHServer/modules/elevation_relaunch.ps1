58ca6450743ca0db998642843537563182257f98
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-01-03 17:43:05 -0500

    formatting only

--- external/PoSHServer/modules/PoSHServer/modules/elevation_relaunch.ps1
+++ external/PoSHServer/modules/PoSHServer/modules/elevation_relaunch.ps1
@@ -1,34 +1,34 @@
 # http://blogs.msdn.com/b/virtual_pc_guy/archive/2010/09/23/a-self-elevating-powershell-script.aspx
 # 
 # Get the ID and security principal of the current user account
-$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
-$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+$myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal ($myWindowsID)
 
 # Get the security principal for the Administrator role
-$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator 
+$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
 
 # Check to see if we are currently running "as Administrator"
 if ($myWindowsPrincipal.IsInRole($adminRole))
-   {
-   # We are running "as Administrator" - so change the title and background color to indicate this
-   $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
-   $Host.UI.RawUI.BackgroundColor = "DarkBlue"
-   clear-host
-   }
+{
+  # We are running "as Administrator" - so change the title and background color to indicate this
+  $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+  $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+  Clear-Host
+}
 else
-   {
-   # We are not running "as Administrator" - so relaunch as administrator
-   # Create a new process object that starts PowerShell
-   $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
-   # Specify the current script path and name as a parameter
-   $newProcess.Arguments = $myInvocation.MyCommand.Definition;
-   # Indicate that the process should be elevated
-   $newProcess.Verb = "runas";
-   # Start the new process
-   [System.Diagnostics.Process]::Start($newProcess);
-   # Exit from the current, unelevated, process
-   exit
-   }
+{
+  # We are not running "as Administrator" - so relaunch as administrator
+  # Create a new process object that starts PowerShell
+  $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+  # Specify the current script path and name as a parameter
+  $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+  # Indicate that the process should be elevated
+  $newProcess.Verb = "runas";
+  # Start the new process
+  [System.Diagnostics.Process]::Start($newProcess);
+  # Exit from the current, unelevated, process
+  exit
+}
 
 # http://stackoverflow.com/questions/8343767/how-to-get-the-current-directory-of-the-cmdlet-being-executed
 function Get-ScriptDirectory {
@@ -46,16 +46,16 @@ function Get-ScriptDirectory {
 
 
 # Run your code that needs to be elevated here
-Write-Host -NoNewLine "Press any key to continue..."
-$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-$root =  [System.IO.Path]::Combine( (Get-ScriptDirectory), "..\..\..")
-pushd  $root
+Write-Host -NoNewline "Press any key to continue..."
+[void]($Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown"))
+$root = [System.IO.Path]::Combine((Get-ScriptDirectory),"..\..\..")
+pushd $root
 
-import-module ./PoSHServer.psd1
-dir
-Write-Host -NoNewLine "Press any key to continue..."
-$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Import-Module ./PoSHServer.psd1
+Get-ChildItem
+Write-Host -NoNewline "Press any key to continue..."
+[void]($Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown"))
 
 start-PoshServer
-Write-Host -NoNewLine "Press any key to continue..."
-$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Write-Host -NoNewline "Press any key to continue..."
+[void]($Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown"))
