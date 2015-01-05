6cbd3cbf8d75c2b4a1a884219ea27e1b5dce76f9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-01-05 08:15:26 -0500

    refactoring only

--- selenium/selenium_dragdrop2.ps1
+++ selenium/selenium_dragdrop2.ps1
@@ -43,9 +43,11 @@ $shared_assemblies = @(
 
 )
 
-$env:SHARED_ASSEMBLIES_PATH = "c:\developer\sergueik\csharp\SharedAssemblies"
+$shared_assemblies_path = 'c:\developer\sergueik\csharp\SharedAssemblies'
 
-$shared_assemblies_path = $env:SHARED_ASSEMBLIES_PATH
+if (($env:SHARED_ASSEMBLIES_PATH -ne $null) -and ($env:SHARED_ASSEMBLIES_PATH -ne '')) {
+  $shared_assemblies_path = $env:SHARED_ASSEMBLIES_PATH
+}
 pushd $shared_assemblies_path
 $shared_assemblies | ForEach-Object { Unblock-File -Path $_; Add-Type -Path $_ }
 popd
