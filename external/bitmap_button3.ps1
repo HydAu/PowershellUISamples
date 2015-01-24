17514b12944b45786d2ae6102c3f81c38a18ff16
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-01-24 17:42:39 -0500

    fixed callback code

--- external/bitmap_button3.ps1
+++ external/bitmap_button3.ps1
@@ -25,6 +25,33 @@ $f = New-Object System.Windows.Forms.Form
 $l1 = New-Object System.Windows.Forms.Label
 $l2 = New-Object System.Windows.Forms.Label
 $o1 = New-Object -TypeName System.Windows.Forms.Button
+$o2 = New-Object -TypeName System.Windows.Forms.Button
+
+# label1
+$l1.BorderStyle = [System.Windows.Forms.BorderStyle]::None
+$l1.Location = New-Object System.Drawing.Point (12,39)
+$l1.Name = 'l1'
+$l1.Text = ''
+$l1.Size = New-Object System.Drawing.Size (172,23)
+$l1.TabIndex = 4
+
+# label2
+$l2.BorderStyle = [System.Windows.Forms.BorderStyle]::None
+$l2.Location = New-Object System.Drawing.Point (12,70)
+$l2.Name = "l2"
+$l2.Text = ''
+$l2.Size = New-Object System.Drawing.Size (172,23)
+$l2.TabIndex = 4
+
+# dDControls
+$o1.Name = "b1"
+$o1.TabIndex = 1
+$global:m = @{
+  'b1' = 'l1';
+  'b2' = 'l2';
+}
+
+
 
 $o1.Location = New-Object System.Drawing.Point (200,32)
 $o1.Size = New-Object System.Drawing.Size (32,32)
@@ -33,6 +60,25 @@ $o1.TabIndex = 1
 $o1.Text = "b1"
 
 # $o1.SetStyle([System.Windows.Forms.ControlStyles]::UserPaint -bor [System.Windows.Forms.ControlStyles]::AllPaintingInWmPaint -bor [System.Windows.Forms.ControlStyles]::DoubleBuffer,$true)
+# http://www.alkanesolutions.co.uk/2013/04/19/embedding-base64-image-strings-inside-a-powershell-application/
+$do = 'iVBORw0KGgoAAAANSUhEUgAAAKAAAAAgBAMAAABnUW7GAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAwUExURQAAAIAAAACAAICAAAAAgIAAgACAgMDAwICAgP8AAAD/AP//AAAA//8A/wD//////08TJkkAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAFHSURBVEjH7daxjsMgDAbgKAvZ8g5dbuTVrFvMlmxku1fNRre7EmwCbk0lel1Ox/bLzidboWqGfajO/Goe5u/q7NNWnV3WZTZYHZDgWoNfElhlvgdDmd06+TIvu6zL/A++BwS+ROYxWNVlvoGO74RnkB8PBOZrSuCFTiCw7I8gZwaRxl545ZEASyuPHGnlst8k6NgfNRDyRJ0g8AYKCBwJLPsN5p29CtJIFhvgRwtMOyyogSnb89oY/LxQv2EqsQoIPFEvCLSxBgJFBiGuHE7QZVUBj5HiRDqITTDuvKAOxmzxBIt+w+8jvRkNBJqoG4S0gQpCihk8+xPo+HZr4G2kY6JuEM2CLRBHiyV49tPP0NPlVkFIE/WDENpgrstMoPNPQJzxNZCOCub6X/iTulbfHuskv2VEXeZ7cBMPSFDWtyfg737ODfMP1mxvUDAf+WQAAAAASUVORK5CYII='
+
+$i = [convert]::FromBase64String($do)
+$s = New-Object IO.MemoryStream ($i,0,$i.Length)
+$s.Write($i,0,$i.Length);
+
+$o1.Image = [System.Drawing.Image]::FromStream($s,$true)
+
+$o2.Location = New-Object System.Drawing.Point (200,70)
+$o2.Size = New-Object System.Drawing.Size (32,32)
+$o2.TabIndex = 2
+$o1.Name = 'b2'
+$o2.Text = "b2"
+$o2.Image = $o1.Image
+
+
+$f.SuspendLayout()
+
 
 $btnState = @{
   'BUTTON_UP' = 0;
@@ -45,13 +91,10 @@ $btnState = @{
 Add-Member -InputObject $o1 -MemberType 'NoteProperty' -Name 'ImgState' -Value $btnState['BUTTON_UP'] -Force
 Add-Member -InputObject $o1 -MemberType 'NoteProperty' -Name 'mouseEnter' -Value $false -Force
 
-$do = 'iVBORw0KGgoAAAANSUhEUgAAAKAAAAAgBAMAAABnUW7GAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAwUExURQAAAIAAAACAAICAAAAAgIAAgACAgMDAwICAgP8AAAD/AP//AAAA//8A/wD//////08TJkkAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAFHSURBVEjH7daxjsMgDAbgKAvZ8g5dbuTVrFvMlmxku1fNRre7EmwCbk0lel1Ox/bLzidboWqGfajO/Goe5u/q7NNWnV3WZTZYHZDgWoNfElhlvgdDmd06+TIvu6zL/A++BwS+ROYxWNVlvoGO74RnkB8PBOZrSuCFTiCw7I8gZwaRxl545ZEASyuPHGnlst8k6NgfNRDyRJ0g8AYKCBwJLPsN5p29CtJIFhvgRwtMOyyogSnb89oY/LxQv2EqsQoIPFEvCLSxBgJFBiGuHE7QZVUBj5HiRDqITTDuvKAOxmzxBIt+w+8jvRkNBJqoG4S0gQpCihk8+xPo+HZr4G2kY6JuEM2CLRBHiyV49tPP0NPlVkFIE/WDENpgrstMoPNPQJzxNZCOCub6X/iTulbfHuskv2VEXeZ7cBMPSFDWtyfg737ODfMP1mxvUDAf+WQAAAAASUVORK5CYII='
 
-$i = [convert]::FromBase64String($do)
-$s = New-Object IO.MemoryStream ($i,0,$i.Length)
-$s.Write($i,0,$i.Length);
+Add-Member -InputObject $o2 -MemberType 'NoteProperty' -Name 'ImgState' -Value $btnState['BUTTON_UP'] -Force
+Add-Member -InputObject $o2 -MemberType 'NoteProperty' -Name 'mouseEnter' -Value $false -Force
 
-$o1.Image = [System.Drawing.Image]::FromStream($s,$true)
 
 $button_OnPaint = {
   param([object]$sender,[System.Windows.Forms.PaintEventArgs]$e)
@@ -75,6 +118,8 @@ $button_OnPaint = {
 }
 
 $o1.add_Paint($button_OnPaint)
+$o2.add_Paint($button_OnPaint)
+
 $button_OnGotFocus = {
   param(
     [object]$sender,[System.EventArgs]$e
@@ -83,6 +128,7 @@ $button_OnGotFocus = {
 
 }
 $o1.add_GotFocus($button_OnGotFocus)
+$o2.add_GotFocus($button_OnGotFocus)
 
 $button_OnLostFocus = {
   param(
@@ -100,6 +146,7 @@ $button_OnLostFocus = {
 
 }
 $o1.add_LostFocus($button_OnLostFocus)
+$o2.add_LostFocus($button_OnLostFocus)
 
 $button_OnMouseEnter = {
   param(
@@ -115,6 +162,7 @@ $button_OnMouseEnter = {
 
 }
 $o1.add_MouseEnter($button_OnMouseEnter)
+$o2.add_MouseEnter($button_OnMouseEnter)
 
 $button_OnMouseLeave = {
   param(
@@ -130,50 +178,8 @@ $button_OnMouseLeave = {
 
 }
 $o1.add_MouseLeave($button_OnMouseLeave)
+$o2.add_MouseLeave($button_OnMouseLeave)
 
-
-# http://www.alkanesolutions.co.uk/2013/04/19/embedding-base64-image-strings-inside-a-powershell-application/
-
-<#
-
-$o2 = New-Object -TypeName 'BitmapButton.BitmapButton'
-
-$o2.Location = New-Object System.Drawing.Point (200,70)
-$o2.Size = New-Object System.Drawing.Size (32,32)
-$o2.TabIndex = 2
-$o1.Name = 'b2'
-$o2.Text = "b2"
-$o2.Image = $o1.Image
-
-#>
-
-$f.SuspendLayout()
-
-# label1
-$l1.BorderStyle = [System.Windows.Forms.BorderStyle]::None
-$l1.Location = New-Object System.Drawing.Point (12,39)
-$l1.Name = 'l1'
-$l1.Text = ''
-$l1.Size = New-Object System.Drawing.Size (172,23)
-$l1.TabIndex = 4
-
-<#
-# label2
-$l2.BorderStyle = [System.Windows.Forms.BorderStyle]::None
-$l2.Location = New-Object System.Drawing.Point (12,70)
-$l2.Name = "l2"
-$l2.Text = ''
-$l2.Size = New-Object System.Drawing.Size (172,23)
-$l2.TabIndex = 4
-#>
-
-# dDControls
-$o1.Name = "b1"
-$o1.TabIndex = 1
-$global:m = @{
-  'b1' = 'l1';
-  'b2' = 'l2';
-}
 
 function find_label {
   param([string]$button_name)
@@ -202,7 +208,8 @@ $button_OnMouseDown = {
 }
 
 $o1.add_MouseDown($button_OnMouseDown)
-# $o2.add_MouseDown($button_OnMouseDown)
+$o2.add_MouseDown($button_OnMouseDown)
+
 $button_OnMouseUp = {
   param(
     [object]$sender,[System.Windows.Forms.MouseEventArgs]$e
@@ -222,15 +229,27 @@ $button_OnMouseUp = {
 }
 
 $o1.add_MouseUp($button_OnMouseUp)
-# $o2.add_MouseUp($button_OnMouseUp)
+$o2.add_MouseUp($button_OnMouseUp)
+
 $button_OnEnabledChanged = {
   param(
     [object]$sender,[System.EventArgs]$e
   )
+
+            if ($sender.Enabled)
+            {
+                $sender.imgState=$btnState['BUTTON_UP']
+            }
+            else
+            {
+                $sender.imgState=btnState['BUTTON_DISABLED']
+            }
+            $sender.Invalidate()
+
 }
 
 $o1.add_EnabledChanged($button_OnEnabledChanged)
-# $o2.add_EnabledChanged($button_OnEnabledChanged)
+$o2.add_EnabledChanged($button_OnEnabledChanged)
 
 $button_OnKeyDown = {
   param(
@@ -251,7 +270,8 @@ $button_OnKeyDown = {
 }
 
 $o1.add_KeyDown($button_OnKeyDown)
-# $o2.add_KeyDown($button_OnKeyDown)
+$o2.add_KeyDown($button_OnKeyDown)
+
 $button_OnKeyUp = {
   param(
     [object]$sender,[System.Windows.Forms.KeyEventArgs]$e
@@ -271,14 +291,14 @@ $button_OnKeyUp = {
 }
 
 $o1.add_KeyUp($button_OnKeyUp)
-# $o2.add_KeyUp($button_OnKeyUp)
+$o2.add_KeyUp($button_OnKeyUp)
 
 # Form
 $f.AutoScaleDimensions = New-Object System.Drawing.SizeF (6.0,13.0)
 $f.AutoScaleMode = [System.Windows.Forms.AutoScaleMode]::Font
 $f.ClientSize = New-Object System.Drawing.Size (263,109)
-# $f.Controls.AddRange(@( $l1,$l2,$o1,$o2))
-$f.Controls.AddRange(@( $l1,$o1))
+$f.Controls.AddRange(@( $l1,$l2,$o1,$o2))
+# $f.Controls.AddRange(@( $l1,$o1))
 $f.Name = 'form'
 $f.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
 $f.Text = 'Bitmap Button Demo'
