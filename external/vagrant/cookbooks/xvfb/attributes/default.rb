85b218492168afa9044964e14e2ce101fb098d1c
sergueik <sergueik@sergueik41.(none)>
2015-02-12 14:13:59 -0500

    initial revision

---
+++ external/vagrant/cookbooks/xvfb/attributes/default.rb
@@ -0,0 +1,5 @@
+default['display_driver']['install_flavor'] = \
+'xvfb'
+
+default['selenium']['url'] = \
+'http://selenium-release.storage.googleapis.com/2.44/selenium-server-standalone-2.44.0.jar'
