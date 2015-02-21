ed6fbcedccd652dfb5898cd3e60e40959dd9ebf7
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-02-21 12:24:04 -0500

    adding more complex tree elements

--- external/treetab_control_test.ps1
+++ external/treetab_control_test.ps1
@@ -34,6 +34,39 @@ function Get-ScriptDirectory
   }
 }
 
+$verificationErrors = New-Object System.Text.StringBuilder
+
+Add-Type -AssemblyName PresentationFramework
+
+
+# requires -version 2
+# origin: http://www.codeproject.com/Articles/68748/TreeTabControl-A-Tree-of-Tab-Items
+
+[xml]$xaml =
+@"
+<?xml version="1.0"?>
+<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml" xmlns:custom="clr-namespace:TreeTab;assembly=TreeTab" Title="Window1" Margin="0,0,0,0">
+  <Grid x:Name="Container">
+    <Grid.RowDefinitions>
+      <RowDefinition Height="30"/>
+      <RowDefinition Height="*"/>
+    </Grid.RowDefinitions>
+    <Grid>
+      <Grid.ColumnDefinitions>
+        <ColumnDefinition/>
+        <ColumnDefinition/>
+      </Grid.ColumnDefinitions>
+      <Button x:Name="Hide_Tree" Grid.Column="1">Hide Tree</Button>
+      <Button x:Name="Show_Tree" Grid.Column="0">Show Tree</Button>
+    </Grid>
+    <Grid x:Name="Container2" Grid.Row="1" Margin="5,5,5,5">
+      <StackPanel x:Name="TreeTabContainer"></StackPanel>
+    </Grid>
+  </Grid>
+</Window>
+"@
+
+
 $shared_assemblies = @(
   'TreeTab.dll',
   'nunit.framework.dll'
@@ -50,63 +83,6 @@ pushd $shared_assemblies_path
 $shared_assemblies | ForEach-Object { Unblock-File -Path $_; Add-Type -Path $_ }
 popd
 
-$verificationErrors = New-Object System.Text.StringBuilder
-
-Add-Type -AssemblyName PresentationFramework
-
-
-# origin: http://www.codeproject.com/Articles/68748/TreeTabControl-A-Tree-of-Tab-Items
-# requires -version 2
-<#
-Exception calling "Load" with "1" argument(s): "Specified class name
-'TreeTabTest.Window1' doesn't match actual root instance type
-'System.Windows.Window'. Remove the Class directive or provide an instance via
-XamlObjectWriterSettings.RootObjectInstance."
-
-Exception calling "Load" with "1" argument(s): "Cannot create unknown type
-'{clr-namespace:TreeTab;assembly=TreeTab}TreeTabControl'."
-#>
-Add-Type -AssemblyName PresentationFramework
-[xml]$xaml =
-@"
-<?xml version="1.0"?>
-<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml" xmlns:custom="clr-namespace:TreeTab;assembly=TreeTab" Title="Window1" Margin="0,0,0,0">
-  <Grid x:Name="Container">
-    <Grid.RowDefinitions>
-      <RowDefinition Height="50"/>
-      <RowDefinition Height="*"/>
-    </Grid.RowDefinitions>
-    <Grid>
-      <Grid.ColumnDefinitions>
-        <ColumnDefinition/>
-        <ColumnDefinition/>
-        <ColumnDefinition/>
-        <ColumnDefinition/>
-        <ColumnDefinition/>
-        <ColumnDefinition/>
-        <ColumnDefinition/>
-      </Grid.ColumnDefinitions>
-      <Button x:Name="btnAddTab1" Grid.Column="0">Add Main Tab</Button>
-      <Button x:Name="btnAddTab2" Grid.Column="1">Add Closable Group Tab</Button>
-      <Button x:Name="btnAddTab3" Grid.Column="2">Add Children to Tab Group</Button>
-      <Button x:Name="btnAddTab4" Grid.Column="3">Add Group Tab</Button>
-      <Button x:Name="btnAddTab5" Grid.Column="4">Add Children to Tab Group</Button>
-      <Button x:Name="btnCollapseTree" Grid.Column="5">Collapse Tree</Button>
-      <Button x:Name="btnExpandTree" Grid.Column="6">Expand Tree</Button>
-    </Grid>
-    <Grid x:Name="Container2" Grid.Row="1" Margin="5,5,5,5">
-      <StackPanel Height="100" x:Name="TreeTabContainer">
-
-  </StackPanel>
-      <!--
-            <custom:TreeTabControl Name="treeTab" IsTreeExpanded="True">
-                
-            </custom:TreeTabControl>
--->
-    </Grid>
-  </Grid>
-</Window>
-"@
 
 Clear-Host
 
@@ -115,42 +91,67 @@ $reader = (New-Object System.Xml.XmlNodeReader $xaml)
 $target = [Windows.Markup.XamlReader]::Load($reader)
 
 $t = New-Object -TypeName 'TreeTab.TreeTabControl'
-# $t | get-member
-# $i = New-Object -TypeName 'TreeTab.TreeItem'  # the TREEITEM_TYPE struct seems to be not visible 
+# $i = New-Object -TypeName 'TreeTab.TreeItem'  # the TREEITEM_TYPE struct seems to not be accessible 
 
 $c = $target.FindName("TreeTabContainer")
 $t.IsTreeExpanded = $true
 $t.Name = "treeTab"
 [void]$t.HideTree()
-[void]$t.AddTabItem("One","Main Tab One",$false,'MAIN','')
-[void]$t.AddTabItem("Two","Group Tab",$true,'GROUP','')
+[void]$t.AddTabItem('Global','Global',$false,'MAIN','')
+[void]$t.AddTabItem('Staging_Environment','Staging Environment',$false,'GROUP','')
+[void]$t.AddTabItem('Test_Environment','Test Environment',$false,'GROUP','')
+[void]$t.AddTabItem('UAT_Environment','UAT Environment',$false,'GROUP','')
 
-[void]$t.AddTabItem("Three","Main Tab Three",$false,'MAIN','')
-[void]$t.AddTabItem("Four","Main Tab Four",$false,'MAIN','')
-[void]$t.AddTabItem("Five", "Group without close", $false, 'GROUP', "This tab cannot be closed!", $null);
-[void]$t.AddTabItem("seven", "Child Group 1", $true, 'MAIN', "I belong to my parent!", [TreeTab.TreeTabItemGroup]($t.GetTabItemById("Five")))
+[void]$t.AddTabItem('Group_with_close','Group with close',$true,'GROUP','This tab can be closed!');
+[void]$t.AddTabItem('6','Child Group 6',$false,'MAIN','I belong to my parent!',[TreeTab.TreeTabItemGroup]($t.GetTabItemById('Group_with_close')))
 
 
-[TreeTab.TreeTabItemGroup]$tParent = [TreeTab.TreeTabItemGroup]$t.GetTabItemById("Two")
-[TreeTab.TreeTabItem]$tItem = $t.AddTabItem("tree","Child Group 1",$true,'MAIN',$tParent)
+[TreeTab.TreeTabItemGroup]$tp0 = [TreeTab.TreeTabItemGroup]$t.GetTabItemById('Staging_Environment')
+[TreeTab.TreeTabItem]$tItem = $t.AddTabItem('Certificates','Certificates',$false,'MAIN',$tp0)
+[void]$t.AddTabItem('IIS_Web_Sites','IIS Web Sites',$false,'GROUP',$tp0)
+[void]$t.AddTabItem('Databases','Databases',$false,'GROUP',$tp0)
 
-[void]$t.AddTabItem("four","Child Group 2",$true,'MAIN',$tParent)
-[void]$t.AddTabItem("five","Child Group 3",$true,'MAIN',$tParent)
+[TreeTab.TreeTabItemGroup]$tp02 = [TreeTab.TreeTabItemGroup]$t.GetTabItemById('Databases')
+[void]$t.AddTabItem('DB_1','DB 1',$true,'MAIN',$tp02)
+[void]$t.AddTabItem('DB_2','DB 2',$true,'MAIN',$tp02)
+
+[TreeTab.TreeTabItemGroup]$tp03 = [TreeTab.TreeTabItemGroup]$t.GetTabItemById('IIS_Web_Sites')
+[void]$t.AddTabItem('Site_1','Site 1',$true,'MAIN',$tp03)
+[void]$t.AddTabItem('Site_2','Site 2',$true,'MAIN',$tp03)
+[void]$t.AddTabItem('Site_3','Site 3',$true,'MAIN',$tp03)
+[void]$t.AddTabItem('Site_4','Site 4',$true,'MAIN',$tp03)
+
+
+[TreeTab.TreeTabItemGroup]$tp01 = [TreeTab.TreeTabItemGroup]$t.GetTabItemById('Test_Environment')
+[TreeTab.TreeTabItem]$t23 = $t.AddTabItem('Certificates1','Certificates',$false,'MAIN',$tp01)
+[void]$t.AddTabItem('IIS_Web_Sites2','IIS Web Sites',$false,'GROUP',$tp01)
+[void]$t.AddTabItem('Databases2','Databases',$false,'GROUP',$tp01)
+
+
+[TreeTab.TreeTabItemGroup]$tp12 = [TreeTab.TreeTabItemGroup]$t.GetTabItemById('Databases2')
+[void]$t.AddTabItem('DB_11','DB 1',$true,'MAIN',$tp12)
+[void]$t.AddTabItem('DB_12','DB 2',$true,'MAIN',$tp12)
+
+[TreeTab.TreeTabItemGroup]$tp13 = [TreeTab.TreeTabItemGroup]$t.GetTabItemById('IIS_Web_Sites2')
+[void]$t.AddTabItem('Site_11','Site 1',$true,'MAIN',$tp13)
+[void]$t.AddTabItem('Site_12','Site 2',$true,'MAIN',$tp13)
+[void]$t.AddTabItem('Site_13','Site 3',$true,'MAIN',$tp13)
+[void]$t.AddTabItem('Site_14','Site 4',$true,'MAIN',$tp13)
+
+
 [void]$t.ShowTree()
 [void]$c.AddChild($t)
 
-
-$bc = $target.FindName("btnAddTab1")
-$bc.add_click.Invoke({
-    # $target.Title = "Hello $((Get-Date).ToString('G'))" 
-    $t.AddTabItem("one","Main Tab",$false,[TreeTab.TreeItem]::TREEITEM_TYPE.MAIN,'')
+$target.FindName("Hide_Tree").add_click.Invoke({
+    [void]$t.HideTree()
   })
-$bc = $null
+$target.FindName("Show_Tree").add_click.Invoke({
+    [void]$t.ShowTree()
+  })
 
 $target.ShowDialog() | Out-Null
 <#
 TODO: handle 
 Execution of the script frequently leads  to
 Exception calling "ShowDialog" with "0" argument(s): "Not enough quota is available to process this command"
-
 #>
