d276ffbf7ac97316ddbd8176752407b2b0958b6f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-02-22 20:03:19 -0500

    merged

--- selenium/with_profile.ps1
+++ selenium/with_profile.ps1
@@ -1,4 +1,4 @@
-#Copyright (c) 2014 Serguei Kouzmine
+#Copyright (c) 2014,2015 Serguei Kouzmine
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -18,31 +18,8 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #THE SOFTWARE.
 
-param(
-  [string]$hub_host = '127.0.0.1',
-  [string]$browser,
-  [string]$version,
-  [string]$profile = 'Selenium'
-)
-
-function set_timeouts {
-  param(
-    [System.Management.Automation.PSReference]$selenium_ref,
-    [int]$explicit = 10,
-    [int]$page_load = 60,
-    [int]$script = 30
-  )
-
-  [void]($selenium_ref.Value.Manage().Timeouts().ImplicitlyWait([System.TimeSpan]::FromSeconds($explicit)))
-  [void]($selenium_ref.Value.Manage().Timeouts().SetPageLoadTimeout([System.TimeSpan]::FromSeconds($pageload)))
-  [void]($selenium_ref.Value.Manage().Timeouts().SetScriptTimeout([System.TimeSpan]::FromSeconds($script)))
-
-}
-
-
 # http://stackoverflow.com/questions/8343767/how-to-get-the-current-directory-of-the-cmdlet-being-executed
-function Get-ScriptDirectory
-{
+function Get-ScriptDirectory {
   $Invocation = (Get-Variable MyInvocation -Scope 1).Value
   if ($Invocation.PSScriptRoot) {
     $Invocation.PSScriptRoot
@@ -54,29 +31,25 @@ function Get-ScriptDirectory
   }
 }
 
-function cleanup
+# TODO: local connections only ?
+function netstat_check
 {
   param(
-    [System.Management.Automation.PSReference]$selenium_ref
+    [string]$selenium_http_port = 4444
   )
-  try {
-    $selenium_ref.Value.Quit()
-  } catch [exception]{
-    # Ignore errors if unable to close the browser
-    Write-Output (($_.Exception.Message) -split "`n")[0]
 
-  }
+  $results = Invoke-Expression -Command "netsh interface ipv4 show tcpconnections"
+
+  $t = $results -split "`r`n" | Where-Object { ($_ -match "\s$selenium_http_port\s") }
+  (($t -ne '') -and $t -ne $null)
+
 }
 
-
 $shared_assemblies = @(
-  "WebDriver.dll",
-  "WebDriver.Support.dll",
-  'nunit.core.dll',
+  'WebDriver.dll',
+  'WebDriver.Support.dll',
   'nunit.framework.dll'
-
 )
-
 
 $shared_assemblies_path = 'c:\developer\sergueik\csharp\SharedAssemblies'
 
@@ -95,67 +68,96 @@ $shared_assemblies | ForEach-Object {
 }
 popd
 
-$verificationErrors = New-Object System.Text.StringBuilder
+[NUnit.Framework.Assert]::IsTrue($host.Version.Major -ge 2)
+# TODO : leftover profile lock file
+<#
+ Directory of C:\Users\sergueik\AppData\Roaming\Mozilla\Firefox\Profiles\wfywwbuv.default
 
-# use Default Web Site to host the page. Enable Directory Browsing.
-
-$hub_port = '4444'
-$uri = [System.Uri](('http://{0}:{1}/wd/hub' -f $hub_host,$hub_port))
-
+10/05/2014  06:21 PM                 0 parent.lock
+#>
+if (Get-Item -Path 'c:\Users\sergueik\AppData\Roaming\Mozilla\Firefox\Profiles\6us7lrj6.Selenium\parent.lock' -ErrorAction 'SilentlyContinue') {
+  Remove-Item -Path 'c:\Users\sergueik\AppData\Roaming\Mozilla\Firefox\Profiles\6us7lrj6.Selenium\parent.lock'
+}
 
 try {
   $connection = (New-Object Net.Sockets.TcpClient)
-  $connection.Connect($hub_host,[int]$hub_port)
+  $connection.Connect("127.0.0.1",4444)
   $connection.Close()
 } catch {
-  Start-Process -FilePath 'C:\Windows\System32\cmd.exe' -ArgumentList 'start cmd.exe /c c:\java\selenium\hub.cmd'
-  Start-Process -FilePath 'C:\Windows\System32\cmd.exe' -ArgumentList 'start cmd.exe /c c:\java\selenium\node.cmd'
-
-  #   Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\selenium.cmd"
-  Start-Sleep -Seconds 5
+  Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\selenium.cmd"
+  Start-Sleep -Seconds 10
 }
-if ($browser -ne $null -and $browser -ne '' -and $browser -match 'firefox') {
+[object]$profile_manager = New-Object OpenQA.Selenium.Firefox.FirefoxProfileManager
+[OpenQA.Selenium.Firefox.FirefoxProfile[]]$profiles = $profile_manager.ExistingProfiles
 
+$profiles | ForEach-Object {
+  $item = $_
+  # TODO - find how to extract information about all profiles
+  $item
+}
 
-  [object]$profile_manager = New-Object OpenQA.Selenium.Firefox.FirefoxProfileManager
-  [OpenQA.Selenium.Firefox.FirefoxProfile[]]$profiles = $profile_manager.ExistingProfiles
-  # assert $profiles.GetType() <-  FirefoxProfile[]
-  $selected_profile_object = $null
-  $profiles | ForEach-Object {
-    $item = $_
-    # TODO - find how to extract information about all profiles
-    $item
-  }
-  # OpenQA.Selenium.Firefox.FirefoxBinary.StartProfile
-  [string]$profilePath = "c:\Users\sergueik\AppData\Roaming\Mozilla\Firefox\Profiles\nmkd7a04.Selenium"
-  [OpenQA.Selenium.Firefox.FirefoxProfile]$selected_profile_object = New-Object OpenQA.Selenium.Firefox.FirefoxProfile ($profilePath)
-  #  $selenium = New-Object OpenQA.Selenium.Firefox.FirefoxDriver($selected_profile_object)
-  #
-  $selenium = New-Object OpenQA.Selenium.Firefox.FirefoxDriver ("C:\Program Files\Mozilla Firefox\firefox.exe",$null)
-  # $selected_profile_object.setPreference("general.useragent.override", "Mozilla/5.0 (Windows NT 6.1; rv:15.0) Gecko/20100101 Firefox/15.0")
+[string]$profile_directory = 'c:\Users\sergueik\AppData\Roaming\Mozilla\Firefox\Profiles\6us7lrj6.Selenium'
+$profile = New-Object OpenQA.Selenium.Firefox.FirefoxProfile ($profile_directory)
+Write-Debug 'Adding extensions'
+$profile.AddExtension("C:\developer\sergueik\powershell_ui_samples\external\java\capture\resources\firebug-2.0.8.xpi")
+$profile.AddExtension("C:\developer\sergueik\powershell_ui_samples\external\java\capture\resources\netExport-0.9b7.xpi")
+Write-Debug 'Settings'
+try {
+  [void]$profile.SetPreference("app.update.enabled",$false)
+} catch [System.Management.Automation.MethodInvocationException]{
+  Write-Output 'Ignored exception'
+}
+
+# Setting Firebug preferences
+# http://getfirebug.com/wiki/index.php/Preferences
+
+[void]$profile.SetPreference("general.useragent.override","Mozilla/5.0 (Windows NT 6.1; rv:15.0) Gecko/20100101 Firefox/15.0")
+[void]$profile.SetPreference("extensions.firebug.addonBarOpened",$true)
+[void]$profile.SetPreference("extensions.firebug.netexport.alwaysEnableAutoExport",$true)
+[void]$profile.SetPreference("extensions.firebug.netexport.autoExportToFile",$true)
+[void]$profile.SetPreference("extensions.firebug.netexport.Automation",$true)
+[void]$profile.SetPreference("extensions.firebug.netexport.showPreview",$false)
+[void]$profile.SetPreference("extensions.firebug.netexport.sendToConfirmation",$false)
+[void]$profile.SetPreference("extensions.firebug.netexport.pageLoadedTimeout",0)
+[void]$profile.SetPreference("extensions.firebug.netexport.Automation",$true)
+[void]$profile.SetPreference("extensions.firebug.netexport.defaultLogDir","C:\temp")
+[void]$profile.SetPreference("extensions.firebug.netexport.saveFiles",$true)
+
+[void]$profile.SetPreference("extensions.firebug.currentVersion","2.08")
+[void]$profile.SetPreference("extensions.firebug.addonBarOpened",$true)
+[void]$profile.SetPreference("extensions.firebug.console.enableSites",$true)
+[void]$profile.SetPreference("extensions.firebug.script.enableSites",$true)
+[void]$profile.SetPreference("extensions.firebug.net.enableSites",$true)
+[void]$profile.SetPreference("extensions.firebug.previousPlacement",1)
+[void]$profile.SetPreference("extensions.firebug.allPagesActivation","on")
+[void]$profile.SetPreference("extensions.firebug.onByDefault",$true)
+[void]$profile.SetPreference("extensions.firebug.defaultPanelName","net")
   # TODO:
   # .AcceptUntrustedCertificates
   # .AlwaysLoadNoFocusLibrary
   # .EnableNativeEvents
-  # [void] SetPreference(string name, string value)
-}
 
-else {
-  Write-Output 'This example only works with Firefox browser'
-  exit 0
-}
+Write-Debug 'Starting'
+<# 
+# This currently does not work:
+  $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Firefox()
 
+  $capability.SetCapability([OpenQA.Selenium.Firefox.FirefoxDriver]::Profile, $profile)
+  $selenium = New-Object OpenQA.Selenium.Remote.RemoteWebDriver ($uri,$capability)
+#>
 
+$selenium = New-Object OpenQA.Selenium.Firefox.FirefoxDriver ($profile)
+
+# $selenium | Get-Member
 $base_url = 'http://www.wikipedia.org/'
 $selenium.Navigate().GoToUrl($base_url)
 $selenium.Navigate().Refresh()
-$selenium
 
-set_timeouts ([ref]$selenium)
-# var hasJQueryLoaded = (bool) js.ExecuteScript("return (window.jQuery != null) && (jQuery.active === 0);");
 
-[int]$timeout = 4000
-# change $timeout to see if the WevDriver is waiting on page  sctript to execute
+Start-Sleep 1
+Write-Output 'Calling the script'
+$timeout = 10
+
 
 [string]$script = "window.setTimeout(function(){document.getElementById('searchInput').value = 'test'}, ${timeout});"
 
@@ -178,10 +180,42 @@ catch [OpenQA.Selenium.NoSuchWindowException]{
 $end = (Get-Date -UFormat "%s")
 $elapsed = New-TimeSpan -Seconds ($end - $start)
 Write-Output ('Elapsed time {0:00}:{1:00}:{2:00} ({3})' -f $elapsed.Hours,$elapsed.Minutes,$elapsed.Seconds,($end - $start))
-# Start-Sleep 30
-# Cleanup
-cleanup ([ref]$selenium)
 
 
+$script = 'window.NetExport.triggerExport("abcde")'
+Write-Output "Calling the script:`r`n${script}"
+
+$start = (Get-Date -UFormat '%s')
+
+try {
+  [void]([OpenQA.Selenium.IJavaScriptExecutor]$selenium).executeAsyncScript($script);
+
+} catch [OpenQA.Selenium.WebDriverTimeoutException]{
+  # Ignore the following:
+  # Timed out waiting for async script result  (Firefox)
+  # asynchronous script timeout: result was not received (Chrome)
+  [NUnit.Framework.Assert]::IsTrue($_.Exception.Message -match '(?:Timed out waiting for async script result|asynchronous script timeout)')
+}
+catch [OpenQA.Selenium.NoSuchWindowException]{
+  Write-Host $_.Exception.Message # Unable to get browser
+  # $_.Exception | Get-Member
+
+} catch [exception]{
+  # http://blogs.msdn.com/b/mwories/archive/2009/06/08/powershell-tips-tricks-getting-more-detailed-error-information-from-powershell.aspx
+  $error[0] | Format-List -Force
+
+}
+$end = (Get-Date -UFormat '%s')
+$elapsed = New-TimeSpan -Seconds ($end - $start)
+Write-Output ('Elapsed time {0:00}:{1:00}:{2:00} ({3})' -f $elapsed.Hours,$elapsed.Minutes,$elapsed.Seconds,($end - $start))
+
+
+Write-Debug 'Closing'
+try {
+  $selenium.Close()
+  $selenium.Quit()
+} catch [exception]{
+  Write-Output (($_.Exception.Message) -split "`n")[0]
+}
 
 
