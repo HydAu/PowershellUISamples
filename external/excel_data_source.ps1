b50e012551b4c4c519dc3efa0e99155f79324545
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-03-08 16:15:19 -0400

    intrmediate revision

--- external/excel_data_source.ps1
+++ external/excel_data_source.ps1
@@ -23,69 +23,77 @@ $sheet_name = 'ServerList$'
 $data_source = "Data Source = $filename"
 $ext_arg = "Extended Properties=Excel 8.0"
 [string]$query = "Select * from [${sheet_name}]"
-[System.Data.OleDb.OleDbConnection]$connection= New-Object System.Data.OleDb.OleDbConnection("$oledb_provider;$data_source;$ext_arg")
-[System.Data.OleDb.OleDbCommand]$command = New-Object System.Data.OleDb.OleDbCommand($query)
+[System.Data.OleDb.OleDbConnection]$connection = New-Object System.Data.OleDb.OleDbConnection ("$oledb_provider;$data_source;$ext_arg")
+[System.Data.OleDb.OleDbCommand]$command = New-Object System.Data.OleDb.OleDbCommand ($query)
 
 
-[System.Data.DataTable]$data_table = new-object System.Data.DataTable
+[System.Data.DataTable]$data_table = New-Object System.Data.DataTable
 [System.Data.OleDb.OleDbDataAdapter]$ole_db_adapter = New-Object System.Data.OleDb.OleDbDataAdapter
 $ole_db_adapter.SelectCommand = $command
 
 $command.Connection = $connection
-# $sql = "Insert into [${sheet_name}] (id,name) values('5','e')"
-# $sql = "Update [${sheet_name}] set name = 'New Name' where id=1";
-# $command.CommandText = sql
-# $command.ExecuteNonQuery()
 $rows = $ole_db_adapter.Fill($data_table)
 $rows | Out-Null
 $connection.open()
 $data_reader = $command.ExecuteReader()
 
-$row = 1
-[System.Data.DataRow]$data_record = $null 
-ForEach ($data_record in $data_table) {
- 
-    # Reading the columns of the current row
-    # $data_record | get-member
-    $server_cell_value = $data_record.'server'
-    $status_cell_value = $data_record.'status'
-    $date_cell_value = $data_record.'date'
-    $result_cell_value = $data_record.'result'
-    $id_cell_value = $data_record.'id'
-    $cell_name = 'id'
-    $id_cell_value = $data_record."${cell_name}"
-    $row_data = @{'id' = $null ; 'servr' = $null ;
- 'status' = $null ;  'date' =  $null ; 'result' = $null  ;} 
-    @('id', 'server', 'status', 'date', 'result') | foreach-object {  
-# $row_data.Keys | foreach-object {  
-# An error occurred while enumerating through a collection: Collection was
-# modified; enumeration operation may not execute..
+$row_num = 1
+[System.Data.DataRow]$data_record = $null
+foreach ($data_record in $data_table) {
+
+  # Reading the columns of the current row
+  Write-Host ("row:{0}" -f $row_num)
+  $row_data = @{
+    'id' = $null;
+    'server' = $null;
+    'status' = $null;
+    'date' = $null;
+    'result' = $null;
+    'guid' = $null;
+
+  }
+  $row_data_keys = New-Object object[] ($row_data.Keys.Count)
+  [array]::copy(($row_data.Keys),$row_data_keys,$row_data.Keys.Count)
+  $row_data_keys | ForEach-Object {
+    # An error occurred while enumerating through a collection: Collection was
+    # modified; enumeration operation may not execute..
     $cell_name = $_
     $cell_value = $data_record."${cell_name}"
-$row_data[ $cell_name ]  = $cell_value
-    }
-    write-host ("row:{0}`nserver={1}`nstatus={2}`nresult={3}`ndate={4}`nid={5}`n" -f $row, $server_cell_value,$status_cell_value,$result_cell_value ,$date_cell_value ,$id_cell_value)
-    $row++
+    $row_data[$cell_name] = $cell_value
+  }
+  Write-Output ("row:{0}" -f $row)
+  $row_data
+  <#
+ $result = @()
+ $row_data_keys | foreach-object {
+ $key_name = $_
+ $result += ( '{0}="{1}"' -f $key_name, $row_data[$key_name] )
+ }
+
+ Write-Output ("row:{0}`n{1}`n" -f $row,[System.String]::Join("`r`n", $result))
+ #>
+  Write-Output "`n"
+  $row_num++
 }
 $data_reader.close()
 $new_guid = $guid = [guid]::NewGuid()
-$sql =  "Insert into [${sheet_name}] ([server],[status],[result],[date],[id]) values('sergueik9',True,42,'03/08/2015', '${new_guid}')"
+$sql = "Insert into [${sheet_name}] ([id],[server],[status],[result],[date],[guid]) values($row_num, 'sergueik9','True',42,'3/8/2015 4:00:00 PM', '${new_guid}')"
 $command.CommandText = $sql
 $result = $command.ExecuteNonQuery()
 
-write-output ('Insert result: {0}' -f $result )
+Write-Output ('Insert result: {0}' -f $result)
 
-# $sql = "UPDATE [${sheet_name}] SET [server] = @server, [status] = @status, [result] = @result  WHERE [id] = @id"
-# $sql = "UPDATE [${sheet_name}] SET [server] = @server, [status] = @status, [result] = @result, [id]  = '@id'  WHERE [server] = @server"
-$sql = "UPDATE [${sheet_name}] SET [server] = @server, [status] = @status, [result] = @result , [id]  = @id  WHERE [server] = @server"
-$command.Parameters.Add("@id", [System.Data.OleDb.OleDbType]::VarChar).Value = ("'{0}'" -f ([Guid]'7c3b83f5-43cd-4c5c-a013-8a97db7dd8b8').ToString())
+# $sql = "UPDATE [${sheet_name}] SET [server] = @server, [status] = @status, [result] = @result , [guid]  = '@guid' WHERE [id] = @id"
+$sql = "UPDATE [${sheet_name}] SET [status] = @status, [result] = @result WHERE [id] = @id"
+# $command.Parameters.Add("@id",[System.Data.OleDb.OleDbType]::VarChar).Value = '2'
+# $command.Parameters.Add("@guid",[System.Data.OleDb.OleDbType]::VarChar).Value = ("{0}" -f ([guid]'ea370517-8c01-4623-b932-68570c9d84c3').ToString())
 # https://msdn.microsoft.com/en-us/library/system.data.oledb.oledbtype%28v=vs.110%29.aspx
-$command.Parameters.Add("@server", [System.Data.OleDb.OleDbType]::VarChar).Value = 'SKOUZMINE9'
-$command.Parameters.Add("@status", [System.Data.OleDb.OleDbType]::Boolean).Value = $true
-$command.Parameters.Add("@result", [System.Data.OleDb.OleDbType]::Numeric).Value = 42
+$command.Parameters.Add("@server",[System.Data.OleDb.OleDbType]::VarChar).Value = 'sergueik9'
+$command.Parameters.Add("@status",[System.Data.OleDb.OleDbType]::Boolean).Value = $true
+$command.Parameters.Add("@result",[System.Data.OleDb.OleDbType]::Numeric).Value = 42
 $command.CommandText = $sql
 $result = $command.ExecuteNonQuery()
-write-output ('Update result: {0}' -f $result )
+Write-Output ('Update result: {0}' -f $result)
 
 $command.Dispose()
 $connection.close()
