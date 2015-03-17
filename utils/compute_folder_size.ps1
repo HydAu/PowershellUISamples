2364b7e463a8477e59dbff2a6b15ef8cc1e318fc
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-03-17 16:26:39 -0400

    iinitial revision

---
+++ utils/compute_folder_size.ps1
@@ -0,0 +1,44 @@
+# 
+function Get-FolderSize
+{
+
+  begin {
+    $fso = New-Object -ComObject Scripting.FileSystemObject
+  }
+  process {
+    $path = $input.fullname
+    $folder = $fso.GetFolder($path)
+    $size = $folder.size
+    if ($size -gt 1mb) {
+    [pscustomobject]@{ 'Name' = $path; 'Size' = ($size / 1Kb) }}
+  }
+
+}
+
+
+$skip = 'sxs'
+pushd 'C:'
+cd '\'
+$level1 = @()
+Get-ChildItem -Directory -ErrorAction SilentlyContinue | ForEach-Object { $path = $_.fullname;
+
+  if (-not ($path -match '\b(Program Files|Program Files \(x86\)|windows|vagrant.*|ruby.*|chef|opscode|java|sxs)\b')) {
+    $level1 += $path
+    Write-Output ('added {0} to $level1 ' -f $path)
+  }
+  else {
+    Write-Output ('Skipping directory from measuring: {0}' -f $path)
+  } }
+popd
+Write-output 'Measuring:' 
+$level1 | Format-Table
+pushd 'C:'
+cd '\'
+$level1 | ForEach-Object {
+
+  Get-ChildItem -LiteralPath $_ -Directory -Recurse -ErrorAction SilentlyContinue | ForEach-Object {
+    $path = $_; # TODO assert 
+    if ($true) { Write-Output $path | Get-FolderSize } }
+} | sort size -Descending | Out-GridView
+
+popd
