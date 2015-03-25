387badd0e73a422710f0a87c949f1f2f3d39ed1c
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-03-24 21:05:46 -0400

    final revision

--- selenium/zoom.ps1
+++ selenium/zoom.ps1
@@ -92,7 +92,7 @@ if ($PSBoundParameters["browser"]) {
     Start-Process -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList "start cmd.exe /c c:\java\selenium\node.cmd"
     Start-Sleep -Seconds 10
   }
-  $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Chrome()
+  $capability = [OpenQA.Selenium.Remote.DesiredCapabilities]::Firefox()
   $uri = [System.Uri]("http://127.0.0.1:4444/wd/hub")
   $selenium = New-Object OpenQA.Selenium.Remote.RemoteWebDriver ($uri,$capability)
 } else {
@@ -112,19 +112,12 @@ Start-Sleep -Milliseconds 4000
 # $selenium.Manage().Window.Size = new-Object System.Drawing.Size(600, 400)
 [void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
 $logo = $selenium.FindElementByXPath("//img[@alt='Mozilla']")
-<#
-cannot send keys to img node:
-Exception calling "SendKeys" with "1" argument(s): 
-"unknown error: cannot focus element
-#>
-
-$target_text_field = $selenium.FindElementByXPath("//input[contains(@type,'email') or contains(@type, 'text') ]")
 
 highlight ([ref]$selenium) ([ref]$logo)
-highlight ([ref]$selenium) ([ref]$target_text_field)
 
 <#
-# Some OpenQA.Selenium.Keys do not work with C# client ?
+# Some OpenQA.Selenium.Keys do not seem to work with C# client 
+# chord is not available
 $target_text.SendKeys(([OpenQA.Selenium.Keys]::Control +  [OpenQA.Selenium.Keys]::Substract))
 start-sleep -milliseconds 300
 #>
@@ -135,26 +128,75 @@ Write-Output 'Zoom in'
 (1,2,3,4,5) | ForEach-Object {
   try{
   # https://msdn.microsoft.com/en-us/library/system.windows.forms.sendkeys.send%28v=vs.110%29.aspx
-  # + is a mnemonic for SHIFT
-  # '^(+)' does not work
-  #  "SendKeys string '^++' is not valid
-  # OpenQA.Selenium.IKeyboard.SendKeys ? 
-  [void]$actions.SendKeys($target_text_field,[System.Windows.Forms.SendKeys]::SendWait("^+`0"))
-  # [void]$actions.SendKeys($target_text_field,([OpenQA.Selenium.Keys]::Control +  [OpenQA.Selenium.Keys]::Add + [OpenQA.Selenium.Keys]::Null  ))
-  # $target_text_field.SendKeys(([OpenQA.Selenium.Keys]::Control + [OpenQA.Selenium.Keys]::Add))
-  # $selenium.Keyboard.SendKeys(([OpenQA.Selenium.Keys]::Control +  [OpenQA.Selenium.Keys]::Add + [OpenQA.Selenium.Keys]::Null  ))
-  # $selenium.Keyboard.SendKeys([System.Windows.Forms.SendKeys]::SendWait('^+'))
-  # $selenium.Keyboard.SendKeys([System.Windows.Forms.SendKeys]::SendWait('^(+)'))
-  # key sequence to send must not be null
-  # Parameter name: keySequence
-  
-  Start-Sleep -Milliseconds 500
+  [void]$selenium.Keyboard.SendKeys([System.Windows.Forms.SendKeys]::SendWait("^{+}"))
+  # http://blogs.msdn.com/b/timid/archive/2014/08/05/send-keys.aspx
+  # [void]$selenium.Keyboard.SendKeys([System.Windows.Forms.SendKeys]::SendWait("^{+}`0"))
   } catch [Exception] { 
-write-output $_.Exception.Message
+    # ignore exception
+    write-Debug $_.Exception.Message
+  }
+  Start-Sleep -Milliseconds 1100
+}
+Write-Output 'Zoom 100%'
+try{
+  [void]$selenium.Keyboard.SendKeys([System.Windows.Forms.SendKeys]::SendWait('^0'))
+  } catch [Exception] { 
+    # ignore exception
+    write-Debug $_.Exception.Message
+  }
+Start-Sleep -Seconds 1
+
+Write-Output 'Reload'
+try{
+[void]$selenium.Keyboard.SendKeys([System.Windows.Forms.SendKeys]::SendWait('^R'))
+  } catch [Exception] { 
+    # ignore exception
+    write-Debug $_.Exception.Message
+  }
+
+Start-Sleep -Seconds 3
+
+Write-Output 'Zoom out'
+(1,2,3,4,5) | ForEach-Object {
+try{
+  [void]$selenium.Keyboard.SendKeys([System.Windows.Forms.SendKeys]::SendWait('^-'))
+  } catch [Exception] { 
+    # ignore exception
+    # key sequence to send must not be null
+    write-Debug $_.Exception.Message
+  }
+
+  Start-Sleep -Milliseconds 1100
+}
+Write-Output 'Zoom 100%'
+
+try{
+  [void]$selenium.Keyboard.SendKeys([System.Windows.Forms.SendKeys]::SendWait('^0'))
+  } catch [Exception] { 
+    # ignore exception
+    write-Debug $_.Exception.Message
+  }
+Start-Sleep -Seconds 3
+<#
+ NOTE: cannot send keys to img node: Exception calling "SendKeys" with "1" argument(s):  "unknown error: cannot focus element
+#>
+
+$target_text_field = $selenium.FindElementByXPath("//input[contains(@type,'email') or contains(@type, 'text') ]")
+
+highlight ([ref]$selenium) ([ref]$target_text_field)
+
+Write-Output 'Zoom in'
+# zoom in does not seem to work on Chrome or Firefox
+(1,2,3,4,5) | ForEach-Object {
+  try{
+  [void]$actions.SendKeys($target_text_field,[System.Windows.Forms.SendKeys]::SendWait("^{+}"))
+  Start-Sleep -Milliseconds 1100
+  } catch [Exception] { 
+   write-output $_.Exception.Message
   }
 }
 [void]$actions.SendKeys($target_text_field,[System.Windows.Forms.SendKeys]::SendWait('^0'))
-Start-Sleep -Seconds 3
+Start-Sleep -Seconds 1
 Write-Output 'Reload'
 
 [void]$actions.SendKeys($target_text_field,[System.Windows.Forms.SendKeys]::SendWait('^R'))
@@ -163,19 +205,14 @@ Start-Sleep -Seconds 3
 Write-Output 'Zoom out'
 (1,2,3) | ForEach-Object {
   [void]$actions.SendKeys($target_text_field,[System.Windows.Forms.SendKeys]::SendWait('^-'))
-  Start-Sleep -Milliseconds 500
+  Start-Sleep -Milliseconds 1100
 }
-Start-Sleep -Seconds 3
+Write-Output 'Zoom 100%'
+
 [void]$actions.SendKeys($target_text_field,[System.Windows.Forms.SendKeys]::SendWait('^0'))
-Start-Sleep -Milliseconds 500
+Start-Sleep -Milliseconds 3000
 
-
-Write-Output 'zoom out'
-(1,2,3) | ForEach-Object {
-  [void]$actions.SendKeys($target_text,[System.Windows.Forms.SendKeys]::SendWait('^-'))
-  Start-Sleep -Milliseconds 500
-}
-Start-Sleep -Seconds 3
+Write-Output 'Done.'
 
 # Cleanup
 cleanup ([ref]$selenium)
