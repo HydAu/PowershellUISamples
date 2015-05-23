1357b61c013907eb8f38b072d510227f1c6b186d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-05-23 17:59:19 -0400

    intermediate revision

--- external/chart_sample.ps1
+++ external/chart_sample.ps1
@@ -27,19 +27,24 @@ namespace Dino.Tools.WebMonitor
     public class FakeChartForm1 : Form
     {
         private IContainer components = null;
+        private ArrayList aSeries1 = new ArrayList();
         public Chart chart1;
         public FakeChartForm1 ()
         {
             InitializeComponent();
         }
-
+/*
         private double demo_function(int i)
         {
             var f1 = 59894 - (8128 * i) + (262 * i * i) - (1.6 * i * i * i);
             return f1;
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+*/
+        public void Form1_LoadData(object sender, EventArgs e){ 
+             aSeries1 = new ArrayList { -10, 20, 40, -20, 10,30,34 };
+        }
+    
+        public void Form1_Load(object sender, EventArgs e)
         {
             chart1.Series.Clear();
             var series1 = new System.Windows.Forms.DataVisualization.Charting.Series
@@ -51,12 +56,102 @@ namespace Dino.Tools.WebMonitor
                 ChartType = SeriesChartType.Line
             };
 
+/*
+
             this.chart1.Series.Add(series1);
 
             for (int i=0; i < 100; i++)
             {
                 series1.Points.AddXY(i, demo_function(i));
             }
+
+*/
+//            ArrayList aSeries1 = new ArrayList { -10, 20, 40, -20, 10 };
+            ArrayList aSeries2 = new ArrayList();
+            // THE SECOND SERIES MUST START FROM ZERO
+            aSeries2.Add(0);
+
+            //THIS IS FOR THE TOTAL COLUMN
+            double nSumSeries1 = 0;
+
+            Console.Error.WriteLine("Series2");
+            for (int nIndex = 0; nIndex < aSeries1.Count; nIndex++)
+            {
+                Console.Error.WriteLine(aSeries2[nIndex]);
+                //COUNT UP FOR THE TOTAL COLUMN
+                nSumSeries1 += Convert.ToDouble(aSeries1[nIndex]);
+                //ADDS THE RESULTS IN THE SECOND SERIES  
+                aSeries2.Add(nSumSeries1);
+
+            }
+
+            chart1.Series.Clear();
+            //THE LAST COLUMN IS THE TOTAL COLUMN TO SHOW ON THE END OF THE CHART
+            aSeries1.Add(nSumSeries1 * -1);
+
+
+            //CREATES THE FIRST SERIE TO SHOW THE VALUES AS RANGE_COLUMNS
+            chart1.Series.Add("Series1");
+            chart1.Series["Series1"].ChartType = SeriesChartType.RangeColumn;
+            chart1.Series["Series1"].Color = Color.Yellow;
+            chart1.Series["Series1"].YValuesPerPoint = 2;
+            chart1.Series["Series1"]["PointWidth"] = "1.0";
+
+            // NO GRIDLINES
+            // chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            // chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+
+            //CREATES THE SECOND SERIES TO SHOW THE CONNECTION LINE BETWEEN THE COLUMNS 
+            chart1.Series.Add("Series2");
+            chart1.Series["Series2"].ChartType = SeriesChartType.RangeColumn;
+            chart1.Series["Series2"].Color = Color.Black;
+            chart1.Series["Series2"].YValuesPerPoint = 2;
+            chart1.Series["Series2"]["PointWidth"] = "1.0";
+
+            //LIST OF VALUES FOR THE SECOND SERIES (LINES BETWEEN THE COLUMNS)
+            ArrayList aSeriesEndPoints = new ArrayList();
+
+            Console.Error.WriteLine("Series1 Values:");
+            for (int nIndex = 0; nIndex < aSeries2.Count; nIndex++)
+            {
+                //CREATES THE COLUMNS FOR THE FIRST SERIES WITH TWO Y-COORDS(STARTPOINT, ENDPOINT)
+                chart1.Series["Series1"].Points.InsertXY(nIndex, nIndex, aSeries2[nIndex], Convert.ToDouble(aSeries2[nIndex]) + Convert.ToDouble(aSeries1[nIndex]));
+                Console.Error.WriteLine("StartPoint:" + aSeries2[nIndex] + "; EndPoint: " + (Convert.ToDouble(aSeries2[nIndex]) + Convert.ToDouble(aSeries1[nIndex])));
+
+                //FILL THE ENDPOINTS IN THE LIST
+                aSeriesEndPoints.Add((Convert.ToDouble(aSeries2[nIndex]) + Convert.ToDouble(aSeries1[nIndex])));
+
+            }
+
+            // THIS IS A TEST - I USE TO ADD SOME PIXEL (0.1) TO ANOTHER LIST FOR THE SECOND SERIES
+            ArrayList aSeriesEndPointsCopy = new ArrayList();
+
+            for (int nIndex = 0; nIndex < aSeriesEndPoints.Count; nIndex++)
+            {
+                switch (System.Math.Sign(Convert.ToDouble(aSeriesEndPoints[nIndex])))
+                {
+                    case 0:
+                        aSeriesEndPointsCopy.Add(Convert.ToDouble(aSeriesEndPoints[nIndex]) + 0.1);
+                        break;
+
+                    case 1:
+                        aSeriesEndPointsCopy.Add(Convert.ToDouble(aSeriesEndPoints[nIndex]) + 0.1);
+                        break;
+
+                    case -1:
+                        aSeriesEndPointsCopy.Add(Convert.ToDouble(aSeriesEndPoints[nIndex]) - 0.1);
+                        break;
+                }
+            }
+
+            Console.Error.WriteLine("Series2 Value:");
+            //AT LEAST I INSERT THE POINTS FOR THE SECOND SERIES (LINES BETWEEN THE COLUMNS)
+            for (int nIndex = 0; nIndex < aSeriesEndPoints.Count - 1; nIndex++)
+            {
+                chart1.Series["Series2"].Points.InsertXY(nIndex, nIndex, Convert.ToDouble(aSeriesEndPoints[nIndex]), Convert.ToDouble(aSeriesEndPointsCopy[nIndex]));
+                Console.Error.WriteLine("StartPoint:" + (Convert.ToDouble(aSeriesEndPoints[nIndex])) + "; EndPoint: " + (Convert.ToDouble(aSeriesEndPointsCopy[nIndex])));
+            }
+
             chart1.Invalidate();
         }
 
@@ -99,7 +194,8 @@ namespace Dino.Tools.WebMonitor
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "FakeChart";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            // want to pass the data 
+            // this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
         }
@@ -120,6 +216,8 @@ $o.PerformLayout()
 $o.Add_Shown({ $o.Activate() })
 $o.KeyPreview = $True
 
+# $o.Form1_LoadData($null, $null)
+# $o.Form1_Load($null, $null)
 [void]$o.ShowDialog()
 
 $o.Dispose()
