0c2898bed36ab6424e539d3fddaf1ad2a0632289
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-07-26 09:28:45 -0400

    intermediate revision

--- external/bitmap_button3.ps1
+++ external/bitmap_button3.ps1
@@ -17,15 +17,13 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #THE SOFTWARE.
+
 # $DebugPreference = 'Continue'
 
 param(
   [switch]$pause
 )
 
-if ($host.Version.Major -le 2) {
-  throw "This script will not work witn Powershell v2."
-}
 
 # http://stackoverflow.com/questions/8343767/how-to-get-the-current-directory-of-the-cmdlet-being-executed
 function Get-ScriptDirectory
@@ -52,10 +50,8 @@ $f = New-Object System.Windows.Forms.Form
 
 $l1 = New-Object System.Windows.Forms.Label
 $l2 = New-Object System.Windows.Forms.Label
-$l3 = New-Object System.Windows.Forms.Label
 $o1 = New-Object -TypeName System.Windows.Forms.Button
 $o2 = New-Object -TypeName System.Windows.Forms.Button
-$o3 = New-Object -TypeName System.Windows.Forms.Button
 
 # label1
 $l1.BorderStyle = [System.Windows.Forms.BorderStyle]::None
@@ -73,35 +69,21 @@ $l2.Text = ''
 $l2.Size = New-Object System.Drawing.Size (172,23)
 $l2.TabIndex = 4
 
-# label2
-$l3.BorderStyle = [System.Windows.Forms.BorderStyle]::None
-$l3.Location = New-Object System.Drawing.Point (12,110)
-$l3.Name = "l3"
-$l3.Text = ''
-$l3.Size = New-Object System.Drawing.Size (172,23)
-$l3.TabIndex = 4
-
-
 # dDControls
+$o1.Name = "b1"
+$o1.TabIndex = 1
 $global:m = @{
   'b1' = 'l1';
   'b2' = 'l2';
-  'b3' = 'l3';
 }
 
 $o1.Location = New-Object System.Drawing.Point (200,32)
 $o1.Size = New-Object System.Drawing.Size (32,32)
 $o1.Name = 'b1'
 $o1.TabIndex = 1
-$o1.Text = 'b1'
-$o1 | get-member
-# return
-$o1.Focused = $false
+$o1.Text = "b1"
 
-# 
 # $o1.SetStyle([System.Windows.Forms.ControlStyles]::UserPaint -bor [System.Windows.Forms.ControlStyles]::AllPaintingInWmPaint -bor [System.Windows.Forms.ControlStyles]::DoubleBuffer,$true)
-
-
 # http://www.alkanesolutions.co.uk/2013/04/19/embedding-base64-image-strings-inside-a-powershell-application/
 $do = 'iVBORw0KGgoAAAANSUhEUgAAAKAAAAAgBAMAAABnUW7GAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAwUExURQAAAIAAAACAAICAAAAAgIAAgACAgMDAwICAgP8AAAD/AP//AAAA//8A/wD//////08TJkkAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAFHSURBVEjH7daxjsMgDAbgKAvZ8g5dbuTVrFvMlmxku1fNRre7EmwCbk0lel1Ox/bLzidboWqGfajO/Goe5u/q7NNWnV3WZTZYHZDgWoNfElhlvgdDmd06+TIvu6zL/A++BwS+ROYxWNVlvoGO74RnkB8PBOZrSuCFTiCw7I8gZwaRxl545ZEASyuPHGnlst8k6NgfNRDyRJ0g8AYKCBwJLPsN5p29CtJIFhvgRwtMOyyogSnb89oY/LxQv2EqsQoIPFEvCLSxBgJFBiGuHE7QZVUBj5HiRDqITTDuvKAOxmzxBIt+w+8jvRkNBJqoG4S0gQpCihk8+xPo+HZr4G2kY6JuEM2CLRBHiyV49tPP0NPlVkFIE/WDENpgrstMoPNPQJzxNZCOCub6X/iTulbfHuskv2VEXeZ7cBMPSFDWtyfg737ODfMP1mxvUDAf+WQAAAAASUVORK5CYII='
 
@@ -115,81 +97,75 @@ $o2.Location = New-Object System.Drawing.Point (200,70)
 $o2.Size = New-Object System.Drawing.Size (32,32)
 $o2.TabIndex = 2
 $o1.Name = 'b2'
-$o2.Text = 'b2'
+$o2.Text = "b2"
 $o2.Image = $o1.Image
 
 
-$o3.Location = New-Object System.Drawing.Point (200,101)
-$o3.Size = New-Object System.Drawing.Size (32,32)
-$o3.Name = 'b3'
-$o3.TabIndex = 3
-$o3.Text = 'b3'
-$o3.Image = $o1.Image
-
 $f.SuspendLayout()
 
-[System.Windows.Forms.Button[]]$buttons = @()
-$buttons += $o1
-$buttons += $o2
-$buttons += $o3
+$btnState = New-Object PSObject
+$btnState | Add-Member -NotePropertyName 'BUTTON_UP' -NotePropertyValue 0
+$btnState | Add-Member -NotePropertyName 'BUTTON_DOWN' -NotePropertyValue 1
+$btnState | Add-Member -NotePropertyName 'BUTTON_FOCUSED' -NotePropertyValue 2
+$btnState | Add-Member -NotePropertyName 'BUTTON_MOUSE_ENTER' -NotePropertyValue 3
+$btnState | Add-Member -NotePropertyName 'BUTTON_DISABLED' -NotePropertyValue 4
+$btnState | Add-Member -NotePropertyName 'BUTTON_FAILED' -NotePropertyValue 5 # currently unused
+
+[System.Windows.Forms.Button[]] $buttons = @()
+$buttons += $o1 
+$buttons += $o2 
 
 # http://www.codeproject.com/Articles/4479/A-Simple-Bitmap-Button-Implementation
 <#
-BitmapButton.cs converted to Powershell syntax.
+BitmapButton.cs comverted to Powershell syntax.
 #>
 
-# This example will fail on W2K3 after script fails to add properties to a System.Windows.Forms.Button object
 
-# properties to maintain the state
-$btnState = New-Object PSObject
-$btnState | Add-Member -MemberType 'NoteProperty'  -Name 'BUTTON_UP' -Value 0
-$btnState | Add-Member -MemberType 'NoteProperty'  -Name 'BUTTON_DOWN' -Value 1
-$btnState | Add-Member -MemberType 'NoteProperty'  -Name 'BUTTON_FOCUSED' -Value 2
-$btnState | Add-Member -MemberType 'NoteProperty'  -Name 'BUTTON_MOUSE_ENTER' -Value 3
-$btnState | Add-Member -MemberType 'NoteProperty'  -Name 'BUTTON_DISABLED' -Value 4
-$btnState | Add-Member -MemberType 'NoteProperty'  -Name 'BUTTON_FAILED' -Value 5 # currently unused
+<#
+Add-Member -InputObject $o1 -MemberType 'NoteProperty' -Name 'ImgState' -Value $btnState.BUTTON_UP -Force
+Add-Member -InputObject $o1 -MemberType 'NoteProperty' -Name 'mouseEnter' -Value $false -Force
 
-$buttons | ForEach-Object {
-  Add-Member -InputObject $_ -MemberType 'NoteProperty'  -Name 'ImgState' -Value $btnState.BUTTON_UP -Force
-  Add-Member -InputObject $_ -MemberType 'NoteProperty'  -Name 'mouseEnter' -Value $false -Force
-  Add-Member -InputObject $_ -MemberType 'NoteProperty'  -Name 'Pressed' -Value $false -Force
-}
-$o1.ImgState = $btnState.BUTTON_UP 
-Add-Member -InputObject $o1  -MemberType 'NoteProperty'  -Name 'ImgState' -Value $btnState.BUTTON_UP -Force
 
-# callbacks
+Add-Member -InputObject $o2 -MemberType 'NoteProperty' -Name 'ImgState' -Value $btnState.BUTTON_UP -Force
+Add-Member -InputObject $o2 -MemberType 'NoteProperty' -Name 'mouseEnter' -Value $false -Force
+#>
+$buttons | foreach-object {
+Add-Member -InputObject $_ -MemberType 'NoteProperty' -Name 'ImgState' -Value $btnState.BUTTON_UP -Force
+Add-Member -InputObject $_ -MemberType 'NoteProperty' -Name 'mouseEnter' -Value $false -Force
+Add-Member -InputObject $_ -MemberType 'NoteProperty' -Name 'Pressed' -Value $false -Force
+
+} 
+
 $button_OnPaint = {
   param([object]$sender,[System.Windows.Forms.PaintEventArgs]$e)
   [System.Drawing.Graphics]$gr = $e.Graphics
-  # This math is used to determine which image go show
-  # TODO: refactor
   [int]$indexWidth = $sender.Size.Width * ([int]$sender.ImgState)
 
-    $size = $sender.Size
-    $size = (New-Object System.Drawing.Size ($sender.Size.Width,$sender.Size.Height))
-
-  # if ($sender.Image.Width -gt $indexWidth)
-  if ($sender.ImgState -ne $btnState.BUTTON_UP)
+  if ($sender.Image.Width -gt $indexWidth)
   {
+    $size = $sender.Size
     $point = (New-Object System.Drawing.Point ($indexWidth,0))
     $rect = New-Object System.Drawing.Rectangle ($point,$size)
     $gr.DrawImage($sender.Image,0,0,$rect,[System.Drawing.GraphicsUnit]::Pixel)
   }
   else
   {
+    $size = (New-Object System.Drawing.Size ($sender.Size.Width,$sender.Size.Height))
     $point = (New-Object System.Drawing.Point (0,0))
     $rect = New-Object System.Drawing.Rectangle ($point,$size)
     $gr.DrawImage($sender.Image,0,0,$rect,[System.Drawing.GraphicsUnit]::Pixel)
   }
 }
 
+$buttons | foreach-object {$_.add_Paint($button_OnPaint)  } 
 $button_OnGotFocus = {
   param(
     [object]$sender,[System.EventArgs]$e
   )
   $o1.ImgState = $btnState.BUTTON_FOCUSED
-  $sender.Invalidate()
+
 }
+$buttons | foreach-object {$_.add_GotFocus($button_OnGotFocus)  } 
 
 $button_OnLostFocus = {
   param(
@@ -204,8 +180,10 @@ $button_OnLostFocus = {
     $sender.ImgState = $btnState.BUTTON_UP
   }
   $sender.Invalidate()
+
 }
 
+$buttons | foreach-object {$_.add_LostFocus($button_OnLostFocus)  } 
 $button_OnMouseEnter = {
   param(
     [object]$sender,[System.EventArgs]$e
@@ -217,7 +195,9 @@ $button_OnMouseEnter = {
   }
   $sender.mouseEnter = $true
   $sender.Invalidate()
+
 }
+$buttons | foreach-object {$_.add_MouseEnter($button_OnMouseEnter)  } 
 
 $button_OnMouseLeave = {
   param(
@@ -233,6 +213,7 @@ $button_OnMouseLeave = {
 
 }
 
+$buttons | foreach-object {$_.add_MouseLeave($button_OnMouseLeave)  } 
 
 function find_label {
   param([string]$button_name)
@@ -248,7 +229,7 @@ $button_OnMouseDown = {
     [object]$sender,[System.Windows.Forms.MouseEventArgs]$e
   )
   $sender.ImgState = $btnState.BUTTON_DOWN
-  $sender.Pressed = $true
+    $sender.Pressed = $true
   $local:label_name = find_label -button_name $sender.Text
   try {
     $elems = $sender.Parent.Controls.Find($local:label_name,$false)
@@ -256,10 +237,11 @@ $button_OnMouseDown = {
     Write-Host $_.Exception.Message
   }
   if ($elems -ne $null) {
-    $elems[0].Text = ('Pressed {0} "{1}"' -f $sender.Text,$sender.ImgState )
+    $elems[0].Text = ('Pressed {0}' -f $sender.Text)
   }
   $sender.Invalidate()
 }
+$buttons | foreach-object {$_.add_MouseDown($button_OnMouseDown)  } 
 
 $button_OnMouseUp = {
   param(
@@ -278,24 +260,26 @@ $button_OnMouseUp = {
   $sender.Invalidate()
 
 }
+$buttons | foreach-object {$_.add_MouseUp($button_OnMouseUp)  } 
 
 $button_OnEnabledChanged = {
   param(
     [object]$sender,[System.EventArgs]$e
   )
 
-  if ($sender.Enabled)
-  {
-    $sender.ImgState = $btnState.BUTTON_UP
-  }
-  else
-  {
-    $sender.ImgState = $btnState.BUTTON_DISABLED
-  }
-  $sender.Invalidate()
+            if ($sender.Enabled)
+            {
+                $sender.imgState = $btnState.BUTTON_UP
+            }
+            else
+            {
+                $sender.imgState = $btnState.BUTTON_DISABLED
+            }
+            $sender.Invalidate()
 
 }
 
+$buttons | foreach-object {$_.add_EnabledChanged($button_OnEnabledChanged)  } 
 
 $button_OnKeyDown = {
   param(
@@ -304,7 +288,7 @@ $button_OnKeyDown = {
   if ($e.KeyData -eq [System.Windows.Forms.Keys]::Space)
   {
     $sender.Pressed = $true
-    $sender.ImgState = $btnState.BUTTON_DOWN
+    $sender.imgState=$btnState.BUTTON_DOWN
     try {
       $elems = $f.Controls.Find('l1',$false)
     } catch [exception]{
@@ -316,13 +300,15 @@ $button_OnKeyDown = {
   $sender.Invalidate()
 }
 
+$buttons | foreach-object {$_.add_KeyDown($button_OnKeyDown)  } 
+
 $button_OnKeyUp = {
   param(
     [object]$sender,[System.Windows.Forms.KeyEventArgs]$e
   )
   if ($e.KeyData -eq [System.Windows.Forms.Keys]::Space)
   {
-    $sender.ImgState = $btnState.BUTTON_FOCUSED
+    $sender.imgState=$btnState.BUTTON_FOCUSED
     try {
       $elems = $f.Controls.Find('l1',$false)
     } catch [exception]{
@@ -334,25 +320,13 @@ $button_OnKeyUp = {
   $sender.Invalidate()
 }
 
-# hook events
-$buttons | ForEach-Object {
-  $_.add_Paint($button_OnPaint)
-  $_.add_GotFocus($button_OnGotFocus)
-  $_.add_LostFocus($button_OnLostFocus)
-  $_.add_MouseEnter($button_OnMouseEnter)
-  $_.add_MouseLeave($button_OnMouseLeave)
-  $_.add_MouseDown($button_OnMouseDown)
-  $_.add_MouseUp($button_OnMouseUp)
-  $_.add_EnabledChanged($button_OnEnabledChanged)
-  $_.add_KeyDown($button_OnKeyDown)
-  $_.add_KeyUp($button_OnKeyUp)
-}
+$buttons | foreach-object {$_.add_KeyUp($button_OnKeyUp)  } 
 
 # Form
 $f.AutoScaleDimensions = New-Object System.Drawing.SizeF (6.0,13.0)
 $f.AutoScaleMode = [System.Windows.Forms.AutoScaleMode]::Font
-$f.ClientSize = New-Object System.Drawing.Size (263,139)
-$f.Controls.AddRange(@( $l1,$l2,$l3,$o1,$o2,$o3))
+$f.ClientSize = New-Object System.Drawing.Size (263,109)
+$f.Controls.AddRange(@( $l1,$l2,$o1,$o2))
 $f.Name = 'form'
 $f.StartPosition = [System.Windows.Forms.FormStartPosition]::CenterScreen
 $f.Text = 'Bitmap Button Job Launcher'
@@ -397,28 +371,24 @@ if ($PSBoundParameters['pause']) {
 } else {
   Start-Sleep -Millisecond 1000
 }
-$total_steps = 20
-1..($total_steps) | ForEach-Object {
+$total_steps = 10
+1..($total_steps ) | ForEach-Object {
 
   $current_step = $_
-  $message = ('Processed {0} / {1}' -f $current_step,$total_steps)
-  Write-Output $message
+  $message = ( 'Processed {0} / {1}' -f $current_step , $total_steps )
+ write-output $message
 
-  $so.Buttons | ForEach-Object {
+$so.Buttons | foreach-object { 
 
-    if ($_.Pressed) {
-      # start-job 
-      $message = ('Processing "{0}"' -f $_.Text)
-      Write-Output $message
-      $_.Enabled = $false
-      Start-Sleep -Millisecond 1000
-      # receive-job 
-      $_.Enabled = $true
-      Start-Sleep -Millisecond 1000
-      $_.Pressed = $false
-    }
-  }
-  Start-Sleep -Millisecond 1000
+if ($_.Pressed){ 
+ $_.Enabled = $false
+ Start-Sleep -Millisecond 1000
+ $_.Enabled = $true
+ Start-Sleep -Millisecond 1000
+ $_.Pressed = $false
+}
+}
+ Start-Sleep -Millisecond 1000
 
 }
 
