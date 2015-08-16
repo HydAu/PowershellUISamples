58230a398ff802212fd653dfd657641aebe3339c
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-08-16 12:00:00 -0400

    final revision

--- external/get_task_scheduler_event_log.ps1
+++ external/get_task_scheduler_event_log.ps1
@@ -21,6 +21,14 @@
 
 # http://www.adamtheautomator.com/powershell-event-logs/
 # https://powertoe.wordpress.com/2009/1a2/30/powershell-and-scheduled-task-logs/
+# https://msdn.microsoft.com/en-us/library/system.diagnostics.eventing.reader%28v=vs.100%29.aspx
+# https://msdn.microsoft.com/en-us/library/system.diagnostics.eventing.reader.eventrecord_properties%28v=vs.110%29.aspx
+# http://stackoverflow.com/questions/8567368/eventlogquery-time-format-expected/8575390#8575390
+# http://blogs.msdn.com/b/davethompson/archive/2011/10/25/running-a-scheduled-task-after-another.aspx
+# http://michal.is/blog/query-the-event-log-with-c-net/
+# http://stackoverflow.com/questions/22685530/could-not-load-file-or-assembly-newtonsoft-json-or-one-of-its-dependencies-ma
+# https://github.com/ricardotealdi/Simple.Serializer
+# switch to http://www.codeproject.com/Articles/785293/Json-Parser-Viewer-and-Serializer
 
 
 function run_helper_assembly
@@ -35,13 +43,15 @@ function run_helper_assembly
 </QueryList>
 "@,
     [string]$logfile = 'report.log',
+    [string]$comment = '',
     [switch]$debug
   )
 
   if ($assembly_name -eq $null -or $assembly_name -eq '') { 
     Throw 'Need assembly name'
   }
-  Write-Debug "Loading embedded assembly ${assembly_name}"
+  Write-Output $comment
+  Write-Output "Loading embedded assembly ${assembly_name}"
 
   $helper = New-Object $assembly_name -ErrorAction 'SilentlyContinue'
 
@@ -62,7 +72,6 @@ function run_helper_assembly
   $result | Select-Object -First 1 | Out-File $report -Append -Encoding 'ASCII'
 
 }
-
 
 
 $report = 'report.log'
@@ -86,17 +95,121 @@ $events_object | Select-Object -First 1 | Format-List
 
 Write-Debug 'Fast version'
 
-# https://msdn.microsoft.com/en-us/library/system.diagnostics.eventing.reader%28v=vs.100%29.aspx
-# https://msdn.microsoft.com/en-us/library/system.diagnostics.eventing.reader.eventrecord_properties%28v=vs.110%29.aspx
-# http://stackoverflow.com/questions/8567368/eventlogquery-time-format-expected/8575390#8575390
-# http://blogs.msdn.com/b/davethompson/archive/2011/10/25/running-a-scheduled-task-after-another.aspx
-# http://michal.is/blog/query-the-event-log-with-c-net/
-# http://stackoverflow.com/questions/22685530/could-not-load-file-or-assembly-newtonsoft-json-or-one-of-its-dependencies-ma
-# https://github.com/ricardotealdi/Simple.Serializer
-# switch to http://www.codeproject.com/Articles/785293/Json-Parser-Viewer-and-Serializer
 
-Write-Debug "Attempting JSON serialization that does not work"
+# suppress the warnings - cannot use the CompilerParameters and ReferencedAssemblies parameters in the same command.
 
+Add-Type -IgnoreWarnings @"
+using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Security;
+using System.Collections;
+using Newtonsoft.Json;
+
+namespace EventQuery
+{
+    public class JsonSerializedWorkingExample
+    {
+        // log the entries to console
+        private bool _verbose;
+        public bool Verbose
+        {
+            get
+            {
+                return _verbose;
+            }
+            set
+            {
+                _verbose = value;
+            }
+
+        }
+
+        private String _query = @"<QueryList>" +
+                  "<Query Id='0' Path='Microsoft-Windows-TaskScheduler/Operational'>" +
+                  "<Select Path='Microsoft-Windows-TaskScheduler/Operational'>" +
+                  "*[System[(Level=1  or Level=2 or Level=3 or Level=4) and " +
+                  "TimeCreated[timediff(@SystemTime) &lt;= 14400000]]]" + "</Select>" +
+                  "</Query>" +
+                  "</QueryList>";
+
+        public String Query
+        {
+            get
+            {
+                return _query;
+            }
+            set
+            {
+                _query = value;
+            }
+        }
+        public object[] QueryActiveLog()
+        {
+            // TODO: Extend structured query to two different event logs.
+            EventLogQuery eventsQuery = new EventLogQuery("Application", PathType.LogName, Query);
+            EventLogReader logReader = new EventLogReader(eventsQuery);
+            return DisplayEventAndLogInformation(logReader);
+        }
+
+        private object[] DisplayEventAndLogInformation(EventLogReader logReader)
+        {
+            ArrayList eventlog_json_arraylist = new ArrayList();
+            for (EventRecord eventInstance = logReader.ReadEvent();
+                null != eventInstance; eventInstance = logReader.ReadEvent())
+            {
+
+	
+                string eventlog_json = null;
+                try { eventlog_json =  JsonConvert.SerializeObject(eventInstance);
+		} catch (Exception e){
+			// Assert
+		}
+                eventlog_json_arraylist.Add(eventlog_json);
+
+                if (Verbose)
+                {
+                    Console.WriteLine("-----------------------------------------------------");
+                    Console.WriteLine("Event ID: {0}", eventInstance.Id);
+                    Console.WriteLine("Level: {0}", eventInstance.Level);
+                    Console.WriteLine("LevelDisplayName: {0}", eventInstance.LevelDisplayName);
+                    Console.WriteLine("Opcode: {0}", eventInstance.Opcode);
+                    Console.WriteLine("OpcodeDisplayName: {0}", eventInstance.OpcodeDisplayName);
+                    Console.WriteLine("TimeCreated: {0}", eventInstance.TimeCreated);
+                    Console.WriteLine("Publisher: {0}", eventInstance.ProviderName);
+                }
+                try
+                {
+                    if (Verbose)
+                    {
+                        Console.WriteLine("Description: {0}", eventInstance.FormatDescription());
+                    }
+                }
+                catch (EventLogException)
+                {
+
+                    // The event description contains parameters, and no parameters were 
+                    // passed to the FormatDescription method, so an exception is thrown.
+
+                }
+
+                // Cast the EventRecord object as an EventLogRecord object to 
+                // access the EventLogRecord class properties
+                EventLogRecord logRecord = (EventLogRecord)eventInstance;
+                if (Verbose)
+                {
+                    Console.WriteLine("Container Event Log: {0}", logRecord.ContainerLog);
+                }
+            }
+            object[] result = eventlog_json_arraylist.ToArray();
+            return result;
+        }
+
+
+    }
+}
+
+
+"@ -ReferencedAssemblies 'System.dll','System.Security.dll','System.Core.dll',([System.IO.Path]::Combine($shared_assemblies_path,$assembly_name))
 
 Add-Type -IgnoreWarnings @"
 
@@ -230,175 +343,8 @@ namespace EventQuery
 
 "@ -ReferencedAssemblies 'System.dll','System.Xml.dll','System.Security.dll','System.Core.dll','System.Runtime.Serialization.dll'
 
-run_helper_assembly -assembly_name 'EventQuery.JsonSerializedNonWorkingExample' -logfile 'broken.log'
-
-Write-Debug "Loading external assembly ${assembly_name}"
-Add-Type -Path ([System.IO.Path]::Combine($shared_assemblies_path,$assembly_name))
-
-$helper = New-Object 'EventQuery.JsonSerializedNonWorkingExample' -ErrorAction 'SilentlyContinue'
-
-$helper.Query = @"
-<QueryList>
-<Query Id='0' Path='Microsoft-Windows-TaskScheduler/Operational'>
-<Select Path='Microsoft-Windows-TaskScheduler/Operational'>*[System[Level=4 and TimeCreated[timediff(@SystemTime) &lt;= 1800000]]]</Select>
-</Query>
-</QueryList>
-"@
-$helper.Verbose = $false
-Write-Debug ("Query:`r`n{0}" -f $helper.Query)
-try {
-  $result = $helper.QueryActiveLog()
-} catch [exception]{
-
-}
-
-Write-Output ('Result: {0} rows' -f $result.count)
-Write-Output 'Sample entry:'
-Write-Output $result | Select-Object -First 1 | ConvertFrom-Json
-
-
-# suppress the warnings - cannot use the CompilerParameters and ReferencedAssemblies parameters in the same command.
-
-Add-Type -IgnoreWarnings @"
-using System;
-using System.Diagnostics.Eventing.Reader;
-using System.Security;
-using System.Collections;
-using Newtonsoft.Json;
-
-namespace EventQuery
-{
-    public class JsonSerializedWorkingExample
-    {
-        // log the entries to console
-        private bool _verbose;
-        public bool Verbose
-        {
-            get
-            {
-                return _verbose;
-            }
-            set
-            {
-                _verbose = value;
-            }
-
-        }
-
-        private String _query = @"<QueryList>" +
-                  "<Query Id='0' Path='Microsoft-Windows-TaskScheduler/Operational'>" +
-                  "<Select Path='Microsoft-Windows-TaskScheduler/Operational'>" +
-                  "*[System[(Level=1  or Level=2 or Level=3 or Level=4) and " +
-                  "TimeCreated[timediff(@SystemTime) &lt;= 14400000]]]" + "</Select>" +
-                  "</Query>" +
-                  "</QueryList>";
-
-        public String Query
-        {
-            get
-            {
-                return _query;
-            }
-            set
-            {
-                _query = value;
-            }
-        }
-        public object[] QueryActiveLog()
-        {
-            // TODO: Extend structured query to two different event logs.
-            EventLogQuery eventsQuery = new EventLogQuery("Application", PathType.LogName, Query);
-            EventLogReader logReader = new EventLogReader(eventsQuery);
-            return DisplayEventAndLogInformation(logReader);
-        }
-
-        private object[] DisplayEventAndLogInformation(EventLogReader logReader)
-        {
-            ArrayList eventlog_json_arraylist = new ArrayList();
-            for (EventRecord eventInstance = logReader.ReadEvent();
-                null != eventInstance; eventInstance = logReader.ReadEvent())
-            {
-
-	
-                string eventlog_json = null;
-                try { eventlog_json =  JsonConvert.SerializeObject(eventInstance);
-		} catch (Exception e){
-			// Assert
-		}
-                eventlog_json_arraylist.Add(eventlog_json);
-
-                if (Verbose)
-                {
-                    Console.WriteLine("-----------------------------------------------------");
-                    Console.WriteLine("Event ID: {0}", eventInstance.Id);
-                    Console.WriteLine("Level: {0}", eventInstance.Level);
-                    Console.WriteLine("LevelDisplayName: {0}", eventInstance.LevelDisplayName);
-                    Console.WriteLine("Opcode: {0}", eventInstance.Opcode);
-                    Console.WriteLine("OpcodeDisplayName: {0}", eventInstance.OpcodeDisplayName);
-                    Console.WriteLine("TimeCreated: {0}", eventInstance.TimeCreated);
-                    Console.WriteLine("Publisher: {0}", eventInstance.ProviderName);
-                }
-                try
-                {
-                    if (Verbose)
-                    {
-                        Console.WriteLine("Description: {0}", eventInstance.FormatDescription());
-                    }
-                }
-                catch (EventLogException)
-                {
-
-                    // The event description contains parameters, and no parameters were 
-                    // passed to the FormatDescription method, so an exception is thrown.
-
-                }
-
-                // Cast the EventRecord object as an EventLogRecord object to 
-                // access the EventLogRecord class properties
-                EventLogRecord logRecord = (EventLogRecord)eventInstance;
-                if (Verbose)
-                {
-                    Console.WriteLine("Container Event Log: {0}", logRecord.ContainerLog);
-                }
-            }
-            object[] result = eventlog_json_arraylist.ToArray();
-            return result;
-        }
-
-
-    }
-}
-
-
-"@ -ReferencedAssemblies 'System.dll','System.Security.dll','System.Core.dll',([System.IO.Path]::Combine($shared_assemblies_path,$assembly_name))
-
-Write-Debug 'Running embedded assembly:'
-
-$helper = New-Object 'EventQuery.JsonSerializedWorkingExample' -ErrorAction 'SilentlyContinue'
-
-$helper.Query = @"
-<QueryList>
-<Query Id='0' Path='Microsoft-Windows-TaskScheduler/Operational'>
-<Select Path='Microsoft-Windows-TaskScheduler/Operational'>*[System[Level=4 and TimeCreated[timediff(@SystemTime) &lt;= 1800000]]]</Select>
-</Query>
-</QueryList>
-"@
-$helper.Verbose = $false
-Write-Debug ("Query:`r`n{0}" -f $helper.Query)
-try {
-  $result = $helper.QueryActiveLog()
-} catch [exception]{
-
-}
-
-Write-Output ('Result: {0} rows' -f $result.count)
-Write-Output 'Sample entry:'
-Write-Output $result | Select-Object -First 1 | ConvertFrom-Json
-
-Write-Debug ('Saving sample entry to {0}' -f $report)
-
-Write-Output '' | Out-File $report -Encoding 'ASCII'
-$result | Select-Object -First 1 | Out-File $report -Append -Encoding 'ASCII'
+run_helper_assembly -assembly_name 'EventQuery.JsonSerializedNonWorkingExample' -logfile 'broken.log' -comment 'Attempting JSON serialization that does not work'
+run_helper_assembly -assembly_name 'EventQuery.JsonSerializedWorkingExample' -logfile 'report.log' -comment 'JSON serialization that works'
 
 
 <#
