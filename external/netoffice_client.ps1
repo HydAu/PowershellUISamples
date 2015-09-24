288256a10356b99622c58daf965698a285af15c8
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-09-24 11:47:05 -0400

    intermediate revision  - debugging cell interface

--- external/netoffice_client.ps1
+++ external/netoffice_client.ps1
@@ -1,3 +1,25 @@
+#Copyright (c) 2015 Serguei Kouzmine
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in
+#all copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#THE SOFTWARE.
+
+# https://github.com/NetOfficeFw/NetOffice
+
 # http://stackoverflow.com/questions/8343767/how-to-get-the-current-directory-of-the-cmdlet-being-executed
 function Get-ScriptDirectory
 {
@@ -39,11 +61,11 @@ $shared_assemblies | ForEach-Object {
 popd
 
 Write-Output 'Start Excel and turn off msg boxes'
-[NetOffice.ExcelApi.Application]$excelApplication = New-Object NetOffice.ExcelApi.Application
-$excelApplication.DisplayAlerts = $false
+[NetOffice.ExcelApi.Application]$excel = New-Object NetOffice.ExcelApi.Application
+$excel.DisplayAlerts = $false
 
 Write-Output 'Add a new workbook'
-[NetOffice.ExcelApi.Workbook]$workBook = $excelApplication.Workbooks.Add()
+[NetOffice.ExcelApi.Workbook]$workBook = $excel.Workbooks.Add()
 [NetOffice.ExcelApi.Worksheet]$workSheet = [NetOffice.ExcelApi.Worksheet]$workBook.Worksheets[1]
 
 ## Get-Member : The member "Caller" is already present.
@@ -72,38 +94,38 @@ namespace ExcelExamples
 
 "@ -ReferencedAssemblies 'System.Windows.Forms.dll','System.Drawing.dll','System.Data.dll'
 function GetDefaultExtension {
-  param([NetOffice.ExcelApi.Application]$application)
+  param(
+   [NetOffice.ExcelApi.Application]$application
+  )
 
   [double]$Version = [System.Convert]::ToDouble($application.Version,[System.Globalization.CultureInfo]::InvariantCulture)
   if ($Version -ge 12.00) {
-    return ".xlsx" }
+    return '.xlsx' }
   else {
-    return ".xls"
+    return '.xls'
   }
 
 
 }
 Write-Output 'Draw back color and perform the BorderAround method'
-$workSheet.Range("`$B2:`$B5").Interior.Color = [ExcelExamples.Example]::ToDouble([System.Drawing.Color]::DarkGreen)
-$workSheet.Range("`$B2:`$B5").BorderAround([NetOffice.ExcelApi.Enums.XlLineStyle]::xlContinuous,[NetOffice.ExcelApi.Enums.XlBorderWeight]::xlMedium,[NetOffice.ExcelApi.Enums.XlColorIndex]::xlColorIndexAutomatic)
+$workSheet.Range('$B2:$B5').Interior.Color = [ExcelExamples.Example]::ToDouble([System.Drawing.Color]::DarkGreen)
+$workSheet.Range('$B2:$B5').BorderAround([NetOffice.ExcelApi.Enums.XlLineStyle]::xlContinuous,[NetOffice.ExcelApi.Enums.XlBorderWeight]::xlMedium,[NetOffice.ExcelApi.Enums.XlColorIndex]::xlColorIndexAutomatic)
 
 Write-Output 'Draw back color and border the range explicitly'
-$workSheet.Range("`$D2:`$D5").Interior.Color = [ExcelExamples.Example]::ToDouble([System.Drawing.Color]::DarkGreen)
-$workSheet.Range("`$D2:`$D5").Borders[[NetOffice.ExcelApi.Enums.XlBordersIndex]::xlInsideHorizontal].LineStyle = [NetOffice.ExcelApi.Enums.XlLineStyle]::xlDouble
-$workSheet.Range("`$D2:`$D5").Borders[[NetOffice.ExcelApi.Enums.XlBordersIndex]::xlInsideHorizontal].Weight = 4
-$workSheet.Range("`$D2:`$D5").Borders[[NetOffice.ExcelApi.Enums.XlBordersIndex]::xlInsideHorizontal].Color = [ExcelExamples.Example]::ToDouble([System.Drawing.Color]::Black)
+$workSheet.Range('$D2:$D5').Interior.Color = [ExcelExamples.Example]::ToDouble([System.Drawing.Color]::DarkGreen)
+$workSheet.Range('$D2:$D5').Borders[[NetOffice.ExcelApi.Enums.XlBordersIndex]::xlInsideHorizontal].LineStyle = [NetOffice.ExcelApi.Enums.XlLineStyle]::xlDouble
+$workSheet.Range('$D2:$D5').Borders[[NetOffice.ExcelApi.Enums.XlBordersIndex]::xlInsideHorizontal].Weight = 4
+$workSheet.Range('$D2:$D5').Borders[[NetOffice.ExcelApi.Enums.XlBordersIndex]::xlInsideHorizontal].Color = [ExcelExamples.Example]::ToDouble([System.Drawing.Color]::Black)
 
 Write-Output 'Add Cells'
 # $workSheet.Cells(1, 1).Value = "We have 2 simple shapes created."
 
 Write-Output 'Save the book'
-[string]$fileExtension = GetDefaultExtension -application $excelApplication
-# [string]$fileExtension = 'xls'
-[string]$workbookFile = [System.IO.Path]::Combine((Get-ScriptDirectory),("Example01.{0}" -f $fileExtension))
+[string]$file_extension = GetDefaultExtension -application $excel
+[string]$workbookFile = [System.IO.Path]::Combine((Get-ScriptDirectory),('Example01.{0}' -f $file_extension))
 $workBook.SaveAs($workbookFile)
 
 Write-Output 'Close excel and dispose reference'
-$excelApplication.Quit()
-$excelApplication.Dispose()
+$excel.Quit()
+$excel.Dispose()
 
-[NetOffice.ExcelApi.Enums.XlLineStyle]::xlLineStyleNone
