d144b4fe1dbb2e0e5153f72960f29a59251e161e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-09-24 15:10:19 -0400

    intermediate revision - added chart example

--- external/csharp/netoffice/Test.cs
+++ external/csharp/netoffice/Test.cs
@@ -8,7 +8,7 @@ using System.IO;
 using System.Globalization;
 
 using NetOffice;
-using Excel = NetOffice.ExcelApi;
+using NetOffice.ExcelApi;
 using NetOffice.ExcelApi.Enums;
 
 namespace ExcelExamples
@@ -19,12 +19,12 @@ namespace ExcelExamples
         public static void Main()
         {
             // start excel and turn off msg boxes
-            Excel.Application excelApplication = new Excel.Application();
+            NetOffice.ExcelApi.Application excelApplication = new NetOffice.ExcelApi.Application();
             excelApplication.DisplayAlerts = false;
 
             // add a new workbook
-            Excel.Workbook workBook = excelApplication.Workbooks.Add();
-            Excel.Worksheet workSheet = (Excel.Worksheet)workBook.Worksheets[1];
+            NetOffice.ExcelApi.Workbook workBook = excelApplication.Workbooks.Add();
+            NetOffice.ExcelApi.Worksheet workSheet = (NetOffice.ExcelApi.Worksheet)workBook.Worksheets[1];
 
             // draw back color and perform the BorderAround method
             workSheet.Range("$B2:$B5").Interior.Color = ToDouble(Color.DarkGreen);
@@ -37,6 +37,17 @@ namespace ExcelExamples
             workSheet.Range("$D2:$D5").Borders[XlBordersIndex.xlInsideHorizontal].Color = ToDouble(Color.Black);
 
             workSheet.Cells[1, 1].Value = "We have 2 simple shapes created.";
+
+
+            // we need some data to display
+            NetOffice.ExcelApi.Range dataRange = PutSampleData(workSheet);
+
+            // create a nice diagram
+            NetOffice.ExcelApi.ChartObject chart = ((NetOffice.ExcelApi.ChartObjects)workSheet.ChartObjects()).Add(70, 100, 375, 225);
+            chart.Chart.SetSourceData(dataRange);
+
+
+
 
             // save the book 
             string fileExtension = GetDefaultExtension(excelApplication);            
@@ -70,7 +81,7 @@ namespace ExcelExamples
         /// </summary>
         /// <param name="application">the instance</param>
         /// <returns>the extension</returns>
-        private static string GetDefaultExtension(Excel.Application application)
+        private static string GetDefaultExtension(NetOffice.ExcelApi.Application application)
         {
             double Version = Convert.ToDouble(application.Version, CultureInfo.InvariantCulture);
             if (Version >= 12.00)
@@ -78,6 +89,36 @@ namespace ExcelExamples
             else
                 return ".xls";
         }
+
+        private static NetOffice.ExcelApi.Range PutSampleData(NetOffice.ExcelApi.Worksheet workSheet)
+        {
+            workSheet.Cells[2, 2].Value = "Datum";
+            workSheet.Cells[3, 2].Value = DateTime.Now.ToShortDateString();
+            workSheet.Cells[4, 2].Value = DateTime.Now.ToShortDateString();
+            workSheet.Cells[5, 2].Value = DateTime.Now.ToShortDateString();
+            workSheet.Cells[6, 2].Value = DateTime.Now.ToShortDateString();
+
+            workSheet.Cells[2, 3].Value = "Columns1";
+            workSheet.Cells[3, 3].Value = 25;
+            workSheet.Cells[4, 3].Value = 33;
+            workSheet.Cells[5, 3].Value = 30;
+            workSheet.Cells[6, 3].Value = 22;
+
+            workSheet.Cells[2, 4].Value = "Column2";
+            workSheet.Cells[3, 4].Value = 25;
+            workSheet.Cells[4, 4].Value = 33;
+            workSheet.Cells[5, 4].Value = 30;
+            workSheet.Cells[6, 4].Value = 22;
+
+            workSheet.Cells[2, 5].Value = "Column3";
+            workSheet.Cells[3, 5].Value = 25;
+            workSheet.Cells[4, 5].Value = 33;
+            workSheet.Cells[5, 5].Value = 30;
+            workSheet.Cells[6, 5].Value = 22;
+
+            return workSheet.Range("$B2:$E6");
+        }
+
 
     }
 }
