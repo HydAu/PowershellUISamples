b3ebbb45afda44e317d85327df0f13ba343ae188
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-10-21 21:16:05 -0400

    initial snapshot

---
+++ external/csharp/form_invoker/Program/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PowershellRunner
+{
+    static class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PowershellRunnerForm());
+        }
+    }
+}
