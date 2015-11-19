35f0db1ade36eb24c931d53a6eb66ef79053a014
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2015-11-18 20:20:19 -0500

    intermediate revision

--- csharp/timing/Program/ProcessTreeScanner.cs
+++ csharp/timing/Program/ProcessTreeScanner.cs
@@ -1,20 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
 using System.Text.RegularExpressions;
 using System.Management;
 using System.ComponentModel;
 using System.Diagnostics;
 /// http://www.c-sharpcorner.com/UploadFile/scottlysle/FindListProcessesCS09102007024714AM/FindListProcessesCS.aspx
+//
 
+using NUnit.Framework;
 
 namespace SystemTrayApp
 {
 
     public class ProcessTreeScanner
     {
-
+    	private const string s_dummy = @"command";
         private bool _navigationStatus;
         private bool _discoveryStatus;
         private string _pattern;
@@ -27,40 +28,36 @@ namespace SystemTrayApp
 
         public void Perform()
         {
-
             this.NavigationStatus = true;
             this.DiscoveryStatus = false;
+            String logPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
-                string parentProcessId = FindProcessByName(this.ParentProcessName);
-
+            string parentProcessId = FindProcessByName(this.ParentProcessName);
+            if (this.NavigationStatus)
+            {
                 string processes = FindProcessByParentProcessId(parentProcessId);
-                Console.WriteLine(processes);
-                if (DiscoveryStatus)
+                if (this.DiscoveryStatus)
                 {
-                    Console.WriteLine("discovered");
+                	Console.WriteLine(String.Format("Discovered {0}", processes));
                 }
-
+            }
         }
 
         public string FindProcessByName(string processName)
         {
             ManagementClass MgmtClass = new ManagementClass("Win32_Process");
-            NavigationStatus = false;
-            String result = null;
+            String s_managed_object_processid = null;
 
             foreach (ManagementObject mo in MgmtClass.GetInstances())
             {
-            	
-            	string x = mo["Name"].ToString();
-            	Console.WriteLine(x);
-            	
-                if (mo["Name"].ToString().ToLower() == processName.ToLower())
+                string s_managed_object_name = mo["Name"].ToString();
+                if (s_managed_object_name.ToLower() == processName.ToLower())
                 {
-                    NavigationStatus = true;
-                    result = mo["ProcessID"].ToString();
+                    this.NavigationStatus = true;
+                    s_managed_object_processid = mo["ProcessID"].ToString();
                 }
             }
-            return result;
+            return s_managed_object_processid;
         }
 
         public string FindProcessByParentProcessId(string parentProcessId)
@@ -68,41 +65,49 @@ namespace SystemTrayApp
 
             StringBuilder sb = new StringBuilder();
             ManagementClass MgmtClass = new ManagementClass("Win32_Process");
-            this.NavigationStatus = false;
             foreach (ManagementObject mo in MgmtClass.GetInstances())
             {
-                if (mo["ParentProcessId"].ToString() == parentProcessId)
+                // recursion needs to be implemented if one wants to collect children of puppet.exe:
+                // typical Puppet manifest sendom uses Package provider on windows
+                // Exec provider is more likely 
+                // therefore Puppet would not be direct parent of the msiexec process.
+
+                // if (mo["ParentProcessId"].ToString() == parentProcessId)
+                // {
+
+                string s_process_selection_patterns = @"(?<known>msi|setup||" + s_dummy + ")";
+                string s_managed_object_commandline = null;
+                try
                 {
-                    sb.Append(mo["Name"].ToString());
+                    s_managed_object_commandline = mo["CommandLine"].ToString();
+                }
+                catch (System.NullReferenceException e)
+                {
+                	// for debugging
+                    Assert.IsNotEmpty(e.Message);
+                }
+                if (s_managed_object_commandline != null)
+                {
+                    sb.Append(s_managed_object_commandline);
                     this.DiscoveryStatus = true;
 
-                    string sCommand = @"command";
-                    string aProcessChoiceRegExp = @"(?<known>msi|setup||" + sCommand + ")";
-                    string s = mo["CommandLine"].ToString();
-                    Console.WriteLine(s);
-
                     MatchCollection myMatchCollection =
-                       Regex.Matches(s, aProcessChoiceRegExp);
+                       Regex.Matches(s_managed_object_commandline, s_process_selection_patterns);
 
                     foreach (Match myMatch in myMatchCollection)
                     {
                         Console.WriteLine("=> " + myMatch.Groups["known"]);
-                        foreach (Group myGroup in myMatch.Groups)
+                        foreach (Group match_group in myMatch.Groups)
                         {
-
-                            foreach (Capture myCapture in myGroup.Captures)
+                            foreach (Capture match_group_capture in match_group.Captures)
                             {
-                                Console.WriteLine("myCapture.Value = " + myCapture.Value);
+                                Console.WriteLine("match_group_capture.Value = " + match_group_capture.Value);
                             }
-
                         }
-
                     }
                 }
             }
             return sb.ToString();
-
         }
-
     }
 }
