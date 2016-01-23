a2b601fab8a5d7002e8366e73373bf598af38856
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2016-01-23 15:17:23 -0500

    intermediate revision - introducing 'FindResource','FindMessage'

--- external/yaml_serializer2.ps1
+++ external/yaml_serializer2.ps1
@@ -1,70 +1,72 @@
 # origin: https://github.com/scottmuc/PowerYaml
 # https://github.com/aaubry/YamlDotNet
 
-$shared_assemblies_path = 'C:\developer\sergueik\csharp\SharedAssemblies'
-[string[]]$shared_assemblies = @(
-  # very old version of assembly copied from PowerYaml
-  'YamlDotNet.Configuration.dll',
-  'YamlDotNet.Converters.dll',
-  'YamlDotNet.RepresentationModel.dll',
-  'YamlDotNet.Core.dll',
-  'nunit.core.dll' # http://www.nuget.org/api/v2/package/NUnit.Core.Engine/3.0.0-beta-4
-  'nunit.framework.dll' # TODO - check if still needed when 3.0 
-)
-
 function Add-CastingFunctions ($value) {
-  if ($PSVersionTable.PSVersion -ge '3.0') { return $value }
+  if ($PSVersionTable.PSVersion -ge "3.0") { return $value }
   return Add-CastingFunctionsForPosh2 ($value)
 }
 
 function Add-CastingFunctionsForPosh2 ($value) {
-  return Add-Member -InputObject $value -Name ToInt -MemberType ScriptMethod -PassThru -Value `
+  return Add-Member -InputObject $value -Name ToInt `
+     -MemberType ScriptMethod -PassThru -Value `
      { [int]$this } |
-  Add-Member -Name ToLong -MemberType ScriptMethod -PassThru -Value `
+  Add-Member -Name ToLong `
+     -MemberType ScriptMethod -PassThru -Value `
      { [long]$this } |
-  Add-Member -Name ToDouble -MemberType ScriptMethod -PassThru -Value `
+  Add-Member -Name ToDouble `
+     -MemberType ScriptMethod -PassThru -Value `
      { [double]$this } |
-  Add-Member -Name ToDecimal -MemberType ScriptMethod -PassThru -Value `
+  Add-Member -Name ToDecimal `
+     -MemberType ScriptMethod -PassThru -Value `
      { [decimal]$this } |
-  Add-Member -Name ToByte -MemberType ScriptMethod -PassThru -Value `
+  Add-Member -Name ToByte `
+     -MemberType ScriptMethod -PassThru -Value `
      { [byte]$this } |
-  Add-Member -Name ToBoolean -MemberType ScriptMethod -PassThru -Value `
+  Add-Member -Name ToBoolean `
+     -MemberType ScriptMethod -PassThru -Value `
      { [System.Boolean]::Parse($this) }
 }
 
-function Get-YamlDocumentFromString {
-  param([string]$raw_data)
-  $s = New-Object System.IO.StringReader ($raw_data)
-  $o = New-Object YamlDotNet.RepresentationModel.YamlStream
-  $o.Load([System.IO.TextReader]$s)
-  return $o.Documents[0]
+function get-YamlData ([string]$data) {
+  $stringReader = New-Object System.IO.StringReader ($data)
+  $yamlStream = New-Object YamlDotNet.RepresentationModel.YamlStream
+  $yamlStream.Load([System.IO.TextReader]$stringReader)
+
+  return $yamlStream
 }
 
-function Get-YamlStream {
-  param([string]$file_path)
-  $r = [System.IO.File]::OpenText($file_path)
-  $o = New-Object YamlDotNet.RepresentationModel.YamlStream
-  $o.Load([System.IO.TextReader]$r)
-  $r.Close()
-  return $o
+function Get-YamlStream ([string]$file) {
+  $streamReader = [System.IO.File]::OpenText($file)
+  $yamlStream = New-Object YamlDotNet.RepresentationModel.YamlStream
+  $yamlStream.Load([System.IO.TextReader]$streamReader)
+  $streamReader.Close()
+  return $yamlStream
 }
 
-function Get-YamlDocument {
-  param([string]$file_path)
-  $o = Get-YamlStream $file_path
-  # BUG ? : 
-  # $yamlStream.RootNode 
-  return $o.Documents[0].RootNode
+function Get-YamlDocument ([string]$file) {
+  $yamlStream = Get-YamlStream $file
+  # Cannot index into a null array.
+  $document = $yamlStream.Documents[0].RootNode
+  # BUG: occationally the $yamlStream does not 
+  # $document = $yamlStream.RootNode
+
+  return $document
 }
 
-function Explode-Node {
-  param($node)
-  $node_type = $node.GetType().Name
-  if ($node_type -eq 'YamlScalarNode') {
+function Get-YamlDocumentFromString ([string]$yamlString) {
+  $stringReader = New-Object System.IO.StringReader ($yamlString)
+  $yamlStream = New-Object YamlDotNet.RepresentationModel.YamlStream
+  $yamlStream.Load([System.IO.TextReader]$stringReader)
+  $document = $yamlStream.Documents[0]
+  return $document
+}
+
+function Explode-Node ($node) {
+  if ($node.GetType().Name -eq "YamlScalarNode") {
     return Convert-YamlScalarNodeToValue $node
-  } elseif ($node_type -eq 'YamlMappingNode') {
+  } elseif ($node.GetType().Name -eq "YamlMappingNode") {
     return Convert-YamlMappingNodeToHash $node
-  } elseif ($node_type -eq 'YamlSequenceNode') {
+  } elseif ($node.GetType().Name -eq "YamlSequenceNode") {
     return Convert-YamlSequenceNodeToList $node
   }
 }
@@ -74,19 +76,19 @@ function Convert-YamlScalarNodeToValue ($node) {
 }
 
 function Convert-YamlMappingNodeToHash ($node) {
-  $h = @{}
-  $y = $node.Children
-  foreach ($k in $y.Keys) {
-    $h[$k.Value] = Explode-Node $y[$k]
+  $hash = @{}
+  $yamlNodes = $node.Children
+  foreach ($key in $yamlNodes.Keys) {
+    $hash[$key.Value] = Explode-Node $yamlNodes[$key]
   }
-  return $h
+  return $hash
 }
 
-function Convert-YamlSequenceNodeToList {
-  param($node)
+function Convert-YamlSequenceNodeToList ($node) {
   $list = @()
-  foreach ($y in $node.Children) {
-    $list += Explode-Node $y
+  $yamlNodes = $node.Children
+  foreach ($yamlNode in $yamlNodes) {
+    $list += Explode-Node $yamlNode
   }
   return $list
 }
@@ -106,69 +108,137 @@ function Get-ScriptDirectory
   }
 }
 
-# http://stackoverflow.com/questions/14894864/how-to-download-a-nuget-package-without-nuget-exe-or-visual-studio-extension
-# http://www.nuget.org/api/v2/package/<assembly>/<version>
-$versions = @{
-  'YamlDotNet.Configuration' = $null; # 'YamlDotNet.Configuration' is no longer available through nuget
-  'YamlDotNet.dll' = '3.7.0'; # currently unused - all functionality is probably in this assembly
-  'YamlDotNet.Converters' = '2.2.0';
-  'YamlDotNet.RepresentationModel' = '2.2.0';
-  'YamlDotNet.Core' = '2.2.0';
+
+function OpenLog {
+  param([string]$runlog)
+
+  $shared_assemblies_path = 'C:\developer\sergueik\csharp\SharedAssemblies'
+  # http://stackoverflow.com/questions/14894864/how-to-download-a-nuget-package-without-nuget-exe-or-visual-studio-extension
+  # http://www.nuget.org/api/v2/package/<assembly>/<version>
+
+  $shared_assemblies = @(
+    'YamlDotNet.dll',# '3.7.0'
+    'nunit.core.dll' # '3.0.0-beta-4'
+    'nunit.framework.dll' # TODO - check if still needed
+  )
+
+  pushd $shared_assemblies_path
+
+  $shared_assemblies | ForEach-Object {
+    Unblock-File -Path $_
+    # Write-Host -foregroundcolor 'Cyan' $_
+    Add-Type -Path $_
+  }
+  popd
+  $data = (Get-Content -Path $runlog) -join "`n"
+  $yaml = get-YamlData $data
+  $log = Explode-Node $yaml.RootNode
+  return $log
+
 }
 
-pushd $shared_assemblies_path
+function FindResourceEventMessage {
+  param(
+    [string]$log,
+    [string]$name,
+    [string]$type,
+    [bool]$changed = $true,
+    [string]$text = ''
+  )
+  $found = $false
+  $runlog = OpenLog ($log)
+  $data = $runlog['resource_statuses']
+  $data.Keys | Where-Object { $_ -match '^(?:\w+)\[(?:\w+)\]$' } |
+  ForEach-Object {
+    $resource = $_
 
-$shared_assemblies | ForEach-Object {
-  Unblock-File -Path $_
-  Write-Output $_
-  Add-Type -Path $_
-}
-popd
-
-$filename = 'previous_run_report.yaml'
-$filepath = [System.IO.Path]::Combine((Get-ScriptDirectory),$filename)
-$data = (Get-Content -Path $filepath) -join "`n"
-$yaml = Get-YamlDocumentFromString $data
-
-$puppet_agent_state_log = Explode-Node $yaml.RootNode
-
-
-Write-Host -ForegroundColor 'yellow' 'Logs:'
-$puppet_agent_state_log['logs'] |
-ForEach-Object {
-  $log = $_
-  Write-Host -ForegroundColor 'green' $log['message']
-}
-
-Write-Host -ForegroundColor 'yellow' 'Resource:'
-
-$data = $puppet_agent_state_log['resource_statuses']
-$data.Keys | Where-Object { $_ -match '^(?:\w+)\[(?:\w+)\]$' } |
-ForEach-Object {
-  $resource = $_
-
-  if ($resource -match '^(\w+)\[(\w+)\]$') {
-    $resource_type = $matches[1]
-    $resource_title = $matches[2]
-
-    if ($resource_type -eq 'Reboot' -and $resource_title -eq 'testrun') {
-      @( 'resource_type',
-        'title') | ForEach-Object {
-        $key = $_
-        Write-Host -ForegroundColor 'green' ('{0}: {1}' -f $key,$data[$resource][$key])
+    if ($resource -match '^(\w+)\[(\w+)\]$') {
+      $resource_type = $matches[1]
+      $resource_title = $matches[2]
+      if ($resource_type -eq $type -and $resource_title -eq $name) {
+        if ($message -eq '' -or ($data[$resource]['changed'] -eq 'true')) {
+          Write-Host -ForegroundColor 'blue' ('message: {0}' -f $data[$resource]['events']['message'])
+          $found = $true
+        }
       }
-
-      @(
-        'skipped',
-        'failed',
-        'changed'
-      ) | ForEach-Object {
-        $key = $_
-        Write-Host -ForegroundColor 'green' ('{0}: {1}' -f $key,($data[$resource][$key] -eq 'true'))
-      }
-
-      Write-Host -ForegroundColor 'blue' ('message: {0}' -f $data[$resource]['events']['message'])
-
     }
   }
+  return $found
+
 }
+
+
+function FindResource {
+  param(
+    [string]$log,
+    [string]$name,
+    [string]$type,
+    [bool]$changed = $true
+  )
+  $found = $false
+  $runlog = OpenLog ($log)
+  $data = $runlog['resource_statuses']
+  $data.Keys | Where-Object { $_ -match '^(?:\w+)\[(?:\w+)\]$' } |
+  ForEach-Object {
+    $resource = $_
+    if ($resource -match '^(\w+)\[(\w+)\]$') {
+      $resource_type = $matches[1]
+      $resource_title = $matches[2]
+      if ($resource_type -eq $type -and $resource_title -eq $name) {
+        if ((-not $changed) -or ($data[$resource]['changed'] -eq 'true')) {
+          @( 'resource_type',
+            'title') | ForEach-Object {
+            $key = $_
+            Write-Host -ForegroundColor 'green' ('{0}: {1}' -f $key,$data[$resource][$key])
+          }
+          @(
+            'skipped',
+            'failed',
+            'changed'
+          ) | ForEach-Object {
+            $key = $_
+            Write-Host -ForegroundColor 'green' ('{0}: {1}' -f $key,($data[$resource][$key] -eq 'true'))
+            $found = $true
+          }
+        }
+
+
+      }
+    }
+  }
+  return $found
+
+}
+
+
+function FindMessage {
+  param(
+    [string]$log,
+    [string]$text
+  )
+  $runlog = OpenLog ($log)
+  $found = $false
+  $runlog['logs'] |
+  ForEach-Object {
+    $entry = $_
+    if ($entry['message'] -match $text) {
+      Write-Host -ForegroundColor 'yellow' 'Logs:'
+      Write-Host -ForegroundColor 'green' $entry['message']
+      $found = $true
+    }
+  }
+  return $found
+}
+
+$resource_name = 'testrun'
+$resource_type = 'Reboot'
+$puppet_run_log_filename = 'previous_run_report.yaml'
+$puppet_run_log = [System.IO.Path]::Combine((Get-ScriptDirectory),$puppet_run_log_filename)
+
+FindResource -log $puppet_run_log -name $resource_name -type $resource_type
+FindMessage -log $puppet_run_log -text 'defined'
+FindResourceEventMessage -log $puppet_run_log -name $resource_name -type $resource_type -text 'defined'
+
+# exec "@(FindResource -log '#{puppet_run_log}' -name '#{resource_name}' -type '#{resource_type}').count -gt 0"
+# exec "@(FindResourceEventMessage '#{puppet_run_log}' -name '#{resource_name}' -type '#{resource_type}' -text '#{message_text}').count -gt 0"
+# exec "@(FindMessage -text '#{text}').count -gt 0"
